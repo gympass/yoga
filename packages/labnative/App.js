691b69fbd5d3bd87e1e042b0bc6520b6c3b3327b
Allysson Santos <aallysson0@gmail.com>
2019-09-10 15:59:05 -0300

    chore(app): remove .eslintrc and .prettierrc from labnative

--- packages/labnative/App.js
+++ packages/labnative/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from "react";
+import { View } from "react-native";
 
-import {Button} from '@gympass/design-system';
+import { Button } from "@gympass/design-system";
 
 const App = () => {
   return (
     <View
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-      }}>
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%"
+      }}
+    >
       <Button />
     </View>
   );
