df3f5bb9754db01e95371394238ed7715deba87f
Allysson Santos <aallysson0@gmail.com>
2019-09-12 10:11:29 -0300

    feat(tokens): init first tokens proposal

---
+++ packages/tokens/index.js
@@ -0,0 +1,119 @@
+const typography = {
+  fontFamily: '',
+  fontSize: {
+    body: {
+      level: {
+        1: 0,
+        2: 0,
+      },
+    },
+    heading: {
+      level: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+      },
+    },
+  },
+};
+const border = {
+  radius: {
+    sharp: 0,
+    rounded: 0,
+    circle: 0,
+  },
+  size: {
+    small: 0,
+    medium: 0,
+    large: 0,
+  },
+};
+const colors = {
+  madrid: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  munich: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  milanSantiago: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  amsterdam: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  saoPaulo: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  buenosAires: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  newYork: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  paris: {
+    crossfit: null,
+    climbing: null,
+    swimming: null,
+    meditation: null,
+  },
+  surface: {
+    vilaOlimpia100: null,
+    vilaOlimpia90: null,
+    vilaOlimpia80: null,
+    vilaOlimpia70: null,
+    vilaOlimpia60: null,
+    vilaOlimpia50: null,
+    vilaOlimpia40: null,
+    vilaOlimpia30: null,
+    vilaOlimpia20: null,
+    vilaOlimpia10: null,
+  },
+};
+const spacing = {
+  xtiny: 0,
+  tiny: 0,
+  xsmall: 0,
+  small: 0,
+  base: 0,
+  medium: 0,
+  large: 0,
+  xlarge: 0,
+  xxlarge: 0,
+};
+const breakpoints = {
+  small: {},
+  medium: {},
+  large: {},
+};
+const elevation = {
+  levels: {
+    1: '0px 1px 2px rgba(0, 0, 0, 0.20)',
+    2: '0px 2px 4px rgba(0, 0, 0, 0.20)',
+    3: '0px 4px 8px rgba(0, 0, 0, 0.20)',
+    4: '0px 8px 16px rgba(0, 0, 0, 0.20)',
+  },
+};
