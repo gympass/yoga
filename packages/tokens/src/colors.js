4accde8698441732f9632c3569f789e9efd8a21d
victormath12 <victorsax12@gmail.com>
2019-09-16 16:34:07 -0300

    feat(colors): adding colors design token

---
+++ packages/tokens/src/colors.js
@@ -0,0 +1,88 @@
+const red = ['#CB3530', '#F46152', '#F48170', '#FFE4E1'];
+const orange = ['#F69755', '#FFAC6F', '#FCBE94', '#FDECE0'];
+const yellow = ['#DFAF26', '#F3D04A', '#FFE461', '#FBFBBE'];
+const green = ['#52E272', '#83FC8B', '#AAFDB6', '#D4FADC'];
+const turquoise = ['#3FA99B', '#58C2B4', '#6FDCCB', '#D8F5F5'];
+const sky = ['#4C9FE4', '#6EBCEA', '#95D4EF', '#CAE5F5'];
+const ocean = ['#001027', '#21364F', '#3C6493', '#D6E1EE'];
+const purple = ['#B3A1F2', '#C5BAF4', '#D8D0FC', '#EBE6FF'];
+const white = '#FFFFFF';
+const black = '#000000';
+const gray = [
+  '#41414A',
+  '#6B6B78',
+  '#9898A7',
+  '#A9A9B8',
+  '#C5C5D6',
+  '#D3D3E2',
+  '#DBDBE8',
+  '#E6E6F0',
+  '#F5F5FA',
+];
+
+const colors = {
+  madrid: {
+    crossfit: red[0],
+    climbing: red[1],
+    swimming: red[2],
+    meditation: red[3],
+  },
+  munich: {
+    crossfit: orange[0],
+    climbing: orange[1],
+    swimming: orange[2],
+    meditation: orange[3],
+  },
+  milan: {
+    crossfit: yellow[0],
+    climbing: yellow[1],
+    swimming: yellow[2],
+    meditation: yellow[3],
+  },
+  amsterdam: {
+    crossfit: green[0],
+    climbing: green[1],
+    swimming: green[2],
+    meditation: green[3],
+  },
+  saoPaulo: {
+    crossfit: turquoise[0],
+    climbing: turquoise[1],
+    swimming: turquoise[2],
+    meditation: turquoise[3],
+  },
+  buenosAires: {
+    crossfit: sky[0],
+    climbing: sky[1],
+    swimming: sky[2],
+    meditation: sky[3],
+  },
+  newYork: {
+    crossfit: ocean[0],
+    climbing: ocean[1],
+    swimming: ocean[2],
+    meditation: ocean[3],
+  },
+  paris: {
+    crossfit: purple[0],
+    climbing: purple[1],
+    swimming: purple[2],
+    meditation: purple[3],
+  },
+  vilaOlimpia: {
+    100: gray[0],
+    90: gray[1],
+    80: gray[2],
+    70: gray[3],
+    60: gray[4],
+    50: gray[5],
+    40: gray[6],
+    30: gray[7],
+    20: gray[8],
+    10: white,
+  },
+  white,
+  black,
+};
+
+export default colors;
