ff6103e2ca21befeb6eef67a258ced58be94087b
alan-oliv <alanbruno@gmail.com>
2019-09-23 15:35:22 -0300

    feat(doc structure): new gatsby doc structure

--- packages/doc/gatsby-config.js
+++ packages/doc/gatsby-config.js
@@ -1,82 +1,31 @@
-require("dotenv").config();
-const queries = require("./src/utils/algolia");
-const config = require("./config");
-const plugins = [
-  'gatsby-plugin-sitemap',
-  'gatsby-plugin-sharp',
-  {
-    resolve: `gatsby-plugin-layout`,
-    options: {
-        component: require.resolve(`./src/templates/docs.js`)
-    }
-  },
-  'gatsby-plugin-styled-components',
-  {
-    resolve: 'gatsby-plugin-mdx',
-    options: {
-      gatsbyRemarkPlugins: [
-        {
-          resolve: "gatsby-remark-images",
-          options: {
-            maxWidth: 1035,
-            sizeByPixelDensity: true
-          }
-        },
-        {
-          resolve: 'gatsby-remark-copy-linked-files'
-        }
-      ],
-      extensions: [".mdx", ".md"]
-    }
-  },
-  'gatsby-plugin-emotion',
-  'gatsby-plugin-remove-trailing-slashes',
-  'gatsby-plugin-react-helmet',
-  {
-    resolve: "gatsby-source-filesystem",
-    options: {
-      name: "docs",
-      path: `${__dirname}/content/`
-    }
-  },
-  {
-    resolve: `gatsby-plugin-gtag`,
-    options: {
-      // your google analytics tracking id
-      trackingId: config.gatsby.gaTrackingId,
-      // Puts tracking script in the head instead of the body
-      head: true,
-      // enable ip anonymization
-      anonymize: false,
-    },
-  },
-];
-if (config.header.search && config.header.search.enabled && config.header.search.algoliaAppId && config.header.search.algoliaAdminKey) {
-  plugins.push({
-    resolve: `gatsby-plugin-algolia`,
-    options: {
-      appId: config.header.search.algoliaAppId, // algolia application id
-      apiKey: config.header.search.algoliaAdminKey, // algolia admin key to index
-      queries,
-      chunkSize: 10000, // default: 1000
-    }}
-  )
-}
 module.exports = {
-  pathPrefix: config.gatsby.pathPrefix,
   siteMetadata: {
-    title: config.siteMetadata.title,
-    description: config.siteMetadata.description,
-    docsLocation: config.siteMetadata.docsLocation,
-    ogImage: config.siteMetadata.ogImage,
-    favicon: config.siteMetadata.favicon,
-    logo: { link: config.header.logoLink ? config.header.logoLink : '/', image: config.header.logo }, // backwards compatible
-    headerTitle: config.header.title,
-    githubUrl: config.header.githubUrl,
-    helpUrl: config.header.helpUrl,
-    tweetText: config.header.tweetText,
-    headerLinks: config.header.links,
-    siteUrl: config.gatsby.siteUrl,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
   },
-  plugins: plugins
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+  ],
 };
