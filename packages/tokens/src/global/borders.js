da8b9b8bb4fa384f343c1851ed14caf0070121ac
ggdaltoso <ggdaltoso@gmail.com>
2019-09-24 17:37:54 -0300

    refactor(tokens): add border jsdoc module

--- packages/tokens/src/global/borders.js
+++ packages/tokens/src/global/borders.js
@@ -1,3 +1,24 @@
+/**
+ * @module borders
+ * @desc Borders tokens module.
+ *
+ * @memberof @gympass/tokens
+ */
+
+/**
+ * A border
+ * @typedef Border
+ *
+ * @type {Object}
+ * @property {String} zero
+ * @property {String} small
+ * @property {String} medium
+ * @property {String} large
+ */
+
+/**
+ * @type {Border}
+ */
 const border = [0, 2, 4, 8];
 
 [border.zero, border.small, border.medium, border.large] = border;
