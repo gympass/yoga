5c0983b785fd73cabf64bf7bb282f0da90939cc0
Victor Caetano <victorcaetano@Victor-Caetano.local>
2019-09-26 11:01:05 -0300

    docs(tokens): adding jsdoc in breakpoints tokens

--- packages/tokens/src/global/breakpoints.js
+++ packages/tokens/src/global/breakpoints.js
@@ -1,3 +1,23 @@
+/**
+ * @module breakpoints
+ * @desc Breakpoints tokens module.
+ *
+ * @memberof @gympass/tokens
+ */
+
+/**
+ * A Breakpoint
+ * @typedef Breakpoint
+ *
+ * @type {Object}
+ * @property {String} small
+ * @property {String} medium
+ * @property {String} large
+ */
+
+/**
+ * @type {Breakpoint}
+ */
 const breakpoints = {
   small: {},
   medium: {},
