1909e6c2dc1168f8053d6606f933d53f2dc99f73
Victor Caetano <victorcaetano@Victor-Caetano.local>
2019-09-26 11:02:54 -0300

    docs(tokens): adding jsdocs in component tokens

--- packages/tokens/src/global/components.js
+++ packages/tokens/src/global/components.js
@@ -1,10 +1,31 @@
 import colors from './colors';
 
+/**
+ * @module components
+ * @desc Components tokens module.
+ *
+ * @memberof @gympass/tokens
+ */
+
+/**
+ * A button component
+ * @typedef Button
+ * @type {Object}
+ *
+ * @property {Object} success
+ * @property {Object} error
+ */
 const button = {
   success: colors.amsterda.crossfit,
   error: colors.madrid.crossfit,
 };
 
+/**
+ * All components
+ * @typedef Component
+ * @type {Object}
+ * @property {Button} button Tokens for button component
+ */
 const components = { button };
 
 export default components;
