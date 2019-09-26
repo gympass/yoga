dbe46eb01f2feaf060c8960e45d58e3e06372481
Victor Caetano <victorcaetano@Victor-Caetano.local>
2019-09-26 11:05:05 -0300

    docs(tokens): adding jsdocs in spacing tokens

--- packages/tokens/src/global/spacing.js
+++ packages/tokens/src/global/spacing.js
@@ -1,3 +1,30 @@
+/**
+ * @module spacing
+ * @desc Spacing tokens module.
+ *
+ * @memberof @gympass/tokens
+ */
+
+/**
+ * A spacing
+ * @typedef Spacing
+ *
+ * @type {Object}
+ * @property {number} zero
+ * @property {number} xtiny
+ * @property {number} tiny
+ * @property {number} xsmall
+ * @property {number} small
+ * @property {number} base
+ * @property {number} medium
+ * @property {number} large
+ * @property {number} xlarge
+ * @property {number} xxlarge
+ */
+
+/**
+ * @type {Spacing}
+ */
 const spacing = [0, 4, 8, 12, 16, 24, 32, 48, 64, 96];
 [
   spacing.zero,
