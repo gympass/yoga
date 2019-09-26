70e16fb08208f0f2abb700ef0cfee6de44658e67
Victor Caetano <victorcaetano@Victor-Caetano.local>
2019-09-26 11:02:06 -0300

    docs(tokens): adding jsdocs in font-weight tokens

--- packages/tokens/src/global/font-weights.js
+++ packages/tokens/src/global/font-weights.js
@@ -1,3 +1,23 @@
+/**
+ * @module fontweight
+ * @desc Font Weights tokens module.
+ *
+ * @memberof @gympass/tokens
+ */
+
+/**
+ * A font Weight
+ * @typedef FontWeight
+ *
+ * @type {Object}
+ * @property {number} regular regular font weight
+ * @property {number} semibold semibold font weight
+ * @property {number} bold bold font weight
+ */
+
+/**
+ * @type {FontWeight}
+ */
 const fontWeights = [400, 600, 700];
 
 [fontWeights.regular, fontWeights.semibold, fontWeights.bold] = fontWeights;
