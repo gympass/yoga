9e3c27f8d8bf9661c526ba40b9ed9b52358627ca
Victor Caetano <victorcaetano@Victor-Caetano.local>
2019-09-26 11:04:13 -0300

    docs(tokens): adding jsdocs in radii tokens

--- packages/tokens/src/global/radii.js
+++ packages/tokens/src/global/radii.js
@@ -1,3 +1,23 @@
+/**
+ * @module radii
+ * @desc Border radius (radii) tokens module.
+ *
+ * @memberof @gympass/tokens
+ */
+
+/**
+ * A border radios (radii)
+ * @typedef Radii
+ *
+ * @type {Object}
+ * @property {number} sharp A sharp value for border radius (0 radius)
+ * @property {number} rounded A rounded value for border radius
+ * @property {String} circle A circle value for border radius (100% radius)
+ */
+
+/**
+ * @type {Radii}
+ */
 const radii = [0, 8, '100%'];
 [radii.sharp, radii.rounded, radii.circle] = radii;
 
