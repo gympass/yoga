f44a0f11d0c55737d6906a54c339e71928544357
Victor Caetano <victorcaetano@Victor-Caetano.local>
2019-09-26 11:03:32 -0300

    docs(tokens): adding jsdocs in fonts tokens

--- packages/tokens/src/global/fonts.js
+++ packages/tokens/src/global/fonts.js
@@ -1,3 +1,20 @@
+/**
+ * @module fonts
+ * @desc Fonts tokens module.
+ *
+ * @memberof @gympass/tokens
+ */
+
+/**
+ * A font
+ * @typedef Font
+ * @type {String[]}
+ * @desc Array that contains all font families.
+ */
+
+/**
+ * @type {Font}
+ */
 const fonts = ['Open Sans'];
 
 export default fonts;
