e0f5c4ead49b2296a4154c47bf4ff49e0cdacb8e
Allysson Santos <aallysson0@gmail.com>
2019-09-26 14:26:45 -0300

    docs(style): add some styles to documentation

--- packages/doc/src/components/Navigation/Navigation.jsx
+++ packages/doc/src/components/Navigation/Navigation.jsx
@@ -7,24 +7,64 @@ import Logo from '../../images/gympass.svg';
 
 const Wrapper = styled.div`
   height: 100%;
+  border-right: 1px solid #f6f6f6;
   padding: 50px;
-  width: 200px;
-
-  ul {
-    padding: 5px 0px;
-    list-style-type: none;
-    li {
-      padding-left: 10px;
-    }
-  }
 `;
 
-const getHtml = tree =>
+const List = styled.ul`
+  padding: 0px;
+  list-style-type: none;
+
+  li {
+    font-size: 22px;
+  }
+
+  a {
+    border-bottom: 1px solid #f46152;
+    color: inherit;
+    padding-bottom: 2px;
+    text-decoration: none;
+    transition: all 0.3s;
+  }
+
+  ${({ level }) =>
+    level === 0 &&
+    `
+    padding-top: 20px;
+  `}
+
+  ${({ level }) =>
+    level > 0 &&
+    `
+      li {
+        font-size: 14px;
+        padding-left: 10px;
+        padding-top: 15px;
+      }
+
+      a {
+        border-bottom: none;
+        display: block;
+        padding: 5px 10px;
+        &: hover {
+          background-color: #fff6f5;
+        }
+      }
+    `}
+
+  ${({ level }) =>
+    level === 1 &&
+    `
+    padding-bottom: 20px;
+  `}
+`;
+
+const getHtml = (tree, level = 1) =>
   Object.values(tree).map(({ title, url, ...childs }) =>
     Object.keys(childs).length ? (
       <li>
         <Link to={url}>{title}</Link>
-        <ul>{getHtml(childs)}</ul>
+        <List level={level}>{getHtml(childs, level + 1)}</List>
       </li>
     ) : (
       <li>
@@ -37,8 +77,10 @@ const Navigation = ({ items }) => {
   const tree = createTree(items);
   return (
     <Wrapper>
-      <Logo />
-      <ul>{getHtml(tree)}</ul>
+      <Link to="/">
+        <Logo />
+      </Link>
+      <List level={0}>{getHtml(tree)}</List>
     </Wrapper>
   );
 };
