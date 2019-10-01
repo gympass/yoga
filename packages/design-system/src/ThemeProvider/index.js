da2b41e2c011cb224217a854d1e922b912639a04
victormath12 <victorsax12@gmail.com>
2019-10-01 14:24:30 -0300

    refactor(themeprovider): adjusting ThemeProvider exports

--- packages/design-system/src/ThemeProvider/index.js
+++ packages/design-system/src/ThemeProvider/index.js
@@ -1,4 +1,4 @@
-import ThemeProvider, { ThemeContext, ThemeConsumer } from './ThemeProvider';
+import ThemeProvider from './ThemeProvider';
 import * as themes from './themes';
 
-export { ThemeProvider as default, ThemeContext, ThemeConsumer, themes };
+export { ThemeProvider as default, themes };
