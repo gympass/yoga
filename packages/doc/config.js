64a1a6fde0a696e2ef446f6323a4fe48c402df19
Allysson Santos <aallysson0@gmail.com>
2019-10-04 17:08:01 -0300

    refactor(gatsby): remove unused gatsby configuration

--- packages/doc/config.js
+++ packages/doc/config.js
@@ -2,43 +2,14 @@ const config = {
   gatsby: {
     pathPrefix: '/',
     siteUrl: 'https://github.com/Gympass/design-system',
-    gaTrackingId: null,
-  },
-  header: {
-    logo:
-      'https://lh3.googleusercontent.com/TaYofylGO1sDLvFc_8FPU0N7ziDTUmI8hO2OMZtWpsRRAgCwGNtC5hVhgC3SnTMMw3fK',
-    logoLink: '',
-    title: 'Gympass Design System',
-    githubUrl: '',
-    helpUrl: '',
-    tweetText: '',
-    links: [{ text: '', link: '' }],
-    search: {
-      enabled: false,
-      indexName: '',
-      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
-      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
-    },
-  },
-  sidebar: {
-    forcedNavOrder: ['/getting-started'],
-    links: [
-      {
-        text: 'Github repository',
-        link: 'https://github.com/Gympass/design-system',
-      },
-    ],
-    frontline: false,
-    ignoreIndex: true,
   },
   siteMetadata: {
     title: 'Gympass Design System',
     description: 'Documentation',
-    ogImage: null,
-    docsLocation: '',
-    favicon:
-      'https://lh3.googleusercontent.com/TaYofylGO1sDLvFc_8FPU0N7ziDTUmI8hO2OMZtWpsRRAgCwGNtC5hVhgC3SnTMMw3fK',
+    github: {
+      componentsPath:
+        'https://github.com/Gympass/design-system/blob/master/packages/design-system/src/',
+    },
   },
 };
 
