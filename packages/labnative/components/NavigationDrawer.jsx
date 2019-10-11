566ef2cdceae7a61e24442597f5cc3dad54d6c9a
ggdaltoso <ggdaltoso@gmail.com>
2019-10-11 17:27:40 -0300

    chore(labnative): toggle menu working

--- packages/labnative/components/NavigationDrawer.jsx
+++ packages/labnative/components/NavigationDrawer.jsx
@@ -2,18 +2,15 @@ import React from 'react';
 import { View, Image, TouchableOpacity } from 'react-native';
 import hamburguer from '../assets/images/drawer.png';
 
-const NavigationDrawer = ({ navigationProps }) => {
-  const toggleDrawer = () => navigationProps.toggleDrawer();
-  return (
-    <View style={{ flexDirection: 'row' }}>
-      <TouchableOpacity onPress={() => toggleDrawer}>
-        <Image
-          source={hamburguer}
-          style={{ width: 25, height: 25, marginLeft: 5 }}
-        />
-      </TouchableOpacity>
-    </View>
-  );
-};
+const NavigationDrawer = ({ navigationProps }) => (
+  <View style={{ flexDirection: 'row' }}>
+    <TouchableOpacity onPress={navigationProps.toggleDrawer}>
+      <Image
+        source={hamburguer}
+        style={{ width: 25, height: 25, marginLeft: 5 }}
+      />
+    </TouchableOpacity>
+  </View>
+);
 
 export default NavigationDrawer;
