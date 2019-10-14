69e673de4fe3871284644be840eb04c7594c3276
ggdaltoso <ggdaltoso@gmail.com>
2019-10-14 11:14:24 -0300

    improvement(labnative): dynamic pages

--- packages/labnative/pages/Button.jsx
+++ packages/labnative/pages/Button.jsx
@@ -1,40 +1,8 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { ThemeProvider, Button } from '@gympass/design-system';
-
-import { createStackNavigator } from 'react-navigation-stack';
-import NavigationDrawer from '../components/NavigationDrawer';
-
-const styles = StyleSheet.create({
-  MainContainer: {
-    flex: 1,
-    paddingTop: 20,
-    alignItems: 'center',
-    marginTop: 50,
-    justifyContent: 'center',
-  },
-});
+import { Button } from '@gympass/design-system';
 
 const ButtonPage = () => {
-  return (
-    <ThemeProvider>
-      <View style={styles.MainContainer}>
-        <Button>Gympass Button</Button>
-      </View>
-    </ThemeProvider>
-  );
+  return <Button>Gympass Button</Button>;
 };
 
-const ButtonNavigation = createStackNavigator({
-  First: {
-    screen: ButtonPage,
-    navigationOptions: ({ navigation }) => ({
-      title: 'ButtonPage',
-      headerLeft: <NavigationDrawer navigationProps={navigation} />,
-      headerStyle: { backgroundColor: '#FF9800' },
-      headerTintColor: '#fff',
-    }),
-  },
-});
-
-export { ButtonPage as default, ButtonNavigation };
+export default ButtonPage;
