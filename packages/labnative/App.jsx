cf160f2b42863f17b2c2546ca64b09522fe41eb9
ggdaltoso <ggdaltoso@gmail.com>
2019-10-14 11:14:24 -0300

    improvement(labnative): dynamic pages

--- packages/labnative/App.jsx
+++ packages/labnative/App.jsx
@@ -1,23 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { StyleSheet, View, Picker, ScrollView } from 'react-native';
 import { createAppContainer } from 'react-navigation';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import {
+  createDrawerNavigator,
+  DrawerNavigatorItems,
+} from 'react-navigation-drawer';
 
-import { HomeNavigation } from './pages/Home';
-import { ButtonNavigation } from './pages/Button';
+import { createStackNavigator } from 'react-navigation-stack';
 
-const AppPages = createDrawerNavigator({
-  HomePage: {
-    screen: HomeNavigation,
-    navigationOptions: {
-      drawerLabel: 'HomePage',
-    },
+import * as Pages from './pages';
+import { ThemeProvider } from '@gympass/design-system';
+import NavigationDrawer from './components/NavigationDrawer';
+
+import { ThemeConsumer } from 'styled-components';
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  ButtonPage: {
-    screen: ButtonNavigation,
-    navigationOptions: {
-      drawerLabel: 'ButtonPage',
+});
+
+const CenteredView = props => {
+  const [themed, setThemed] = useState('endUser');
+  return (
+    <ThemeProvider theme={themed}>
+      <Picker
+        selectedValue={themed}
+        style={{ height: 50, width: '100%' }}
+        onValueChange={theme => setThemed(theme)}
+      >
+        <Picker.Item label="End User" value="endUser" />
+        <Picker.Item label="Corp" value="corp" />
+        <Picker.Item label="Gyms" value="gyms" />
+      </Picker>
+      <View style={styles.MainContainer} {...props} />
+    </ThemeProvider>
+  );
+};
+
+const drawerOptions = {};
+
+Object.entries(Pages).map(([name, Page]) => {
+  const navigator = createStackNavigator({
+    [name]: {
+      screen: () => (
+        <CenteredView>
+          <Page />
+        </CenteredView>
+      ),
+      navigationOptions: ({ navigation: { toggleDrawer } }) => ({
+        title: name,
+        headerLeft: <NavigationDrawer toggleDrawer={toggleDrawer} />,
+      }),
     },
-  },
+  });
+
+  drawerOptions[name] = {
+    screen: navigator,
+    navigationOptions: {
+      drawerLabel: name,
+    },
+  };
+});
+
+const AppPages = createDrawerNavigator(drawerOptions, {
+  contentComponent: props => (
+    <ThemeProvider>
+      <ThemeConsumer>
+        {theme => (
+          <DrawerNavigatorItems
+            {...props}
+            activeTintColor={theme.colors.primary}
+          />
+        )}
+      </ThemeConsumer>
+    </ThemeProvider>
+  ),
 });
 
 export default createAppContainer(AppPages);
--- packages/labnative/components/NavigationDrawer.jsx
+++ packages/labnative/components/NavigationDrawer.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { View, Image, TouchableOpacity } from 'react-native';
 import hamburguer from '../assets/images/drawer.png';
 
-const NavigationDrawer = ({ navigationProps }) => (
+const NavigationDrawer = ({ toggleDrawer }) => (
   <View style={{ flexDirection: 'row' }}>
-    <TouchableOpacity onPress={navigationProps.toggleDrawer}>
+    <TouchableOpacity onPress={toggleDrawer}>
       <Image
         source={hamburguer}
         style={{ width: 25, height: 25, marginLeft: 5 }}
