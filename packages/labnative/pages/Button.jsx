e9b94cf5d5132802c933e1a332716c0095516f8b
Allysson Santos <aallysson0@gmail.com>
2019-10-16 13:41:44 -0300

    feat(button): add native button

--- packages/labnative/pages/Button.jsx
+++ packages/labnative/pages/Button.jsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Button } from '@gympass/design-system';
 
-const ButtonPage = () => {
-  return <Button>Gympass Button</Button>;
-};
+const ScrollView = styled.ScrollView`
+  padding: 10px;
+  width: 100%;
+`;
+
+const ButtonWrapper = styled.View`
+  margin-bottom: 10px;
+`;
+
+const ButtonPage = () => (
+  <ScrollView>
+    <ButtonWrapper>
+      <Button small>Small Button</Button>
+    </ButtonWrapper>
+
+    <ButtonWrapper>
+      <Button>Contained Button</Button>
+    </ButtonWrapper>
+
+    <ButtonWrapper>
+      <Button disabled>Disabled Button</Button>
+    </ButtonWrapper>
+
+    <ButtonWrapper>
+      <Button outline>Outline Button</Button>
+    </ButtonWrapper>
+
+    <ButtonWrapper>
+      <Button outline disabled>
+        Disabled Outline Button
+      </Button>
+    </ButtonWrapper>
+
+    <ButtonWrapper>
+      <Button text>Text Button</Button>
+    </ButtonWrapper>
+
+    <ButtonWrapper>
+      <Button text disabled>
+        Disabled Text Button
+      </Button>
+    </ButtonWrapper>
+
+    <ButtonWrapper style={{ width: '100%' }}>
+      <Button full>Full Button</Button>
+    </ButtonWrapper>
+  </ScrollView>
+);
 
 export default ButtonPage;
