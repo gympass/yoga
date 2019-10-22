65308dbb3115dd1de6e226e7809d91446effb770
alanoliv <alan.oliveira@gympass.com>
2019-10-22 17:30:12 -0300

    feat(list): component structure

--- packages/design-system/src/index.js
+++ packages/design-system/src/index.js
@@ -1,4 +1,5 @@
 import ThemeProvider, { themes } from './ThemeProvider';
 import Button from './Button';
+import List from './List';
 
-export { ThemeProvider, themes, Button };
+export { ThemeProvider, themes, Button, List };
