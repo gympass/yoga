aa189e4a1fd99f7d5f634e0dbb8d947819b81dbf
Allysson Santos <aallysson0@gmail.com>
2019-10-23 08:55:33 -0300

    improvement(jest): change test files to component directory

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,15 @@
 const config = {
-  testMatch: ['<rootDir>/tests/**/*.test.jsx'],
+  testMatch: ['<rootDir>/packages/**/*.test.jsx'],
   transform: { '^.+\\.jsx?$': 'babel-jest' },
 };
 
 if (process.env.NODE_ENV === 'native') {
   config.preset = '@testing-library/react-native';
+  config.moduleNameMapper = {
+    'styled-components': require.resolve(
+      'styled-components/native/dist/styled-components.native.cjs',
+    ),
+  };
 }
 
 module.exports = config;
--- packages/design-system/src/ThemeProvider/index.native.js
+++
@@ -1,4 +0,0 @@
-import ThemeProvider from './ThemeProvider.native';
-import * as themes from './themes';
-
-export { ThemeProvider as default, themes };
