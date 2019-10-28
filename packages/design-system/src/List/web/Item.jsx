96e3f0cef029ed9f7c9dcea01dbf789fc94c1643
alanoliv <alan.oliveira@gympass.com>
2019-10-28 17:25:47 -0300

    refactor(list): improving components towards code-review

--- packages/design-system/src/List/web/Item.jsx
+++ packages/design-system/src/List/web/Item.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledItem = styled.li``;
+const StyledItem = styled.li`
+  ${({
+    theme: {
+      components: {
+        list: {
+          listItem: {
+            padding: {
+              top: paddingTop,
+              right: paddingRight,
+              bottom: paddingBottom,
+              left: paddingLeft,
+            },
+          },
+        },
+      },
+    },
+  }) => `
+      padding: ${paddingTop}px ${paddingRight}px ${paddingBottom}px ${paddingLeft}px;
+    }
+  }
+`}
+`;
 
 const Item = ({ ...rest }) => <StyledItem {...rest} />;
 
