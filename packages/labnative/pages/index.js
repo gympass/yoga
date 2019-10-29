2e5a16a9d8b3747eabe833cfdbc334eda821337b
Allysson Santos <aallysson0@gmail.com>
2019-10-29 16:59:44 -0300

    feat(slider): create new native component Slider

--- packages/labnative/pages/index.js
+++ packages/labnative/pages/index.js
@@ -1,5 +1,6 @@
 import Button from './Button';
 import List from './List';
 import Switch from './Switch';
+import Slider from './Slider';
 
-export { Button, Switch, List };
+export { Button, Switch, List, Slider };
--- packages/yoga/src/index.js
+++ packages/yoga/src/index.js
@@ -2,5 +2,6 @@ import ThemeProvider, { themes } from './ThemeProvider';
 import Button from './Button';
 import List from './List';
 import Checkbox from './Checkbox';
+import Slider from './Slider';
 
-export { ThemeProvider, themes, Button, List, Checkbox };
+export { ThemeProvider, themes, Button, List, Checkbox, Slider };
