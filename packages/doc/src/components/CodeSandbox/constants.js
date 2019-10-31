50db53079b3102d71f579bc90eca68cf86801a6b
alanoliv <alan.oliveira@gympass.com>
2019-10-30 23:20:04 -0300

    improvement(doc/codesandbox): component now receives a string as children

--- packages/doc/src/components/CodeSandbox/constants.js
+++ packages/doc/src/components/CodeSandbox/constants.js
@@ -1,11 +1,12 @@
 const HTML = '<div id="root"></div>';
+const URL = 'https://codesandbox.io/api/v1/sandboxes/define?json=1';
 
-const CODE = `import React from 'react';
+const getCode = component => `import React from 'react';
 import ReactDOM from 'react-dom';
 import {ThemeProvider, Button} from '@gympass/yoga';
 
 const App = () => <ThemeProvider>
-<Button>Gympass</Button>
+${component}
 </ThemeProvider>
 
 ReactDOM.render(
@@ -13,36 +14,35 @@ ReactDOM.render(
   document.getElementById('root')
 );`;
 
-const PACKAGE = {
-  files: {
-    'package.json': {
-      content: {
-        dependencies: {
-          react: 'latest',
-          'react-dom': 'latest',
-          '@gympass/yoga': 'latest',
-          'styled-components': 'latest',
+const getPackage = component =>
+  JSON.stringify({
+    files: {
+      'package.json': {
+        content: {
+          dependencies: {
+            react: 'latest',
+            'react-dom': 'latest',
+            '@gympass/yoga': 'latest',
+            'styled-components': 'latest',
+          },
         },
       },
+      'index.js': {
+        content: getCode(component),
+      },
+      'index.html': {
+        content: HTML,
+      },
     },
-    'index.js': {
-      content: CODE,
-    },
-    'index.html': {
-      content: HTML,
-    },
-  },
-};
+  });
 
-const URL = 'https://codesandbox.io/api/v1/sandboxes/define?json=1';
-
-const METHOD = {
+const setOptions = component => ({
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
   },
-  body: JSON.stringify(PACKAGE),
-};
+  body: getPackage(component),
+});
 
-export { URL, METHOD };
+export { URL, setOptions };
