457ad5ef150a11860679343624c7263a3d473d2f
alanoliv <alan.oliveira@gympass.com>
2019-11-04 10:50:18 -0300

    improvement(doc/navigation): themifying navigation

--- packages/doc/src/components/Navigation/Navigation.jsx
+++ packages/doc/src/components/Navigation/Navigation.jsx
@@ -2,84 +2,97 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 import { arrayOf, object } from 'prop-types';
-import tokens from '@gympass/yoga-tokens';
 
 import Arrow from '../../images/arrow-dropdown.svg';
-
 import createTree from './tree';
 
-const { colors } = tokens;
-
 const Wrapper = styled.div`
-  align-items: center;
-  background-color: ${colors.gray[0]};
-  box-shadow: inset -1px 0px 0px #f6f6f6;
-  display: flex;
-  flex-direction: column;
-  grid-area: Navigation;
-  height: 100%;
-  width: 320px;
+  ${({
+    theme: {
+      colors: { gray: grayPallete },
+    },
+  }) => `
+    align-items: center;
+    background-color: ${grayPallete[0]};
+    display: flex;
+    flex-direction: column;
+    grid-area: Navigation;
+    height: 100%;
+    width: 320px;
 
-  span {
-    color: #999;
-  }
+    span {
+      color: ${grayPallete[4]};
+    }
+  `};
 `;
 
 const Nav = styled.div`
-  width: 100%;
-  padding: 30px;
   height: 100vh;
+  padding: 30px;
+  width: 100%;
 `;
 
 const List = styled.ul`
-  padding: 0px;
-  list-style-type: none;
   font-size: 16px;
+  list-style-type: none;
+  padding: 0px;
   width: 100%;
 `;
 
 const AnchorLink = styled(Link)`
-  border-right: 1px solid transparent;
-  color: inherit;
-  display: block;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px 0px 10px 0px;
-  text-decoration: none;
-  text-indent: ${({ level }) => `calc(15px * ${level})`};
-  transition: all 0.3s;
-  width: 100%;
+  ${({
+    level,
+    theme: {
+      colors: { gray: grayPallete },
+    },
+  }) => `
+    color: ${grayPallete[4]};
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0px 10px 0px;
+    text-decoration: none;
+    text-indent: calc(15px * ${level});
+    transition: all 0.3s;
+    width: 100%;
+  `};
+`;
 
-  ${({ as }) =>
-    !as &&
-    `
-    &:hover {
-      color: #f46152;
+const ListItem = styled.li`
+  ${({
+    active,
+    theme: {
+      colors: {
+        primary: { length: len, [len - 1]: primaryColor },
+      },
+    },
+  }) => `
+    & > ${AnchorLink} {
+      ${
+        active
+          ? `
+          color: ${primaryColor};
+            `
+          : ''
+      }
     }
   `}
 `;
 
-const ListItem = styled.li`
-  & > ${AnchorLink} {
-    ${({ active }) =>
-      active &&
-      `
-      color: #F46152;
-  `}
-  }
-`;
-
 const Colapsible = styled.div`
-  cursor: pointer;
-  color: ${colors.gray[3]};
-
+  ${({
+    theme: {
+      colors: { gray: grayPallete },
+    },
+  }) => `
+    cursor: pointer;
+    color: ${grayPallete[3]};
+    svg {
+      width: 10px;
+      margin-left: 5px;
+    }
+  `};
   + ul {
     display: ${({ visible }) => (visible ? 'block' : 'none')};
-  }
-
-  svg {
-    width: 10px;
-    margin-left: 5px;
   }
 `;
 
