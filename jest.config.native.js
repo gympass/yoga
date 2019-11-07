3ec55c06843d17a8100a505d7eb0a611d7f46f3e
Allysson Santos <aallysson0@gmail.com>
2019-11-07 14:07:18 -0300

    test(yoga/jest): add testing library asserts and configure jest.setup to each platforms

--- jest.config.native.js
+++ jest.config.native.js
@@ -3,6 +3,7 @@ module.exports = {
   transform: { '^.+\\.jsx?$': 'babel-jest' },
   displayName: 'native',
   preset: '@testing-library/react-native',
+  setupFilesAfterEnv: ['./jest.setup.native.js'],
   moduleNameMapper: {
     'styled-components': require.resolve(
       'styled-components/native/dist/styled-components.native.cjs',
