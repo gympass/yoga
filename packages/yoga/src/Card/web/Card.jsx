b96182a1e22010f7028f6ca7f2aaff7b430e9194
victormath12 <victorsax12@gmail.com>
2019-11-08 12:55:30 -0300

    feat(card): creating Card Component for web

---
+++ packages/yoga/src/Card/web/Card.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { node } from 'prop-types';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  ${({
+    theme: {
+      components: {
+        card: { padding, background, radii, elevation },
+      },
+    },
+  }) => `
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+
+    width: 100%;
+    padding: ${padding}px;
+    border-radius: ${radii}px;
+    background-color: ${background.default};
+    box-shadow: ${elevation};
+  `}
+`;
+
+const Card = ({ children, ...rest }) => (
+  <CardContainer {...rest}>{children}</CardContainer>
+);
+
+Card.propTypes = {
+  children: node,
+};
+
+Card.defaultProps = {
+  children: 'Card',
+};
+
+export default Card;
