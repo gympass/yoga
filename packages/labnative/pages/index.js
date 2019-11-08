525250a859e45bc1c7350afbb961715f41cf0cca
victormath12 <victorsax12@gmail.com>
2019-11-08 12:57:49 -0300

    docs(card): creating labnative Card

--- packages/labnative/pages/index.js
+++ packages/labnative/pages/index.js
@@ -1,5 +1,6 @@
 import Button from './Button';
 import List from './List';
 import Switch from './Switch';
+import Card from './Card';
 
-export { Button, Switch, List };
+export { Button, Switch, List, Card };
