5be266275811edf659b9d5e4709f2b4f633997e5
Allysson Santos <aallysson0@gmail.com>
2019-11-19 11:17:58 -0300

    refactor(yoga/stepper): wrap step styled component in a functional component to work on docs

--- packages/yoga/src/Stepper/web/Step.jsx
+++ packages/yoga/src/Stepper/web/Step.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
+import { string } from 'prop-types';
 import styled from 'styled-components';
 
-const Step = styled.div`
+const Wrapper = styled.div`
   width: 100%;
 `;
+
+const Step = props => <Wrapper {...props} />;
+
+Step.propTypes = {
+  /** Label to be displayed below the stepper dot. */
+  label: string.isRequired,
+};
 
 Step.displayName = 'Stepper.Step';
 
