8879abce68872105e82531a618bd8fca495bcb10
Allysson Santos <aallysson0@gmail.com>
2019-11-19 10:09:12 -0300

    refactor(yoga/labnative): remove return from stepper page

--- packages/labnative/pages/Stepper.jsx
+++ packages/labnative/pages/Stepper.jsx
@@ -8,22 +8,20 @@ const StyledView = styled.View`
   padding: 10px;
 `;
 
-const StepperPage = () => {
-  return (
-    <StyledView>
-      <Stepper activeStep={0}>
-        <Stepper.Step label="Confirm Booking">
-          <Text>Confirm Booking Content</Text>
-        </Stepper.Step>
-        <Stepper.Step label="Class Booked">
-          <Text>Class Booked Content</Text>
-        </Stepper.Step>
-        <Stepper.Step label="Check-in">
-          <Text>Check-in Content</Text>
-        </Stepper.Step>
-      </Stepper>
-    </StyledView>
-  );
-};
+const StepperPage = () => (
+  <StyledView>
+    <Stepper activeStep={1}>
+      <Stepper.Step label="Confirm Booking">
+        <Text>Confirm Booking Content</Text>
+      </Stepper.Step>
+      <Stepper.Step label="Class Booked">
+        <Text>Class Booked Content</Text>
+      </Stepper.Step>
+      <Stepper.Step label="Check-in">
+        <Text>Check-in Content</Text>
+      </Stepper.Step>
+    </Stepper>
+  </StyledView>
+);
 
 export default StepperPage;
