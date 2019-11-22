39a71dd5624c26a0b7c110dee9fb7a987e211c34
ggdaltoso <ggdaltoso@gmail.com>
2019-11-22 16:31:03 -0300

    refactor(yoga/theme): new file structure for theme provider. Add Yoga prefix
    We add yoga prefix to our theme to avoid naming clashes. You could see more details
    [here](https://github.com/styled-components/styled-components-experimentation/blob/master/component-libraries/shared-component-libraries.md)
    
    BREAKING CHANGE: Adding a new prefix to our theme results in something like this:
    
    re #40

---
+++ packages/yoga/src/Theme/index.js
@@ -0,0 +1,4 @@
+import ThemeProvider from './Provider';
+import * as themes from './themes';
+
+export { ThemeProvider as default, themes };
---
+++ packages/yoga/src/Theme/themes/index.js
@@ -0,0 +1,5 @@
+import Corporate from './Corporate';
+import EndUser from './EndUser';
+import Gyms from './Gyms';
+
+export { EndUser as default, EndUser, Corporate, Gyms };
