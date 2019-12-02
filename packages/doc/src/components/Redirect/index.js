ee6b6721bdfa5b199d0254942564665b2e6f98e9
Allysson Santos <aallysson0@gmail.com>
2019-12-02 17:34:45 -0300

    chore(yoga/doc): add a initial slash in navigation links

--- packages/doc/src/components/Redirect/index.js
+++ packages/doc/src/components/Redirect/index.js
@@ -1,11 +1,16 @@
 import { useEffect } from 'react';
+import { string } from 'prop-types';
 import { navigate } from 'gatsby';
 
 const Redirect = ({ url }) => {
   useEffect(() => {
     navigate(url);
-  }, []);
+  });
   return null;
+};
+
+Redirect.propTypes = {
+  url: string.isRequired,
 };
 
 export default Redirect;
