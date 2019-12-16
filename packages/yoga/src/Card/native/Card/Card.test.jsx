4bddb024b128d436d997c28600c7289ebbb6497f
ggdaltoso <ggdaltoso@gmail.com>
2019-12-16 15:19:10 -0300

    test(plancard): tests for Card and PlanCard (Native)

--- packages/yoga/src/Card/native/Card/Card.test.jsx
+++ packages/yoga/src/Card/native/Card/Card.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import { Text } from 'react-native';
-import ThemeProvider from '../../../Theme';
+import { ThemeProvider, Button } from '../../..';
 import Card from '.';
 
 describe('<Card />', () => {
@@ -10,7 +10,15 @@ describe('<Card />', () => {
       const { container } = render(
         <ThemeProvider>
           <Card>
-            <Text>Hello World</Text>
+            <Card.Header>
+              <Text>Hello World</Text>
+            </Card.Header>
+            <Card.Content>
+              <Text>It&apos;s fine here</Text>
+            </Card.Content>
+            <Card.Actions>
+              <Button>Action&apos;s children must be Button</Button>
+            </Card.Actions>
           </Card>
         </ThemeProvider>,
       );
--- packages/yoga/src/Card/web/Card/Card.test.jsx
+++ packages/yoga/src/Card/web/Card/Card.test.jsx
@@ -19,34 +19,5 @@ describe('<Card />', () => {
       );
       expect(container).toMatchSnapshot();
     });
-
-    it('should match snapshot with variant Card', () => {
-      const { container } = render(
-        <ThemeProvider>
-          <Card variant="primary">
-            <Card.Header>Hello World</Card.Header>
-            <Card.Content>It&apos;s fine here</Card.Content>
-            <Card.Actions>
-              <Button>Action&apos;s children must be Button</Button>
-            </Card.Actions>
-          </Card>
-          <Card variant="secondary">
-            <Card.Header>Hello World</Card.Header>
-            <Card.Content>It&apos;s fine here</Card.Content>
-            <Card.Actions>
-              <Button>Action&apos;s children must be Button</Button>
-            </Card.Actions>
-          </Card>
-          <Card variant="tertiary">
-            <Card.Header>Hello World</Card.Header>
-            <Card.Content>It&apos;s fine here</Card.Content>
-            <Card.Actions>
-              <Button>Action&apos;s children must be Button</Button>
-            </Card.Actions>
-          </Card>
-        </ThemeProvider>,
-      );
-      expect(container).toMatchSnapshot();
-    });
   });
 });
