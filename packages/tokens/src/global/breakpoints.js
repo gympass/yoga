851d9d43bfe2827eb469ffb0c8b50c693387e1a3
Allysson Santos <aallysson0@gmail.com>
2019-12-18 17:55:10 -0300

    feat(yoga/tokens): add breakpoint definitions

--- packages/tokens/src/global/breakpoints.js
+++ packages/tokens/src/global/breakpoints.js
@@ -10,18 +10,60 @@
  * @typedef Breakpoint
  *
  * @type {Object}
- * @property {String} small
- * @property {String} medium
- * @property {String} large
+ * @property {Object} xxsmall
+ * @property {Object} xsmall
+ * @property {Object} small
+ * @property {Object} medium
+ * @property {Object} large
+ * @property {Object} xlarge
+ * @property {Object} xxlarge
+ * @property {Object} xxxlarge
  */
 
 /**
  * @type {Breakpoint}
  */
 const breakpoints = {
-  small: {},
-  medium: {},
-  large: {},
+  xxsmall: {
+    width: 0,
+    margin: 20,
+    gutter: 16,
+  },
+  xsmall: {
+    width: 360,
+    margin: 20,
+    gutter: 16,
+  },
+  small: {
+    width: 480,
+    margin: 20,
+    gutter: 16,
+  },
+  medium: {
+    width: 768,
+    margin: 20,
+    gutter: 16,
+  },
+  large: {
+    width: 1024,
+    margin: 71,
+    gutter: 24,
+  },
+  xlarge: {
+    width: 1200,
+    margin: 71,
+    gutter: 24,
+  },
+  xxlarge: {
+    width: 1366,
+    margin: 71,
+    gutter: 24,
+  },
+  xxxlarge: {
+    width: 1600,
+    margin: 71,
+    gutter: 24,
+  },
 };
 
 export default breakpoints;
