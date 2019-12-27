6f8c0116bedad9c780e392e8c33c81fcb9c69848
Allysson Santos <aallysson0@gmail.com>
2019-12-27 14:45:24 -0300

    improvement(yoga/grid): add start keyword to the Hide component

--- packages/yoga/src/Grid/web/hideQuery.js
+++ packages/yoga/src/Grid/web/hideQuery.js
@@ -1,6 +1,7 @@
 import { css } from 'styled-components';
 
 const hideQuery = ({
+  xxs: { width: xxsWidth } = {},
   xs: { width: xsWidth } = {},
   sm: { width: smWidth } = {},
   md: { width: mdWidth } = {},
@@ -14,8 +15,18 @@ const hideQuery = ({
       display: none !important;
     }
   `,
+  'xxs-start': css`
+    @media (min-width: ${xxsWidth}px) {
+      display: none !important;
+    }
+  `,
   xs: css`
     @media (min-width: ${xsWidth}px) and (max-width: ${smWidth - 1}px) {
+      display: none !important;
+    }
+  `,
+  'xs-start': css`
+    @media (min-width: ${xsWidth}px) {
       display: none !important;
     }
   `,
@@ -24,8 +35,18 @@ const hideQuery = ({
       display: none !important;
     }
   `,
+  'sm-start': css`
+    @media (min-width: ${smWidth}px) {
+      display: none !important;
+    }
+  `,
   md: css`
     @media (min-width: ${mdWidth}px) and (max-width: ${lgWidth - 1}px) {
+      display: none !important;
+    }
+  `,
+  'md-start': css`
+    @media (min-width: ${mdWidth}px) {
       display: none !important;
     }
   `,
@@ -34,12 +55,27 @@ const hideQuery = ({
       display: none !important;
     }
   `,
+  'lg-start': css`
+    @media (min-width: ${lgWidth}px) {
+      display: none !important;
+    }
+  `,
   xl: css`
     @media (min-width: ${xlWidth}px) and (max-width: ${xxlWidth - 1}px) {
       display: none !important;
     }
   `,
+  'xl-start': css`
+    @media (min-width: ${xlWidth}px) {
+      display: none !important;
+    }
+  `,
   xxl: css`
+    @media (min-width: ${xxlWidth}px) and (max-width: ${xxxlWidth - 1}px) {
+      display: none !important;
+    }
+  `,
+  'xxl-start': css`
     @media (min-width: ${xxlWidth}px) and (max-width: ${xxxlWidth - 1}px) {
       display: none !important;
     }
