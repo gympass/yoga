3d01a0d04aee1d5ab40a5a5f382f0b4d0fa654bb
Allysson Santos <aallysson0@gmail.com>
2020-01-07 18:25:01 -0300

    improvement(component-cli): change index files to add new component name

--- scripts/component/component.js
+++ scripts/component/component.js
@@ -1,11 +1,23 @@
-const { indexTemplate, componentTemplate } = require('./templates/component');
+const {
+  rootIndexTemplate,
+  indexTemplate,
+  componentTemplate,
+} = require('./templates/component');
 const { createDirectory, createFile } = require('./factory');
 
-const component = (name, { web, native }) => {
+const component = (name, { base, web, native }) => {
   createDirectory([web, native]);
 
+  createFile(`${web}/index.js`, indexTemplate(name));
   createFile(`${web}/${name}.jsx`, componentTemplate(name));
+  createFile(`${native}/index.js`, indexTemplate(name));
   createFile(`${native}/${name}.jsx`, componentTemplate(name, 'native'));
+
+  createFile(`${base}/${name}/index.js`, rootIndexTemplate(name, 'web'));
+  createFile(
+    `${base}/${name}/index.native.js`,
+    rootIndexTemplate(name, 'native'),
+  );
 };
 
 module.exports = component;
