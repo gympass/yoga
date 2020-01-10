44d062c8d7d71f52987083794115f7d594be637d
ggdaltoso <ggdaltoso@gmail.com>
2020-01-10 16:27:36 -0300

    feat(svg): aDD SVG SUPPORT IN REACT NATIVE

--- packages/yoga/src/Rating/native/Rating.jsx
+++ packages/yoga/src/Rating/native/Rating.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-// import Star from '../star.svg';
+import Star from '../star.svg';
 
 const Rating = styled.View``;
 
 export default () => {
   const [size, setSize] = useState(12);
 
-  return <Rating>{/* <Star width={size} height={size} /> */}</Rating>;
+  return (
+    <Rating>
+      <Star width={size} height={size} />
+    </Rating>
+  );
 };
