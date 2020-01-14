4f23846471d914f1e63ae4504ea397211f07e433
ggdaltoso <ggdaltoso@gmail.com>
2020-01-14 16:20:05 -0300

    docs(rating): improve rating documentation

--- packages/labnative/pages/Rating.jsx
+++ packages/labnative/pages/Rating.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Rating } from '@gympass/yoga';
+import Svg, { Circle } from 'react-native-svg/lib/commonjs';
 
 const ScrollView = styled.ScrollView`
   margin-bottom: 50px;
@@ -15,8 +16,20 @@ const StyledText = styled.Text`
   text-align: center;
 `;
 
+const CircleIcon = props => (
+  <Svg {...props}>
+    <Circle cx={6} cy={7} r={5} />
+  </Svg>
+);
+
 const ButtonPage = () => (
-  <ScrollView>
+  <ScrollView
+    style={{ width: '100%' }}
+    contentContainerStyle={{
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
     <StyledText>Rating</StyledText>
 
     <Rating value={1} />
@@ -27,6 +40,17 @@ const ButtonPage = () => (
     <Rating value={3.5} />
     <Rating value={4} />
     <Rating value={4.5} />
+
+    <StyledText>Custom Icon</StyledText>
+
+    <Rating icon={CircleIcon} value={1} />
+    <Rating icon={CircleIcon} value={1.5} />
+    <Rating icon={CircleIcon} value={2} />
+    <Rating icon={CircleIcon} value={2.5} />
+    <Rating icon={CircleIcon} value={3} />
+    <Rating icon={CircleIcon} value={3.5} />
+    <Rating icon={CircleIcon} value={4} />
+    <Rating icon={CircleIcon} value={4.5} />
   </ScrollView>
 );
 
