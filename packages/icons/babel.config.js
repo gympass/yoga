ec74caead273a11e02391d8176591b75522e6f93
Allysson Santos <aallysson0@gmail.com>
2020-01-14 10:11:06 -0300

    feat(icons): create icons package

---
+++ packages/icons/babel.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    web: {
+      presets: [['@babel/preset-env']],
+      ignore: ['**/*.native.js'],
+      plugins: ['inline-react-svg'],
+    },
+
+    esm: {
+      presets: [['@babel/preset-env', { modules: false }]],
+      ignore: ['**/*.native.js'],
+      plugins: ['inline-react-svg'],
+    },
+
+    native: {
+      only: ['**/*.native.js'],
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      plugins: [
+        [
+          'transform-rename-import',
+          { original: '^(.+?)\\.svg$', replacement: '$1.js' },
+        ],
+      ],
+    },
+
+    svg: {
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        [
+          '@dr.pogodin/babel-preset-svgr',
+          {
+            svgr: {
+              native: true,
+            },
+          },
+        ],
+      ],
+    },
+  },
+};
---
+++ packages/icons/src/svg/index.js
@@ -0,0 +1,4 @@
+import Heart from './heart.svg';
+import Star from './star.svg';
+
+export { Star, Heart };
