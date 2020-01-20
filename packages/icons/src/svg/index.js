f60a663bd6ab74d6c146ac8331fe055764036077
ggdaltoso <ggdaltoso@gmail.com>
2020-01-20 15:58:12 -0300

    feat(icons): new Icon: triangle alert

--- packages/icons/src/svg/index.js
+++ packages/icons/src/svg/index.js
@@ -1,5 +1,6 @@
 import Star from './star.svg';
 import Clock from './clock.svg';
 import Building from './building.svg';
+import TriangleAlert from './triangle_alert.svg';
 
-export { Star, Clock, Building };
+export { Star, Clock, Building, TriangleAlert };
