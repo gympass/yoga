7bbb45ce282e562d97f3a3134fa8db01b6600e8e
ggdaltoso <ggdaltoso@gmail.com>
2020-01-21 09:27:07 -0300

    feat(tag): new component: Tag

---
+++ packages/yoga/src/Tag/native/Tag.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTag = styled.Text``;
+
+const Tag = props => <StyledTag {...props} />;
+
+Tag.propTypes = {};
+Tag.defaultProps = {};
+
+export default Tag;
---
+++ packages/yoga/src/Tag/native/Tag.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { ThemeProvider, Tag } from '../..';
+
+describe('<Tag />', () => {
+  it('should match snapshot', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Tag />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
