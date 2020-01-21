fab0793e996402499f0864dbac7976efa641cdb6
ggdaltoso <ggdaltoso@gmail.com>
2020-01-21 14:27:34 -0300

    docs(tag): remove scrollview from labnative

--- packages/labnative/pages/Tag.jsx
+++ packages/labnative/pages/Tag.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { Tag } from '@gympass/yoga';
 import { Building } from '@gympass/yoga-icons';
-import { ScrollView, View } from 'react-native';
 
 const TagWrapper = styled.View`
   width: 100%;
   margin-bottom: 10px;
   align-items: center;
   justify-content: space-between;
-  height: 180px;
+  height: 250px;
 `;
 
 const TagPage = () => (
@@ -17,38 +16,9 @@ const TagPage = () => (
     <Tag>default</Tag>
     <Tag icon={Building}>default with custom icon</Tag>
     <Tag icon={false}>default without icon</Tag>
-    <View
-      style={{
-        height: 30,
-      }}
-    >
-      <ScrollView horizontal>
-        <Tag
-          style={{
-            marginRight: 16,
-          }}
-          variant="informative"
-        >
-          informative
-        </Tag>
-        <Tag
-          style={{
-            marginRight: 16,
-          }}
-          variant="positive"
-        >
-          positive
-        </Tag>
-        <Tag
-          style={{
-            marginRight: 16,
-          }}
-          variant="negative"
-        >
-          negative
-        </Tag>
-      </ScrollView>
-    </View>
+    <Tag variant="informative">informative</Tag>
+    <Tag variant="positive">positive</Tag>
+    <Tag variant="negative">negative</Tag>
     <Tag full variant="warning">
       No spots left
     </Tag>
