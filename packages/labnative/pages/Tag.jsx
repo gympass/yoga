2718a5766daa0c39850636e95890695a5dd7bf54
ggdaltoso <ggdaltoso@gmail.com>
2020-01-21 13:16:49 -0300

    docs(tag): improve labnative doc

--- packages/labnative/pages/Tag.jsx
+++ packages/labnative/pages/Tag.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 import { Tag } from '@gympass/yoga';
 import { Building } from '@gympass/yoga-icons';
+import { ScrollView, View } from 'react-native';
 
 const TagWrapper = styled.View`
   width: 100%;
   margin-bottom: 10px;
   align-items: center;
   justify-content: space-between;
-  height: 250px;
+  height: 180px;
 `;
 
 const TagPage = () => (
@@ -16,9 +17,38 @@ const TagPage = () => (
     <Tag>default</Tag>
     <Tag icon={Building}>default with custom icon</Tag>
     <Tag icon={false}>default without icon</Tag>
-    <Tag variant="informative">informative</Tag>
-    <Tag variant="positive">positive</Tag>
-    <Tag variant="negative">negative</Tag>
+    <View
+      style={{
+        height: 30,
+      }}
+    >
+      <ScrollView horizontal>
+        <Tag
+          style={{
+            marginRight: 16,
+          }}
+          variant="informative"
+        >
+          informative
+        </Tag>
+        <Tag
+          style={{
+            marginRight: 16,
+          }}
+          variant="positive"
+        >
+          positive
+        </Tag>
+        <Tag
+          style={{
+            marginRight: 16,
+          }}
+          variant="negative"
+        >
+          negative
+        </Tag>
+      </ScrollView>
+    </View>
     <Tag full variant="warning">
       No spots left
     </Tag>
