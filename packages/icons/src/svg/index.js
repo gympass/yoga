adb442d1181e06888d5b95618c8ab99a2d864c83
Allysson Santos <aallysson0@gmail.com>
2020-01-29 14:23:35 -0300

    improvement(icons): add visibility and close icons with 24px shape

--- packages/icons/src/svg/index.js
+++ packages/icons/src/svg/index.js
@@ -2,7 +2,16 @@ import Star from './star.svg';
 import Clock from './clock.svg';
 import Building from './building.svg';
 import TriangleAlert from './triangle_alert.svg';
+import Close from './close.svg';
 import Visibility from './visibility.svg';
 import VisibilityOff from './Visibility_off.svg';
 
-export { Star, Clock, Building, TriangleAlert, Visibility, VisibilityOff };
+export {
+  Star,
+  Clock,
+  Building,
+  TriangleAlert,
+  Close,
+  Visibility,
+  VisibilityOff,
+};
