2047d911652b93b68dc7cca7a83e3fbdaa53e3de
alanoliv <alan.oliveira@gympass.com>
2020-01-29 20:20:45 -0300

    improvement(doc): smarter import injection function

--- packages/doc/src/components/CodeBlock/shared/functions.js
+++ packages/doc/src/components/CodeBlock/shared/functions.js
@@ -1,12 +1,23 @@
-const injectImport = (imports, component) => {
-  const index = imports.indexOf(` } from '@gympass/yoga'`);
-  const importsWithComponent = [
-    imports.slice(0, index),
-    `, ${component}`,
-    imports.slice(index),
-  ].join('');
+const injectImport = (imports, componentList, paths) => {
+  let injectedString = imports;
 
-  return importsWithComponent;
+  paths.forEach((path, index) => {
+    const components = componentList[index].split(', ');
+    const stringPosition = injectedString.indexOf(` } from '${path}'`);
+
+    components.forEach(component => {
+      const findComponent = new RegExp(component, 'gm');
+      if (!injectedString.match(findComponent)) {
+        injectedString = [
+          injectedString.slice(0, stringPosition),
+          `, ${component}`,
+          injectedString.slice(stringPosition),
+        ].join('');
+      }
+    });
+  });
+
+  return injectedString;
 };
 
 export default injectImport;
