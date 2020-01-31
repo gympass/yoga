91a3bfd9237349daaf268f463846f85b57996149
Allysson Santos <aallysson0@gmail.com>
2020-01-31 11:17:21 -0300

    improvement(input): add keyboard support to clean and password features and full prop

--- packages/yoga/src/Input/web/Password.jsx
+++ packages/yoga/src/Input/web/Password.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
+import { bool } from 'prop-types';
 import { Visibility, VisibilityOff } from '@gympass/yoga-icons';
 
 import Input from './Input';
@@ -11,21 +12,57 @@ const InlineBlock = styled.div`
 const Wrapper = styled.div`
   position: relative;
   display: inline-block;
+`;
 
+const IconWrapper = styled.div`
   ${({
+    disabled,
     theme: {
-      yoga: { spacing },
+      yoga: {
+        colors,
+        spacing,
+        components: { input },
+      },
     },
   }) => `
+    position: absolute;
+    top: ${spacing.xsmall}px;
+    right: ${spacing.xsmall}px;
+
+    padding-right: ${spacing.xsmall}px;
+    padding-left: ${spacing.xsmall}px;
+
+    height: ${input.height}px;
+    cursor: pointer;
+
+    ${
+      disabled
+        ? `
+      cursor: not-allowed;
+      pointer-events: none;
+    `
+        : ''
+    }
+
     svg {
-      top: ${spacing.xsmall}px;
-      right: ${spacing.xsmall}px;
-      height: calc(100% - ${spacing.xsmall * 2}px);
-    }  
+      height: ${input.height}px;
+
+      fill: ${input.font.color.default};
+
+      box-sizing: content-box;
+
+      ${
+        disabled
+          ? `
+            fill: ${colors.disabled.background};
+            pointer-events: none;`
+          : ''
+      }  
+    }
   `}
 `;
 
-const Password = props => {
+const Password = ({ disabled, ...props }) => {
   const [showPassword, toggleShowPassword] = useState(false);
   const inputRef = useRef(null);
 
@@ -35,9 +72,14 @@ const Password = props => {
     element.setSelectionRange(element.value.length, element.value.length);
   }, [showPassword]);
 
-  const onClick = element => {
-    element.focus();
-    toggleShowPassword(!showPassword);
+  const togglePassword = e => {
+    if (e.type === 'click' || e.key === 'Enter' || e.key === ' ') {
+      toggleShowPassword(!showPassword);
+    }
+
+    if (e.type === 'click') {
+      inputRef.current.focus();
+    }
   };
 
   return (
@@ -45,19 +87,31 @@ const Password = props => {
       <Wrapper>
         <Input
           {...props}
+          disabled={disabled}
           ref={inputRef}
           cleanable={false}
           type={showPassword ? 'text' : 'password'}
         />
 
-        {showPassword ? (
-          <Visibility onClick={() => onClick(inputRef.current)} />
-        ) : (
-          <VisibilityOff onClick={() => onClick(inputRef.current)} />
-        )}
+        <IconWrapper
+          tabIndex={disabled ? null : 0}
+          onClick={togglePassword}
+          onKeyDown={togglePassword}
+          disabled={disabled}
+        >
+          {showPassword ? <Visibility /> : <VisibilityOff />}
+        </IconWrapper>
       </Wrapper>
     </InlineBlock>
   );
+};
+
+Password.propTypes = {
+  disabled: bool,
+};
+
+Password.defaultProps = {
+  disabled: false,
 };
 
 export default Password;
