ad555901fdbb544d57b4bdbb58a53eed7eee651c
Allysson Santos <aallysson0@gmail.com>
2020-02-06 14:16:03 -0300

    refactor(input): remove variant prop

--- packages/yoga/src/Input/web/index.js
+++ packages/yoga/src/Input/web/index.js
@@ -1,4 +1,7 @@
 import Input from './Input';
 import Password from './Password';
+import InputNumber from './Number';
+import Tel from './Tel';
+import Email from './Email';
 
-export { Input, Password };
+export { Input, Password, InputNumber, Tel, Email };
