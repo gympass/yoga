3c4efe67d74767d38b79d5d62ccdb00e6c088a63
Allysson Santos <aallysson0@gmail.com>
2020-02-10 17:57:33 -0300

    test(input): create snapshot tests to native input

--- packages/yoga/src/Input/native/Input.test.jsx
+++ packages/yoga/src/Input/native/Input.test.jsx
@@ -1,16 +1,70 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 
 import { ThemeProvider, Input } from '../..';
 
 describe('<Input />', () => {
-  it('should match snapshot', () => {
-    const { container } = render(
-      <ThemeProvider>
-        <Input />
-      </ThemeProvider>,
-    );
+  describe('Snapshots', () => {
+    it('should match with default input', () => {
+      const { container } = render(
+        <ThemeProvider>
+          <Input />
+        </ThemeProvider>,
+      );
 
-    expect(container).toMatchSnapshot();
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match with label', () => {
+      const { container } = render(
+        <ThemeProvider>
+          <Input label="Input" />
+        </ThemeProvider>,
+      );
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot when input is focused', () => {
+      const { container, getByTestId } = render(
+        <ThemeProvider>
+          <Input label="Input" testID="input" />
+        </ThemeProvider>,
+      );
+
+      fireEvent.focus(getByTestId('input'));
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match with disabled input', () => {
+      const { container } = render(
+        <ThemeProvider>
+          <Input disabled />
+        </ThemeProvider>,
+      );
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match with error', () => {
+      const { container } = render(
+        <ThemeProvider>
+          <Input error="Error message" />
+        </ThemeProvider>,
+      );
+
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should match with helper text and max length', () => {
+      const { container } = render(
+        <ThemeProvider>
+          <Input helper="Helper text" maxLength={20} />
+        </ThemeProvider>,
+      );
+
+      expect(container).toMatchSnapshot();
+    });
   });
 });
