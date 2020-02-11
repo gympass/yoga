eb2aaa50914968c6c552088fd0a92882b68391e3
ggdaltoso <ggdaltoso@gmail.com>
2020-02-11 10:20:18 -0300

    docs(progress): remove pointless examples

--- packages/labnative/pages/Progress.jsx
+++ packages/labnative/pages/Progress.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 import { Progress } from '@gympass/yoga';
 
-import { DocTitle } from '../components';
-
 const ProgressPage = () => (
   <ScrollView
     style={{
@@ -12,31 +10,19 @@ const ProgressPage = () => (
       padding: 80,
     }}
   >
-    <DocTitle>Default</DocTitle>
-    <Progress max={100} value={35} />
-
-    <DocTitle>Numeric value on the left (default)</DocTitle>
-    <Progress max={100} value={20} label={{ value: 20 }} />
-    <Progress max={100} value={40} label={{ value: 40 }} />
-    <Progress max={100} value={67} label={{ value: 67 }} />
-
-    <DocTitle>Numeric value on the right</DocTitle>
+    <Progress max={100} value={20} label={{ value: 20, placement: 'left' }} />
+    <Progress max={100} value={40} label={{ value: 40, placement: 'left' }} />
     <Progress
       max={100}
-      value={100}
-      label={{ value: 100, placement: 'right' }}
+      value={67}
+      label={{ value: 67, placement: 'left' }}
+      style={{ marginBottom: 10 }}
     />
-    <Progress max={100} value={65} label={{ value: 65, placement: 'right' }} />
-    <Progress max={100} value={12} label={{ value: 12, placement: 'right' }} />
 
-    <DocTitle>Alphabetic value on the left (default)</DocTitle>
-    <Progress max={100} value={30} label={{ value: 'Some description' }} />
-
-    <DocTitle>Alphabetic value on the right (default)</DocTitle>
     <Progress
       max={100}
       value={70}
-      label={{ value: 'Some description', placement: 'right' }}
+      label={{ value: 'Some description', placement: 'left' }}
     />
   </ScrollView>
 );
