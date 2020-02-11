ad5a09b00c57ae9fbb6be352830c65aea1e60ccc
ggdaltoso <ggdaltoso@gmail.com>
2020-02-11 11:07:31 -0300

    refactor(theme): break base theme into small pieces

---
+++ packages/yoga/src/Theme/themes/components/grid.js
@@ -0,0 +1,21 @@
+const grid = () => ({
+  gutter: {
+    desktop: 24,
+    mobile: 16,
+  },
+  container: {
+    width: 1600,
+    margin: {
+      desktop: {
+        left: 71,
+        right: 71,
+      },
+      mobile: {
+        left: 20,
+        right: 20,
+      },
+    },
+  },
+});
+
+export default grid;
