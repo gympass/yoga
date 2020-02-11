2c1d9638f50e0653a15513bf9df512653f46ec57
ggdaltoso <ggdaltoso@gmail.com>
2020-02-11 10:00:43 -0300

    test(progress): add unit tests (native)

--- packages/yoga/src/Progress/native/Progress.test.jsx
+++ packages/yoga/src/Progress/native/Progress.test.jsx
@@ -8,6 +8,38 @@ describe('<Progress />', () => {
     const { container } = render(
       <ThemeProvider>
         <Progress />
+        <Progress max={100} />
+        <Progress max={100} value={60} />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot with label string', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Progress max={100} label={{ value: 'Some decription here' }} />
+        <Progress
+          max={100}
+          value={65}
+          label={{ value: 'Some decription here', placement: 'right' }}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot with label number', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Progress max={100} value={20} label={{ value: 20 }} />
+        <Progress
+          max={100}
+          value={65}
+          label={{ value: 65, placement: 'right' }}
+        />
       </ThemeProvider>,
     );
 
