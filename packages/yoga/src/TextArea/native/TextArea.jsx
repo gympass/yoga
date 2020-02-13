e3970c1361ed235cc5b3f999e51a0a7b3cd98bb0
Allysson Santos <aallysson0@gmail.com>
2020-02-13 17:14:25 -0300

    fix(input): fix icons padding

--- packages/yoga/src/TextArea/native/TextArea.jsx
+++ packages/yoga/src/TextArea/native/TextArea.jsx
@@ -8,12 +8,18 @@ const StyledInput = styled(Input)`
   height: 88px;
 `;
 
+/**
+ * TextArea component represents a multi-line plain-text editing control,
+ * useful when you want to allow users to enter a sizeable amount of free-form
+ * text, for example a comment on a review or feedback form.
+ */
 const TextArea = props => (
   <StyledInput {...props} textAlignVertical="top" multiline cleanable={false} />
 );
 
 TextArea.propTypes = {
   disabled: bool,
+  /** A error text to be displayed below field */
   error: string,
   full: bool,
   /** A helper text to be displayed below field */
@@ -25,7 +31,6 @@ TextArea.propTypes = {
   value: string,
   onBlur: func,
   onChangeText: func,
-  /** callback invoked when close icon is clicked */
   onFocus: func,
 };
 
