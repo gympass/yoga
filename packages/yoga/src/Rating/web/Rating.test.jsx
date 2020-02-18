aef33918d7fb509e68bac8e21768d80b9d0b6bf1
ggdaltoso <ggdaltoso@gmail.com>
2020-02-18 11:13:24 -0300

    test(rating): add tests for rating interaction

--- packages/yoga/src/Rating/web/Rating.test.jsx
+++ packages/yoga/src/Rating/web/Rating.test.jsx
@@ -1,9 +1,31 @@
+/* eslint-disable jsx-a11y/mouse-events-have-key-events */
+
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { ThemeProvider } from '../..';
 import Rating from '.';
 
 describe('<Rating />', () => {
+  const originalOffsetWidth = Object.getOwnPropertyDescriptor(
+    HTMLElement.prototype,
+    'offsetWidth',
+  );
+
+  beforeAll(() => {
+    Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
+      configurable: true,
+      value: 76, // 5 stars (5 * 12px + 4 * 4px gutter)
+    });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(
+      HTMLElement.prototype,
+      'offsetWidth',
+      originalOffsetWidth,
+    );
+  });
+
   describe('Snapshots', () => {
     it('should match snapshot with Rating', () => {
       const { container } = render(
@@ -60,18 +82,91 @@ describe('<Rating />', () => {
     it('should match snapshot with different icon', () => {
       const Circle = props => (
         <svg {...props}>
-          <circle cx="6" cy="6" r="6" />
+          <circle cx="12" cy="12" r="12" />
         </svg>
       );
 
       const { container } = render(
         <ThemeProvider>
-          <Rating icon={Circle} value={0} />
-          <Rating icon={Circle} value={1} />
-          <Rating icon={Circle} value={2} />
+          <Rating icon={{ type: Circle }} value={0} />
+          <Rating icon={{ type: Circle }} value={1} />
+          <Rating icon={{ type: Circle }} value={2} />
         </ThemeProvider>,
       );
       expect(container).toMatchSnapshot();
+    });
+
+    it('should match snapshot when mouse over', () => {
+      const { container } = render(
+        <ThemeProvider>
+          <Rating readOnly={false} />
+        </ThemeProvider>,
+      );
+
+      const { firstChild } = container;
+
+      fireEvent.mouseMove(firstChild, {
+        /** Each star has 12px. Gutter is 4px.
+         * For the third star, we should set clentX > 32px and < 48px */
+        clientX: 40,
+      });
+
+      expect(container).toMatchSnapshot();
+    });
+  });
+
+  describe('Events', () => {
+    it('should call onRate when clicking', () => {
+      const onRateMock = jest.fn();
+
+      const { container } = render(
+        <ThemeProvider>
+          <Rating onRate={onRateMock} readOnly={false} />
+        </ThemeProvider>,
+      );
+
+      const { firstChild } = container;
+
+      fireEvent.mouseMove(firstChild, {
+        clientX: 40, // third star position
+      });
+
+      fireEvent.click(firstChild);
+
+      expect(onRateMock).toHaveBeenCalled();
+      expect(onRateMock).toHaveBeenCalledWith(3);
+    });
+
+    it('should call onMouseOver callback', () => {
+      const onMouseOverMock = jest.fn();
+
+      const { container } = render(
+        <ThemeProvider>
+          <Rating onMouseOver={onMouseOverMock} readOnly={false} />
+        </ThemeProvider>,
+      );
+
+      const { firstChild } = container;
+
+      fireEvent.mouseOver(firstChild);
+
+      expect(onMouseOverMock).toHaveBeenCalled();
+    });
+
+    it('should call onMouseLeave callback', () => {
+      const onMouseLeaveMock = jest.fn();
+
+      const { container } = render(
+        <ThemeProvider>
+          <Rating onMouseLeave={onMouseLeaveMock} readOnly={false} />
+        </ThemeProvider>,
+      );
+
+      const { firstChild } = container;
+
+      fireEvent.mouseLeave(firstChild);
+
+      expect(onMouseLeaveMock).toHaveBeenCalled();
     });
   });
 });
