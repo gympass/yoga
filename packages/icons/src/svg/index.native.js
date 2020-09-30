b16c129ef7dca8f0975992bba6aa409a6e302bb0
Allysson Santos <aallysson0@gmail.com>
2020-09-30 18:14:05 -0300

    feat(icons): add all gympass icons

--- packages/icons/src/svg/index.native.js
+++ packages/icons/src/svg/index.native.js
@@ -1,27 +1,277 @@
+import Accessibility from './accessibility.svg';
+import Accessible from './accessible.svg';
+import AddBooking from './add_booking.svg';
+import Add from './add.svg';
+import AlertCircle from './alert_circle.svg';
+import AlertTriangle from './alert_triangle.svg';
 import ArrowDown from './arrow_down.svg';
+import ArrowDropDown from './arrow_drop_down.svg';
+import ArrowDropUp from './arrow_drop_up.svg';
+import ArrowLeft from './arrow_left.svg';
+import ArrowRight from './arrow_right.svg';
+import BankStatement from './bank_statement.svg';
+import BarChart from './bar_chart.svg';
+import BookingFilled from './booking_filled.svg';
+import Booking from './booking.svg';
+import BuildingFilled from './building_filled.svg';
 import Building from './building.svg';
+import BulletList from './bullet_list.svg';
+import Camera from './camera.svg';
+import Cart from './cart.svg';
+import Chat from './chat.svg';
+import CheckFilled from './check_filled.svg';
+import Check from './check.svg';
+import CheckedFull from './checked_full.svg';
+import CheckedHalf from './checked_half.svg';
+import CheckinFilled from './checkin_filled.svg';
+import Checkin from './checkin.svg';
+import ChevronDown from './chevron_down.svg';
+import ChevronLeft from './chevron_left.svg';
+import ChevronRight from './chevron_right.svg';
+import ChevronUp from './chevron_up.svg';
 import Clock from './clock.svg';
+import CloseFilled from './close_filled.svg';
 import Close from './close.svg';
+import CompassFilled from './compass_filled.svg';
+import Compass from './compass.svg';
+import Copy from './copy.svg';
+import CornerUpLeft from './corner_up_left.svg';
+import CornerUpRight from './corner_up_right.svg';
+import CreditCard from './credit_card.svg';
+import Disconnect from './disconnect.svg';
+import DislikeFilled from './dislike_filled.svg';
+import Dislike from './dislike.svg';
+import Doc from './doc.svg';
 import Done from './done.svg';
+import Download from './download.svg';
+import Draggable from './draggable.svg';
+import Dumbbell from './dumbbell.svg';
+import Edit from './edit.svg';
+import Exit from './exit.svg';
+import Facebook from './facebook.svg';
+import FamilyMember from './family_member.svg';
+import FavoriteFilled from './favorite_filled.svg';
+import Favorite from './favorite.svg';
 import Filter from './filter.svg';
+import Google from './google.svg';
+import Headphone from './headphone.svg';
+import Heartbeat from './heartbeat.svg';
+import HelpCircle from './help_circle.svg';
+import Help from './help.svg';
+import History from './history.svg';
+import HomeFilled from './home_filled.svg';
+import Home from './home.svg';
+import Image from './image.svg';
+import Info from './info.svg';
+import Instagram from './instagram.svg';
+import JumpRope from './jump_rope.svg';
+import LikeFilled from './like_filled.svg';
+import Like from './like.svg';
+import Link from './link.svg';
+import Linkedin from './linkedin.svg';
+import Location from './location.svg';
+import LockOff from './lock_off.svg';
+import LockOn from './lock_on.svg';
 import Lock from './lock.svg';
+import LockerRoom from './locker_room.svg';
+import LogOut from './log_out.svg';
+import Loop from './loop.svg';
+import MailRead from './mail_read.svg';
+import MailUnread from './mail_unread.svg';
+import MapPin from './map_pin.svg';
+import Map from './map.svg';
+import MenuList from './menu_list.svg';
+import MenuMore from './menu_more.svg';
+import Money from './money.svg';
+import Monitor from './monitor.svg';
+import NetworkOff from './network_off.svg';
+import NetworkOn from './network_on.svg';
+import NotificationFilled from './notification_filled.svg';
+import NotificationOff from './notification_off.svg';
+import PaperPin from './paper_pin.svg';
+import Paperclip from './paperclip.svg';
+import Password from './password.svg';
+import PhoneFilled from './phone_filled.svg';
+import Phone from './phone.svg';
+import PieChart from './pie_chart.svg';
+import PinFilled from './pin_filled.svg';
+import PlayFilled from './play_filled.svg';
+import PlusCircle from './plus_circle.svg';
+import Printer from './printer.svg';
+import QRCodeReader from './qr_code_reader.svg';
+import QRCode from './qr_code.svg';
+import Refresh from './refresh.svg';
+import SearchFilled from './search_filled.svg';
+import Search from './search.svg';
+import Settings from './settings.svg';
+import ShareAndroid from './share_android.svg';
+import ShareIos from './share_ios.svg';
+import ShieldCheck from './shield_check.svg';
+import ShieldSecure from './shield_secure.svg';
+import Signal from './signal.svg';
+import SmartphoneFilled from './smartphone_filled.svg';
+import Smartphone from './smartphone.svg';
+import SortAscending from './sort_ascending.svg';
+import SortDescending from './sort_descending.svg';
+import StarFilled from './star_filled.svg';
 import Star from './star.svg';
+import Tag from './tag.svg';
+import Time from './time.svg';
+import Token from './token.svg';
+import Trash from './trash.svg';
+import TrendingUp from './trending_up.svg';
 import TriangleAlert from './triangle_alert.svg';
-import Visibility from './visibility.svg';
+import Twitter from './twitter.svg';
+import Upload from './upload.svg';
+import Upload2 from './upload2.svg';
+import UserAdd from './user_add.svg';
+import UserFilled from './user_filled.svg';
+import User from './user.svg';
+import Video from './video.svg';
 import VisibilityOff from './visibility_off.svg';
+import VisibilityOn from './visibility_on.svg';
+import Visibility from './visibility.svg';
+import Wallet from './wallet.svg';
+import Whatsapp from './whatsapp.svg';
+import YoutubeFilled from './youtube_filled.svg';
 import Youtube from './youtube.svg';
 
 export {
+  Accessibility,
+  Accessible,
+  AddBooking,
+  Add,
+  AlertCircle,
+  AlertTriangle,
   ArrowDown,
+  ArrowDropDown,
+  ArrowDropUp,
+  ArrowLeft,
+  ArrowRight,
+  BankStatement,
+  BarChart,
+  BookingFilled,
+  Booking,
+  BuildingFilled,
   Building,
+  BulletList,
+  Camera,
+  Cart,
+  Chat,
+  CheckFilled,
+  Check,
+  CheckedFull,
+  CheckedHalf,
+  CheckinFilled,
+  Checkin,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
   Clock,
+  CloseFilled,
   Close,
+  CompassFilled,
+  Compass,
+  Copy,
+  CornerUpLeft,
+  CornerUpRight,
+  CreditCard,
+  Disconnect,
+  DislikeFilled,
+  Dislike,
+  Doc,
   Done,
+  Download,
+  Draggable,
+  Dumbbell,
+  Edit,
+  Exit,
+  Facebook,
+  FamilyMember,
+  FavoriteFilled,
+  Favorite,
   Filter,
+  Google,
+  Headphone,
+  Heartbeat,
+  HelpCircle,
+  Help,
+  History,
+  HomeFilled,
+  Home,
+  Image,
+  Info,
+  Instagram,
+  JumpRope,
+  LikeFilled,
+  Like,
+  Link,
+  Linkedin,
+  Location,
+  LockOff,
+  LockOn,
   Lock,
+  LockerRoom,
+  LogOut,
+  Loop,
+  MailRead,
+  MailUnread,
+  MapPin,
+  Map,
+  MenuList,
+  MenuMore,
+  Money,
+  Monitor,
+  NetworkOff,
+  NetworkOn,
+  NotificationFilled,
+  NotificationOff,
+  PaperPin,
+  Paperclip,
+  Password,
+  PhoneFilled,
+  Phone,
+  PieChart,
+  PinFilled,
+  PlayFilled,
+  PlusCircle,
+  Printer,
+  QRCodeReader,
+  QRCode,
+  Refresh,
+  SearchFilled,
+  Search,
+  Settings,
+  ShareAndroid,
+  ShareIos,
+  ShieldCheck,
+  ShieldSecure,
+  Signal,
+  SmartphoneFilled,
+  Smartphone,
+  SortAscending,
+  SortDescending,
+  StarFilled,
   Star,
+  Tag,
+  Time,
+  Token,
+  Trash,
+  TrendingUp,
   TriangleAlert,
-  Visibility,
+  Twitter,
+  Upload,
+  Upload2,
+  UserAdd,
+  UserFilled,
+  User,
+  Video,
   VisibilityOff,
+  VisibilityOn,
+  Visibility,
+  Wallet,
+  Whatsapp,
+  YoutubeFilled,
   Youtube,
 };
