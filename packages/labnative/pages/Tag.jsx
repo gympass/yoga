0624f40e87bea3f8b2882ec3bd0454d3721b30d1
ggdaltoso <ggdaltoso@gmail.com>
2020-10-14 09:10:05 -0300

    fix(tag): add more examples (colors)

--- packages/labnative/pages/Tag.jsx
+++ packages/labnative/pages/Tag.jsx
@@ -5,10 +5,13 @@ import { Building } from '@gympass/yoga-icons';
 
 const TagWrapper = styled.View`
   width: 100%;
+  padding: 20px;
   margin-bottom: 10px;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-evenly;
   height: 250px;
+  flex-direction: row;
+  flex-wrap: wrap;
 `;
 
 const TagPage = () => (
@@ -16,10 +19,26 @@ const TagPage = () => (
     <Tag>default</Tag>
     <Tag icon={Building}>default with custom icon</Tag>
     <Tag icon={false}>default without icon</Tag>
-    <Tag variant="informative">informative</Tag>
-    <Tag variant="positive">positive</Tag>
-    <Tag variant="negative">negative</Tag>
-    <Tag full variant="warning">
+    <Tag variant="primary">primary</Tag>
+    <Tag variant="secondary">secondary</Tag>
+
+    <Tag variant="vibin">vibin</Tag>
+    <Tag variant="hope">hope</Tag>
+    <Tag variant="energy">energy</Tag>
+    <Tag variant="relax">relax</Tag>
+    <Tag variant="peace">peace</Tag>
+    <Tag variant="verve">verve</Tag>
+    <Tag variant="uplift">uplift</Tag>
+    <Tag variant="deepPurple">deepPurple</Tag>
+    <Tag variant="stamina">stamina</Tag>
+    <Tag variant="dark">dark</Tag>
+    <Tag variant="medium">medium</Tag>
+    <Tag variant="deep">deep</Tag>
+    <Tag variant="light">light</Tag>
+    <Tag variant="clear">clear</Tag>
+    <Tag variant="white">white</Tag>
+
+    <Tag full variant="relax">
       No spots left
     </Tag>
   </TagWrapper>
