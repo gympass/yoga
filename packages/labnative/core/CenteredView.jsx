1535a120eaeb5ae826134f2dc056a66f5ed0672f
ggdaltoso <ggdaltoso@gmail.com>
2020-10-14 09:05:41 -0300

    refactor(labnative): remove all theme dependencies and also the theme changer

--- packages/labnative/core/CenteredView.jsx
+++ packages/labnative/core/CenteredView.jsx
@@ -30,11 +30,10 @@ const themeChoices = [
 ];
 
 const CenteredView = props => {
-  const [themed, setThemed] = useState(themeChoices[0]);
   const [darkMode, setDarkMode] = useState('#FFF');
 
   return (
-    <ThemeProvider theme={themed.value}>
+    <ThemeProvider>
       <>
         <View
           style={{
@@ -46,62 +45,6 @@ const CenteredView = props => {
             paddingHorizontal: 20,
           }}
         >
-          {Platform.OS === 'ios' ? (
-            <TouchableWithoutFeedback
-              style={{
-                height: 50,
-              }}
-              onPress={() =>
-                ActionSheetIOS.showActionSheetWithOptions(
-                  {
-                    options: themeChoices.map(t => t.label),
-                  },
-                  buttonIndex => {
-                    setThemed(themeChoices[buttonIndex]);
-                  },
-                )
-              }
-            >
-              <View
-                style={{
-                  alignItems: 'center',
-                  flexDirection: 'row',
-                  justifyContent: 'flex-end',
-                }}
-              >
-                <Image
-                  source={paletteIcon}
-                  style={{ width: 25, height: 25, marginRight: 12 }}
-                />
-                <Text
-                  style={{
-                    color: darkMode === '#FFF' ? '#41414A' : '#FFF',
-                  }}
-                >
-                  {themed.label}
-                </Text>
-              </View>
-            </TouchableWithoutFeedback>
-          ) : (
-            <Picker
-              selectedValue={themed.label}
-              style={{
-                width: '40%',
-                color: darkMode === '#FFF' ? '#41414A' : '#FFF',
-              }}
-              onValueChange={selectedTheme => {
-                setThemed(
-                  themeChoices[
-                    themeChoices.findIndex(t => t.value === selectedTheme)
-                  ],
-                );
-              }}
-            >
-              {themeChoices.map(({ label, value }) => (
-                <Picker.Item label={label} value={value} key={value} />
-              ))}
-            </Picker>
-          )}
           <ThemeConsumer>
             {theme => (
               <View
@@ -124,7 +67,7 @@ const CenteredView = props => {
                     setDarkMode(darkMode === '#FFF' ? '#41414A' : '#FFF');
                   }}
                   value={darkMode === '#41414A'}
-                  trackColor={{ true: theme.yoga.colors.primary[3] }}
+                  trackColor={{ true: theme.yoga.colors.primary }}
                 />
               </View>
             )}
