81fd0f242a89ecfd0e75a07314d400abdcf3555d
ggdaltoso <ggdaltoso@gmail.com>
2020-10-14 09:10:26 -0300

    refactor(lint): export named function

--- packages/labnative/customTransformer.js
+++ packages/labnative/customTransformer.js
@@ -1,7 +1,11 @@
 const reactNativeSvgTransformer = require('react-native-svg-transformer');
 const upstreamTransformer = require('metro-react-native-babel-transformer');
 
-module.exports.transform = function({ src, filename, options }) {
+module.exports.transform = function customSVGTransformer({
+  src,
+  filename,
+  options,
+}) {
   if (filename.endsWith('.svg') || filename.endsWith('.svgx')) {
     return reactNativeSvgTransformer.transform({ src, filename, options });
   }
