2e09b67ca8b8b49fc06d7fc78fabad107154e3ff
ggdaltoso <ggdaltoso@gmail.com>
2020-10-21 09:37:07 -0300

    ci(publish): remove echos and add EOF

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 ​
 BRANCH=`git rev-parse --abbrev-ref HEAD`
-echo "on $BRANCH branch"
 ​
 if [ $BRANCH == 'master' ];
 then
-  echo "yarn lerna publish --yes"
+  yarn lerna publish --yes
 elif [ $BRANCH == 'alpha' ] || [ $BRANCH == 'beta' ];
 then
-  echo "yarn lerna publish --conventional-prerelease --yes --preid $BRANCH"
+  yarn lerna publish --conventional-prerelease --yes --preid $BRANCH
 else
   echo "Nothing to publish on branch $BRANCH..."
 fi
