876727b2cf11b7270c9564d41820cdb5573895ee
ggdaltoso <ggdaltoso@gmail.com>
2020-11-18 09:50:00 -0300

    docs(expo): add peerDependencies to Snack embed

--- packages/doc/src/components/CodeBlock/CodeBlock.jsx
+++ packages/doc/src/components/CodeBlock/CodeBlock.jsx
@@ -5,53 +5,61 @@ import { ReactLive, PrismHighlight, SnackEmbed } from '.';
 import { importStatement } from './shared/templates';
 import { YogaComponents, YogaIcons, NativeComponents } from './shared/modules';
 
+const buildImportString = (code, modules) => {
+  const findComponents = /(?:<)(\w*)(?=\s*?\/?>*)/gm;
+  const sortModules = /(@gympass\/yoga*)/gm;
+  const imports = [];
+  const foundComponents = code.match(findComponents) || [];
+
+  modules
+    .sort(a => (a.path.match(sortModules) ? -1 : 0))
+    .forEach(({ name, path }) => {
+      const moduleComponents = {
+        components: [
+          ...new Set(
+            foundComponents.map(c => c.replace('<', '')).filter(c => c),
+          ),
+        ]
+          .filter(importedComponent =>
+            Object.keys(name).includes(importedComponent),
+          )
+          .filter(
+            item => !imports.filter(c => c.components.includes(item)).length,
+          ),
+        path,
+      };
+
+      if (moduleComponents.components.length > 0) {
+        imports.push(moduleComponents);
+      }
+    });
+
+  return [
+    ...imports.map(({ components, path }) => importStatement(components, path)),
+  ].join('\n');
+};
+
+const packages = [
+  { name: YogaIcons, path: '@gympass/yoga-icons' },
+  { name: YogaComponents, path: '@gympass/yoga' },
+  { name: NativeComponents, path: 'react-native' },
+];
+
+const peerDependencies = [
+  { path: 'styled-components' },
+  { path: 'react-native-svg' },
+  { path: 'react-is' },
+];
+
 const CodeBlock = ({ children: sampleCode, center, state, type, theme }) => {
   const code = sampleCode.trim();
 
-  const buildImportString = modules => {
-    const findComponents = /(?:<)(\w*)(?=\s*?\/?>*)/gm;
-    const sortModules = /(@gympass\/yoga*)/gm;
-    const imports = [];
-    const foundComponents = code.match(findComponents) || [];
+  const imports = type === 'highlight' ? [] : buildImportString(code, packages);
+  const dependencies = Array.from(
+    [...packages, ...peerDependencies],
+    ({ path }) => path,
+  );
 
-    modules
-      .sort(a => (a.path.match(sortModules) ? -1 : 0))
-      .forEach(({ name, path }) => {
-        const moduleComponents = {
-          components: [
-            ...new Set(
-              foundComponents.map(c => c.replace('<', '')).filter(c => c),
-            ),
-          ]
-            .filter(importedComponent =>
-              Object.keys(name).includes(importedComponent),
-            )
-            .filter(
-              item => !imports.filter(c => c.components.includes(item)).length,
-            ),
-          path,
-        };
-
-        if (moduleComponents.components.length > 0) {
-          imports.push(moduleComponents);
-        }
-      });
-
-    return [
-      ...imports.map(({ components, path }) =>
-        importStatement(components, path),
-      ),
-    ].join('\n');
-  };
-
-  const packages = [
-    { name: YogaIcons, path: '@gympass/yoga-icons' },
-    { name: YogaComponents, path: '@gympass/yoga' },
-    { name: NativeComponents, path: 'react-native' },
-  ];
-
-  const imports = type === 'highlight' ? [] : buildImportString(packages);
-  const dependencies = Array.from(packages, ({ path }) => path);
   const codeblockData = {
     center,
     code,
