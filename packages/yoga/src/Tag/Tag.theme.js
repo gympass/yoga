247098cc6596c396d0555159717aba34146b041c
ggdaltoso <ggdaltoso@gmail.com>
2021-02-24 10:24:10 -0300

    docs(tag): update Tag jsdoc

--- packages/yoga/src/Tag/Tag.theme.js
+++ packages/yoga/src/Tag/Tag.theme.js
@@ -1,3 +1,11 @@
+/**
+ * @param {{
+ *  spacing: import('packages/tokens/src/global/spacing').Spacing
+ *  fontSizes: import('packages/tokens/src/global/font-sizes').FontSize
+ *  fontWeights: import('packages/tokens/src/global/font-weights').FontWeight
+ *  radii: import('packages/tokens/src/global/radii').Radii
+ * }} tag
+ */
 const Tag = ({ spacing, fontSizes, fontWeights, radii }) => ({
   icon: {
     margin: {
