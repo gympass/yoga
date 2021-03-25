ff9cdad1ba51251f54f9bb5cd2672252ac3e2deb
Lucas Coelho <lucascoelhodacosta@gmail.com>
2021-03-25 15:53:26 -0300

    feat(card.actions): accept Tag inside Card.Actions

--- packages/yoga/src/Card/native/Card/Actions.jsx
+++ packages/yoga/src/Card/native/Card/Actions.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { arrayOf, oneOfType } from 'prop-types';
 
 import Button from '../../../Button';
+import Tag from '../../../Tag';
 import { typeOf } from '../../../shared/propTypes';
 
 const Actions = styled.View``;
@@ -13,6 +14,8 @@ Actions.propTypes = {
       typeOf(Button.Text),
       typeOf(Button),
       typeOf(Button.Link),
+      typeOf(Tag),
+      typeOf(Tag.Informative),
     ]),
     arrayOf(
       oneOfType([
@@ -20,6 +23,8 @@ Actions.propTypes = {
         typeOf(Button.Text),
         typeOf(Button),
         typeOf(Button.Link),
+        typeOf(Tag),
+        typeOf(Tag.Informative),
       ]),
     ),
   ]).isRequired,
--- packages/yoga/src/Card/web/Card/Actions.jsx
+++ packages/yoga/src/Card/web/Card/Actions.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { arrayOf, oneOfType } from 'prop-types';
 
 import Button from '../../../Button';
+import Tag from '../../../Tag';
 import { typeOf } from '../../../shared/propTypes';
 
 const Actions = styled.footer``;
@@ -13,6 +14,8 @@ Actions.propTypes = {
       typeOf(Button.Text),
       typeOf(Button.Outline),
       typeOf(Button.Link),
+      typeOf(Tag),
+      typeOf(Tag.Informative),
     ]),
     arrayOf(
       oneOfType([
@@ -20,6 +23,8 @@ Actions.propTypes = {
         typeOf(Button.Text),
         typeOf(Button.Outline),
         typeOf(Button.Link),
+        typeOf(Tag),
+        typeOf(Tag.Informative),
       ]),
     ),
   ]).isRequired,
