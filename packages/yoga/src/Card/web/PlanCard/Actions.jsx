48d8702658c55512edc23ac6c8f5f5c0bb7cbd98
Nathalia Pacheco <nathalia.yp19@gmail.com>
2021-04-06 20:18:14 -0300

    feat(plancard.actions): create button actions

--- packages/yoga/src/Card/web/PlanCard/Actions.jsx
+++ packages/yoga/src/Card/web/PlanCard/Actions.jsx
@@ -1,12 +1,40 @@
 import styled from 'styled-components';
 
 import Actions from '../Card/Actions';
+import Button from '../../../Button';
 import theme from '../../../Theme/helpers/themeReader';
 
-const PlanActions = styled(Actions)`
+const {
+  components: {
+    card: {
+      plan: { actions },
+    },
+  },
+} = theme;
+
+const PlanCardButton = styled(Button).attrs({
+  small: true,
+  full: true,
+})``;
+
+const PlanCardButtonText = styled(Button.Text).attrs({
+  small: true,
+  full: true,
+})`
+  margin-top: ${actions.buttonText.margin.top}px;
+  margin-bottom: ${actions.buttonText.margin.bottom}px;
+`;
+
+const PlanCardActions = styled(Actions)`
   margin-top: ${theme.components.card.plan.actions.margin.top}px;
 `;
 
-PlanActions.displayName = 'PlanCard.Actions';
+PlanCardActions.displayName = 'PlanCard.Actions';
+PlanCardButton.displayName = 'PlanCard.Button';
+PlanCardButtonText.displayName = 'PlanCard.ButtonText';
 
-export default PlanActions;
+export {
+  PlanCardActions as Actions,
+  PlanCardButton as Button,
+  PlanCardButtonText as ButtonText,
+};
