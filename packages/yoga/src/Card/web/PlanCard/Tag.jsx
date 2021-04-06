4f8c2ea8e8acbbda58b3795c66d555e8b76f23e5
Nathalia Pacheco <nathalia.yp19@gmail.com>
2021-04-06 14:43:21 -0300

    refactor(plancard.tag): use TagInformative

--- packages/yoga/src/Card/web/PlanCard/Tag.jsx
+++ packages/yoga/src/Card/web/PlanCard/Tag.jsx
@@ -1,15 +1,14 @@
 import styled, { css } from 'styled-components';
 
 import { PLAN_LINE_HEIGHT } from './PlanCard';
+import Tag from '../../../Tag';
 import theme from '../../../Theme/helpers/themeReader';
 
-const Tag = styled.span`
+const StyledTag = styled(Tag.Informative)`
   position: absolute;
   display: inline-flex;
   align-items: center;
   justify-content: center;
-
-  background-color: transparent;
 
   ${props => {
     const {
@@ -21,20 +20,10 @@ const Tag = styled.span`
     return css`
       top: ${plan.tag.position.top + PLAN_LINE_HEIGHT}px;
       left: ${plan.tag.position.left}px;
-
-      padding-left: ${plan.tag.padding.left}px;
-      padding-right: ${plan.tag.padding.right}px;
-
-      border: ${plan.tag.border.width}px solid ${plan.tag.border.color};
-      border-radius: ${plan.tag.radius}px;
-
-      font-size: ${plan.tag.font.size}px;
-      line-height: ${plan.tag.font.lineHeight}px;
-      font-weight: ${plan.tag.font.weight};
     `;
   }}
 `;
 
-Tag.displayName = 'PlanCard.Tag';
+StyledTag.displayName = 'PlanCard.Tag';
 
-export default Tag;
+export default StyledTag;
