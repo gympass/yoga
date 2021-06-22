25b36c415120145737d1893baf4584327d33c70f
Thiago Amâncio <invillia.thiago.amancio@ext.gympass.com>
2021-06-22 13:59:07 -0300

    fix: android font weight issue

--- packages/yoga/src/Text/textStyle.native.jsx
+++ packages/yoga/src/Text/textStyle.native.jsx
@@ -1,8 +1,31 @@
 import { css } from 'styled-components';
+import { Platform } from 'react-native';
 import defaultStyle from './sharedTextStyle';
 
 const textStyle = type => () => css`
   ${defaultStyle(type)};
+  ${({
+    light,
+    theme: {
+      yoga: {
+        baseFont,
+        fontWeights,
+        components: {
+          text: {
+            [type]: { fontWeight },
+          },
+        },
+      },
+    },
+  }) => `
+  font-family: ${
+    (fontWeight === 400 || Platform.OS === 'ios') && fontWeight
+      ? baseFont.family
+      : `${baseFont.family}-${fontWeight}`
+  };
+    
+    ${light ? `font-family: ${baseFont.family}-${fontWeights.light};` : ''}
+  `}
 `;
 
 export default textStyle;
