12ae7733cc5aa7c840c4b22eb22e19c79c8cf755
ggdaltoso <ggdaltoso@gmail.com>
2021-06-23 16:49:31 -0300

    feat(system): adding layout module
    with display, positions, position, top, right, bottom, left submodules

---
+++ packages/system/src/layout.js
@@ -0,0 +1,64 @@
+import { toPx } from './unit';
+import { compose, generator, getSpacing } from './theme';
+
+const display = props =>
+  generator({
+    props,
+    prop: ['display', 'd'],
+    cssProperty: 'display',
+    getter: () => ({}),
+  });
+
+const position = props =>
+  generator({
+    props,
+    prop: ['position'],
+    cssProperty: 'position',
+    getter: () => ({}),
+  });
+
+const top = props =>
+  generator({
+    props,
+    prop: ['top'],
+    cssProperty: 'top',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const right = props =>
+  generator({
+    props,
+    prop: ['right'],
+    cssProperty: 'right',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const bottom = props =>
+  generator({
+    props,
+    prop: ['bottom'],
+    cssProperty: 'bottom',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const left = props =>
+  generator({
+    props,
+    prop: ['left'],
+    cssProperty: 'left',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const positions = compose(
+  position,
+  top,
+  right,
+  bottom,
+  left,
+);
+
+export { display, positions, position, top, right, bottom, left };
