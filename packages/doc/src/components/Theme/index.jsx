6e23ff3836c2fa9cfbe635a3eaa460f04fe6d8f6
Matheus Martis <mathews_2010@outlook.com>
2021-07-07 10:25:07 -0300

    feat(theme): index to import and export theme components

---
+++ packages/doc/src/components/Theme/index.jsx
@@ -0,0 +1,23 @@
+import Colors from './Colors';
+import Fonts from './Fonts';
+import FontSizes from './FontSizes';
+import FontWeights from './FontWeights';
+import LineHeights from './Lineheights';
+import Breakpoints from './Breakpoints';
+import Spacing from './Spacing';
+import Borders from './Borders';
+import Elevations from './Elevations';
+import Shape from './Shape';
+
+export {
+  Colors,
+  Fonts,
+  FontSizes,
+  FontWeights,
+  LineHeights,
+  Breakpoints,
+  Spacing,
+  Borders,
+  Elevations,
+  Shape,
+};
