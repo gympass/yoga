3a20f9331b4ee66563ae95959d2335f1e423af1e
LeticiaSoares <lele_leticia.94@hotmail.com>
2021-07-14 18:03:06 -0300

    test: for component avatar

--- packages/yoga/src/Avatar/native/Avatar.test.jsx
+++ packages/yoga/src/Avatar/native/Avatar.test.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
-import { ThemeProvider, Avatar } from '../..';
+import { ThemeProvider } from '../..';
+import Avatar from './Avatar';
 
 describe('<Avatar />', () => {
   it('should match snapshot', () => {
