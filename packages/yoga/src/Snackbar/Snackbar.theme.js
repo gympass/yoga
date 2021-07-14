3f5a23dd5a75debe253d96baa7a43e7d63e775ad
Wendler Eis <wendler.boaventura@gympass.com>
2021-07-14 11:45:16 -0300

    feat(snackbar): add styles with basic behavior

--- packages/yoga/src/Snackbar/Snackbar.theme.js
+++ packages/yoga/src/Snackbar/Snackbar.theme.js
@@ -1,3 +1,26 @@
-const Snackbar = () => ({});
+const Snackbar = ({ colors, elevations, radii, spacing }) => ({
+  border: {
+    radius: radii.small,
+  },
+  height: {
+    min: spacing.xxxlarge,
+    max: spacing.huge,
+  },
+  padding: {
+    default: spacing.small,
+  },
+  shadow: {
+    default: elevations.small,
+  },
+  variant: {
+    success: {
+      backgroundColor: colors.feedback.success.light,
+    },
+  },
+  width: {
+    min: 320,
+    max: 600,
+  },
+});
 
 export default Snackbar;
--- packages/yoga/src/Snackbar/web/Snackbar.jsx
+++ packages/yoga/src/Snackbar/web/Snackbar.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
+import { func, string, bool } from 'prop-types';
+
+import { CheckedFull, Close } from '@gympass/yoga-icons';
+
+import Icon from '../../Icon';
+import Button from '../../Button';
+import Text from '../../Text';
+
+import { theme } from '../../Theme';
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-right: ${theme.spacing.xxsmall}px;
+`;
+
+const IconButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  cursor: pointer;
+
+  &:hover {
+    svg {
+      fill: ${theme.colors.text.secondary};
+    }
+  }
+`;
+
+const Message = styled(Text.Small)`
+  flex: 1;
+  margin-right: ${theme.spacing.small}px;
+`;
+
+const ActionsWrapper = styled.aside`
+  display: flex;
+  align-items: center;
+
+  svg {
+    margin-left: ${theme.spacing.small}px;
+    margin-right: ${theme.spacing.zero};
+  }
+`;
 
 const StyledSnackbar = styled.div`
   ${({
@@ -9,14 +53,79 @@ const StyledSnackbar = styled.div`
       },
     },
   }) => `
-    height: ${snackbar.height.small}px;
-  `}
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+  
+      position: absolute;
+      bottom: ${theme.spacing.xxlarge}px;
+      right: ${theme.spacing.xxlarge}px;
+  
+      min-width: ${snackbar.width.min}px;
+      max-width: ${snackbar.width.max}px;
+  
+      min-height: ${snackbar.height.min}px;
+      max-height: ${snackbar.height.max}px;
+  
+      padding: ${snackbar.padding.default}px;
+      
+      background-color: ${snackbar.variant.success.backgroundColor};
+      
+      border-radius: ${snackbar.border.radius}px;
+  
+      box-shadow: ${snackbar.shadow.default};
+    `}
 `;
 
-const Snackbar = props => <StyledSnackbar {...props} />;
+const Snackbar = ({
+  open,
+  hasIcon,
+  message,
+  actionText,
+  onAction,
+  onClose,
+  ...props
+}) => (
+  <StyledSnackbar {...props}>
+    {hasIcon && (
+      <IconWrapper>
+        <Icon as={CheckedFull} fill="secondary" width="large" height="large" />
+      </IconWrapper>
+    )}
 
-Snackbar.propTypes = {};
+    <Message>{message}</Message>
 
-Snackbar.defaultProps = {};
+    <ActionsWrapper>
+      {onAction && actionText && (
+        <Button.Link onClick={onAction} secondary small>
+          {actionText}
+        </Button.Link>
+      )}
+
+      {onClose && (
+        <IconButtonWrapper>
+          <Icon as={Close} fill="secondary" width="large" height="large" />
+        </IconButtonWrapper>
+      )}
+    </ActionsWrapper>
+  </StyledSnackbar>
+);
+
+Snackbar.propTypes = {
+  message: string.isRequired,
+  hasIcon: bool,
+  open: bool,
+  actionText: string,
+  onAction: func,
+  onClose: func,
+};
+
+Snackbar.defaultProps = {
+  hasIcon: true,
+  open: false,
+  actionText: undefined,
+  onAction: undefined,
+  onClose: undefined,
+};
 
 export default Snackbar;
