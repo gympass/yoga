b33ecebab41e28cda4a23874f4299c7560d5a3ec
Matheus Martis <mathews_2010@outlook.com>
2021-07-21 15:59:21 -0300

    refactor(theme): change values to example

--- packages/doc/src/components/Theme/FontWeights.jsx
+++ packages/doc/src/components/Theme/FontWeights.jsx
@@ -3,22 +3,24 @@ import styled, { withTheme, css } from 'styled-components';
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(5, 1fr);
 
   margin: 30px 0;
+
+  @media (max-width: 425px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
-  border-bottom: 1px solid gray;
   padding: 20px 0;
 
   span {
     padding: 4px 0;
 
     :nth-child(1) {
-      font-weight: bold;
       font-size: 28px;
     }
   }
@@ -34,38 +36,43 @@ const FontWeights = ({ theme }) => {
   return (
     <Wrapper>
       <Content>
-        <span> {theme.yoga.fontWeights.light} </span>
-        <span> light </span>
         <Text theme={theme} weight={0}>
-          Gympass
-        </Text>{' '}
+          {theme.yoga.fontWeights.light}
+        </Text>
+        <Text theme={theme} weight={0}>
+          light
+        </Text>
       </Content>
       <Content>
-        <span> {theme.yoga.fontWeights.regular} </span>
-        <span> regular </span>
         <Text theme={theme} weight={1}>
-          Gympass
+          {theme.yoga.fontWeights.regular}
+        </Text>
+        <Text theme={theme} weight={1}>
+          regular
         </Text>
       </Content>
       <Content>
-        <span> {theme.yoga.fontWeights.medium} </span>
-        <span> medium </span>
         <Text theme={theme} weight={2}>
-          Gympass
+          {theme.yoga.fontWeights.medium}
+        </Text>
+        <Text theme={theme} weight={2}>
+          medium
         </Text>
       </Content>
       <Content>
-        <span> {theme.yoga.fontWeights.bold} </span>
-        <span> bold </span>
         <Text theme={theme} weight={3}>
-          Gympass
+          {theme.yoga.fontWeights.bold}
+        </Text>
+        <Text theme={theme} weight={3}>
+          bold
         </Text>
       </Content>
       <Content>
-        <span> {theme.yoga.fontWeights.black} </span>
-        <span> black </span>
         <Text theme={theme} weight={4}>
-          Gympass
+          {theme.yoga.fontWeights.black}
+        </Text>
+        <Text theme={theme} weight={4}>
+          black
         </Text>
       </Content>
     </Wrapper>
