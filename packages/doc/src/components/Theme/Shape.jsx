289a2097f0929e4c4359800a2ee4f87c4168087b
Matheus Martis <mathews_2010@outlook.com>
2021-07-21 16:04:15 -0300

    refactor(theme): add new values to example

--- packages/doc/src/components/Theme/Shape.jsx
+++ packages/doc/src/components/Theme/Shape.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import styled, { withTheme, css } from 'styled-components';
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin: 30px 0;
+
+  @media (max-width: 425px) {
+    flex-direction: column;
+  }
 `;
 
 const Content = styled.div`
   display: flex;
   flex-direction: column;
-  border-bottom: 1px solid gray;
   padding: 20px 0;
 
   span {
@@ -23,11 +27,15 @@ const Content = styled.div`
 `;
 
 const Border = styled.div`
-  ${({ theme, value, valueWidth }) => css`
-    height: 36px;
-    width: ${valueWidth}px;
-    background-color: ${theme.yoga.colors.secondary};
-    border-radius: ${theme.yoga.radii[value]}px;
+  ${({ theme, value }) => css`
+    height: 20px;
+    width: 20px;
+    border-color: ${theme.yoga.colors.secondary};
+    border-top-left-radius: ${theme.yoga.radii[value]}px;
+    border: 2px;
+    border-style: solid;
+    border-right: none;
+    border-bottom: none;
   `}
 `;
 
@@ -35,24 +43,44 @@ const Shape = ({ theme }) => {
   return (
     <Wrapper>
       <Content>
-        <Border theme={theme} value={0} valueWidth={90} />
+        <Border theme={theme} value={0} />
         <span>Sharp</span>
         <span>radii.sharp</span>
+        <span>
+          value: <strong> 0 </strong>
+        </span>
       </Content>
       <Content>
         <Border theme={theme} value={1} valueWidth={90} />
-        <span>SemiRounded</span>
-        <span>radii.semiRounded</span>
+        <span>Xsmall</span>
+        <span>radii.xsmall</span>
+        <span>
+          value: <strong> 4 </strong>
+        </span>
       </Content>
       <Content>
         <Border theme={theme} value={2} valueWidth={90} />
-        <span>Rounded</span>
-        <span>radii.rounded</span>
+        <span>Small</span>
+        <span>radii.small</span>
+        <span>
+          value: <strong> 8 </strong>
+        </span>
       </Content>
       <Content>
-        <Border theme={theme} value={3} valueWidth={36} />
+        <Border theme={theme} value={3} valueWidth={90} />
+        <span>Regular</span>
+        <span>radii.regular</span>
+        <span>
+          value: <strong> 16 </strong>
+        </span>
+      </Content>
+      <Content>
+        <Border theme={theme} value={4} valueWidth={36} />
         <span>Circle</span>
         <span>radii.circle</span>
+        <span>
+          value: <strong> 9999 </strong>
+        </span>
       </Content>
     </Wrapper>
   );
