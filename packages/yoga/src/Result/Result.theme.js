e745a39f5e5b2351cac74764c103a285b7260f1f
LeticiaSoares <lele_leticia.94@hotmail.com>
2021-07-26 17:49:11 -0300

    wip

--- packages/yoga/src/Result/Result.theme.js
+++ packages/yoga/src/Result/Result.theme.js
@@ -1,3 +1,11 @@
-const Result = () => ({});
+const Result = ({ spacing }) => ({
+  margin: {
+    left: spacing.medium,
+  },
+  border: {
+    heigth: 1,
+    color: spacing.medium,
+  },
+});
 
 export default Result;
