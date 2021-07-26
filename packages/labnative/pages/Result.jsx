13a65eed223e69274f1176072131f0ff0104f4f4
LeticiaSoares <lele_leticia.94@hotmail.com>
2021-07-26 17:49:11 -0300

    feat: adjustment in component result

--- packages/labnative/pages/Result.jsx
+++ packages/labnative/pages/Result.jsx
@@ -1,11 +1,66 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Result } from '@gympass/yoga';
+import styled from 'styled-components';
+import { SearchList, Avatar } from '@gympass/yoga';
+import { BuildingFilled } from '@gympass/yoga-icons';
+import { DocTitle } from '../components';
+
+const attendanceList = [
+  {
+    description: 'Attendance 01',
+    icon: BuildingFilled,
+  },
+  {
+    description: 'Attendance 02',
+    icon: BuildingFilled,
+  },
+];
+
+const ScrollView = styled.ScrollView`
+  padding: 10px;
+  margin-bottom: 50px;
+
+  width: 100%;
+`;
+
+const SearchListWrapper = styled.View`
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+
+  padding: 20px;
+  margin-bottom: 10px;
+
+  width: 100%;
+  height: 250px;
+`;
 
 const ResultPage = () => (
-  <View>
-    <Result>Result</Result>
-  </View>
+  <ScrollView>
+    <SearchListWrapper>
+      <DocTitle>Default Avatar without image prop</DocTitle>
+      <SearchList>
+        <SearchList.Header
+          rate="5.0"
+          avatar={<Avatar />}
+          attendanceList={attendanceList}
+        >
+          Partner Name
+        </SearchList.Header>
+      </SearchList>
+    </SearchListWrapper>
+    <SearchListWrapper>
+      <DocTitle>Default Avatar without image prop</DocTitle>
+      <SearchList>
+        <SearchList.Header
+          rate="5.0"
+          avatar={<Avatar.Circle />}
+          attendanceList={attendanceList}
+        >
+          Partner Name
+        </SearchList.Header>
+      </SearchList>
+    </SearchListWrapper>
+  </ScrollView>
 );
 
 export default ResultPage;
---
+++ packages/yoga/src/Result/native/Rate.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import { string } from 'prop-types';
+import { StarFilled } from '@gympass/yoga-icons';
+import Icon from '../../Icon';
+import Text from '../../Text';
+
+const Content = styled.View`
+  display: flex;
+  flex-direction: row;
+  margin-left: 10px;
+  align-items: center;
+`;
+
+const RateValue = styled(Text.Medium)`
+  ${({
+    theme: {
+      yoga: {
+        spacing: { xxxsmall },
+      },
+    },
+  }) => {
+    return `
+      margin-left:${xxxsmall};
+    `;
+  }}
+`;
+
+const Rate = ({ rate }) => (
+  <Content>
+    <Icon as={StarFilled} fill="deep" width="xsmall" height="xsmall" />
+    <RateValue variant="deep" size="xsmall">
+      {rate}
+    </RateValue>
+  </Content>
+);
+
+Rate.propTypes = {
+  rate: string.isRequired,
+};
+
+export default Rate;
