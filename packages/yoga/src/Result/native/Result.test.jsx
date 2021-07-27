3adb34aa47032b7abaef23d6debc4726964543bd
LeticiaSoares <lele_leticia.94@hotmail.com>
2021-07-27 13:29:45 -0300

    feat: add result button

--- packages/yoga/src/Result/native/Result.test.jsx
+++ packages/yoga/src/Result/native/Result.test.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import { render } from '@testing-library/react-native';
 
+import { BuildingFilled } from '@gympass/yoga-icons';
 import { ThemeProvider, Result } from '../..';
+import Avatar from '../../Avatar';
+
+const attendanceList = [
+  {
+    description: 'Attendance 01',
+    icon: BuildingFilled,
+  },
+  {
+    description: 'Attendance 02',
+    icon: BuildingFilled,
+  },
+];
 
 describe('<Result />', () => {
   it('should match snapshot', () => {
     const { container } = render(
       <ThemeProvider>
-        <Result />
+        <Result
+          rate="5.0"
+          avatar={<Avatar.Circle />}
+          attendances={attendanceList}
+          title="Partner Name"
+          subTitle="Activity"
+          divided
+        >
+          <Result.Button>Link Button</Result.Button>
+        </Result>
       </ThemeProvider>,
     );
 
