2454448b55d18ec95d8a231095b56d7afda7209b
ggdaltoso <ggdaltoso@gmail.com>
2021-07-29 16:32:12 -0300

    refactor(doc): using Box

--- packages/doc/src/components/Theme/FontWeights.jsx
+++ packages/doc/src/components/Theme/FontWeights.jsx
@@ -1,5 +1,7 @@
+import { media } from '@gympass/yoga-helpers';
+import { Box } from '@gympass/yoga/src';
 import React from 'react';
-import styled, { withTheme, css } from 'styled-components';
+import styled, { withTheme } from 'styled-components';
 
 const Wrapper = styled.div`
   display: grid;
@@ -7,76 +9,50 @@ const Wrapper = styled.div`
 
   margin: 30px 0;
 
-  @media (max-width: 425px) {
+  ${media.max('sm')`
     grid-template-columns: repeat(3, 1fr);
-  }
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  padding: 20px 0;
-
-  span {
-    padding: 4px 0;
-
-    :nth-child(1) {
-      font-size: 28px;
-    }
-  }
-`;
-
-const Text = styled.span`
-  ${({ theme, weight }) => css`
-    font-weight: ${theme.yoga.fontWeights[weight]};
   `}
 `;
 
-const FontWeights = ({ theme }) => {
-  return (
-    <Wrapper>
-      <Content>
-        <Text theme={theme} weight={0}>
-          {theme.yoga.fontWeights.light}
-        </Text>
-        <Text theme={theme} weight={0}>
-          light
-        </Text>
-      </Content>
-      <Content>
-        <Text theme={theme} weight={1}>
-          {theme.yoga.fontWeights.regular}
-        </Text>
-        <Text theme={theme} weight={1}>
-          regular
-        </Text>
-      </Content>
-      <Content>
-        <Text theme={theme} weight={2}>
-          {theme.yoga.fontWeights.medium}
-        </Text>
-        <Text theme={theme} weight={2}>
-          medium
-        </Text>
-      </Content>
-      <Content>
-        <Text theme={theme} weight={3}>
-          {theme.yoga.fontWeights.bold}
-        </Text>
-        <Text theme={theme} weight={3}>
-          bold
-        </Text>
-      </Content>
-      <Content>
-        <Text theme={theme} weight={4}>
-          {theme.yoga.fontWeights.black}
-        </Text>
-        <Text theme={theme} weight={4}>
-          black
-        </Text>
-      </Content>
-    </Wrapper>
-  );
-};
+const Content = props => (
+  <Box display="flex" flexDirection="column" pv="medium" {...props} />
+);
+
+const TextBox = props => <Box as="span" pv="xxxsmall" {...props} />;
+
+const FontWeights = ({ theme }) => (
+  <Wrapper>
+    <Content>
+      <TextBox fw="light" fs={28}>
+        {theme.yoga.fontWeights.light}
+      </TextBox>
+      <TextBox fw="light">light</TextBox>
+    </Content>
+    <Content>
+      <TextBox fw="regular" fs={28}>
+        {theme.yoga.fontWeights.regular}
+      </TextBox>
+      <TextBox fw="regular">regular</TextBox>
+    </Content>
+    <Content>
+      <TextBox fw="medium" fs={28}>
+        {theme.yoga.fontWeights.medium}
+      </TextBox>
+      <TextBox fw="medium">medium</TextBox>
+    </Content>
+    <Content>
+      <TextBox fw="bold" fs={28}>
+        {theme.yoga.fontWeights.bold}
+      </TextBox>
+      <TextBox fw="bold">bold</TextBox>
+    </Content>
+    <Content>
+      <TextBox fw="black" fs={28}>
+        {theme.yoga.fontWeights.black}
+      </TextBox>
+      <TextBox fw="black">black</TextBox>
+    </Content>
+  </Wrapper>
+);
 
 export default withTheme(FontWeights);
