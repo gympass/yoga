5fc3a2ed2dc20cef28bb33af8c56f8ecfa67f4e1
ggdaltoso <ggdaltoso@gmail.com>
2021-07-29 16:22:48 -0300

    refactor(doc): using Box

--- packages/doc/src/components/Theme/FontSizes.jsx
+++ packages/doc/src/components/Theme/FontSizes.jsx
@@ -1,99 +1,64 @@
 import React from 'react';
-import styled, { withTheme, css } from 'styled-components';
+import { withTheme } from 'styled-components';
+import { Box } from '@gympass/yoga';
 
-const Table = styled.table`
-  tr {
-    border-bottom: 0.3px gray solid;
-    th {
-      padding: 20px 5px;
-      font-weight: normal;
+import { Table } from './Breakpoints';
 
-      :nth-child(1) {
-        font-weight: bolder;
-      }
-    }
-  }
-`;
+const Th = props => <Box as="th" {...props} />;
 
-const Th = styled.th`
-  ${({ theme, size }) => css`
-    font-size: ${theme.yoga.fontSizes[size]}px;
-  `}
-`;
-
-const FontSizes = ({ theme }) => {
-  return (
-    <Table>
-      <tr>
-        <th> Theme </th>
-        <th> Value </th>
-        <th> Example </th>
-      </tr>
-      <tr>
-        <th> fontSizes.xxsmall </th>
-        <th> {theme.yoga.fontSizes.xxsmall} </th>
-        <Th theme={theme} size={0}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.xsmall </th>
-        <th> {theme.yoga.fontSizes.xsmall} </th>
-        <Th theme={theme} size={1}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.small </th>
-        <th> {theme.yoga.fontSizes.small} </th>
-        <Th theme={theme} size={2}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.medium </th>
-        <th> {theme.yoga.fontSizes.medium} </th>
-        <Th theme={theme} size={3}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.large </th>
-        <th> {theme.yoga.fontSizes.large} </th>
-        <Th theme={theme} size={4}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.xlarge </th>
-        <th> {theme.yoga.fontSizes.xlarge} </th>
-        <Th theme={theme} size={5}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.xxlarge </th>
-        <th> {theme.yoga.fontSizes.xxlarge} </th>
-        <Th theme={theme} size={6}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.xxxlarge </th>
-        <th> {theme.yoga.fontSizes.xxxlarge} </th>
-        <Th theme={theme} size={7}>
-          live the missionss
-        </Th>
-      </tr>
-      <tr>
-        <th> fontSizes.huge </th>
-        <th> {theme.yoga.fontSizes.huge} </th>
-        <Th theme={theme} size={8}>
-          live the missionss
-        </Th>
-      </tr>
-    </Table>
-  );
-};
+const FontSizes = ({ theme }) => (
+  <Table>
+    <tr>
+      <th> Theme </th>
+      <th> Value </th>
+      <th> Example </th>
+    </tr>
+    <tr>
+      <th> fontSizes.xxsmall </th>
+      <th> {theme.yoga.fontSizes.xxsmall} </th>
+      <Th fs="xxsmall">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.xsmall </th>
+      <th> {theme.yoga.fontSizes.xsmall} </th>
+      <Th fs="xsmall">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.small </th>
+      <th> {theme.yoga.fontSizes.small} </th>
+      <Th fs="small">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.medium </th>
+      <th> {theme.yoga.fontSizes.medium} </th>
+      <Th fs="medium">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.large </th>
+      <th> {theme.yoga.fontSizes.large} </th>
+      <Th fs="large">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.xlarge </th>
+      <th> {theme.yoga.fontSizes.xlarge} </th>
+      <Th fs="xlarge">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.xxlarge </th>
+      <th> {theme.yoga.fontSizes.xxlarge} </th>
+      <Th fs="xxlarge">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.xxxlarge </th>
+      <th> {theme.yoga.fontSizes.xxxlarge} </th>
+      <Th fs="xxxlarge">live the missionss</Th>
+    </tr>
+    <tr>
+      <th> fontSizes.huge </th>
+      <th> {theme.yoga.fontSizes.huge} </th>
+      <Th fs="huge">live the missionss</Th>
+    </tr>
+  </Table>
+);
 
 export default withTheme(FontSizes);
