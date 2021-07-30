620193c5b7332ca4f33fc76aefe9234fc9ca6df6
Matheus Martis <mathews_2010@outlook.com>
2021-07-30 15:04:44 -0300

    test(avatar): add unit tests

---
+++ packages/yoga/src/Avatar/web/Avatar.test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { UserFilled } from '@gympass/yoga-icons';
+import { ThemeProvider } from '../..';
+import Avatar from './Avatar';
+
+describe('<Avatar />', () => {
+  it('should match snapshot in default avatar', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Avatar width={78} height={78} fill="black" />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+  it('should match snapshot in default avatar with placeholder prop', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Avatar placeholder={UserFilled} />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+  it('should match snapshot in circle avatar', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Avatar.Circle />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+  it('should match snapshot in circle avatar with placeholder prop', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Avatar.Circle placeholder={UserFilled} />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
