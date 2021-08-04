f12dc328ade5ff80e68b5ed68f97ea3699811bbf
alycecristines <alyce.cristine.s@gmail.com>
2021-08-04 14:00:27 -0300

    feat: add elipses in details

--- packages/yoga/src/Result/native/Details.jsx
+++ packages/yoga/src/Result/native/Details.jsx
@@ -13,13 +13,30 @@ import styled from 'styled-components';
 import Text from '../../Text';
 import Icon from '../../Icon';
 
+const Container = styled.Text`
+  max-width: 220px;
+  text-align-vertical: bottom;
+`;
+
 const Wrapper = styled.View`
   display: flex;
   flex-direction: row;
   align-items: center;
 `;
-
 const IconWrapper = styled.View`
+  ${({
+    theme: {
+      yoga: {
+        spacing: { small },
+      },
+    },
+  }) => `
+    width: ${small}px;
+    height: ${small}px;
+  `}
+`;
+
+const Separator = styled.View`
   ${({
     theme: {
       yoga: {
@@ -27,11 +44,12 @@ const IconWrapper = styled.View`
       },
     },
   }) => `
-    margin-right: ${xxxsmall}px;
+    width: ${xxxsmall}px;
+    height: ${xxxsmall}px;
   `}
 `;
 
-const Separator = styled(Text)`
+const StyledText = styled(Text.Small)`
   ${({
     variant,
     theme: {
@@ -52,41 +70,54 @@ const ResultDetails = ({ items, limit, dots, renderItem: Item }) => {
 
   return (
     <Wrapper>
-      {items &&
-        refinedList.map(({ icon: IconComponent, variant, ...props }, index) => {
-          const isLastItem = index === refinedList.length - 1;
-          const showNumbersOfItemsLeft = isLastItem && limit !== 0;
-          const numberOfItemsLeft = items.length - limit;
+      <Container numberOfLines={1}>
+        {items &&
+          refinedList.map(
+            ({ icon: IconComponent, variant, ...props }, index) => {
+              const isLastItem = index === refinedList.length - 1;
+              const showNumbersOfItemsLeft = isLastItem && limit !== 0;
+              const numberOfItemsLeft = items.length - limit;
 
-          return (
-            <Wrapper>
-              {IconComponent && (
-                <IconWrapper>
-                  <Icon
-                    as={IconComponent}
-                    fill={variant}
-                    width={14}
-                    height={14}
-                  />
-                </IconWrapper>
-              )}
-              <Item variant={variant} {...props} />
+              return (
+                <>
+                  {IconComponent && (
+                    <IconWrapper>
+                      <Icon
+                        as={IconComponent}
+                        fill={variant}
+                        width={14}
+                        height={14}
+                        style={{ marginTop: 3 }}
+                      />
+                    </IconWrapper>
+                  )}
+                  <Item variant={variant} {...props} />
 
-              {!isLastItem &&
-                (dots ? (
-                  <Separator variant={variant}>•</Separator>
-                ) : (
-                  <Separator />
-                ))}
+                  {!isLastItem &&
+                    (dots ? (
+                      <>
+                        <Separator />
+                        <StyledText variant={variant}>•</StyledText>
+                        <Separator />
+                      </>
+                    ) : (
+                      <Separator />
+                    ))}
 
-              {showNumbersOfItemsLeft && (
-                <Separator variant={variant}>
-                  + {numberOfItemsLeft.toString().padStart(2, '0')}
-                </Separator>
-              )}
-            </Wrapper>
-          );
-        })}
+                  {showNumbersOfItemsLeft && (
+                    <>
+                      <Separator />
+                      <StyledText variant={variant}>
+                        + {numberOfItemsLeft.toString().padStart(2, '0')}
+                      </StyledText>
+                      <Separator />
+                    </>
+                  )}
+                </>
+              );
+            },
+          )}
+      </Container>
     </Wrapper>
   );
 };
