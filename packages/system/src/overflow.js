c7d2bb12d932e6d2079f6a3653fb00278aa39931
Matheus Martis <mathews_2010@outlook.com>
2021-08-04 13:56:08 -0300

    feat(overflow): create a new overflow prop to component box

---
+++ packages/system/src/overflow.js
@@ -0,0 +1,30 @@
+import { compose, generator } from './theme';
+
+const overflow = props =>
+  generator({
+    props,
+    prop: ['overflow', 'of'],
+    cssProperty: 'overflow',
+  });
+
+const overflowX = props =>
+  generator({
+    props,
+    prop: ['overflowX', 'ox'],
+    cssProperty: 'overflow-x',
+  });
+
+const overflowY = props =>
+  generator({
+    props,
+    prop: ['overflowY', 'oy'],
+    cssProperty: 'overflow-y',
+  });
+
+const overflows = compose(
+  overflow,
+  overflowX,
+  overflowY,
+);
+
+export { overflows, overflow, overflowX, overflowY };
