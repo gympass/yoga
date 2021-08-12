601697dea81cfb771943ec91d30226827937affc
ggdaltoso <ggdaltoso@gmail.com>
2021-08-12 12:07:39 -0300

    chore(babel): update loose config

--- babel.config.js
+++ babel.config.js
@@ -2,46 +2,31 @@ module.exports = {
   env: {
     web: {
       ignore: ['**/native', '**/*.native.js', '**/*.test.jsx'],
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      plugins: [
-        'inline-react-svg',
-        'import-glob',
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ],
+      presets: [['@babel/preset-env', { loose: true }], '@babel/preset-react'],
+      plugins: ['inline-react-svg', 'import-glob'],
     },
     esm: {
       ignore: ['**/native', '**/*.native.js', '**/*.test.jsx'],
       presets: [
-        ['@babel/preset-env', { modules: false }],
+        ['@babel/preset-env', { loose: true, modules: false }],
         '@babel/preset-react',
       ],
-      plugins: [
-        'import-glob',
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ],
+      plugins: ['import-glob'],
     },
     native: {
       ignore: ['**/*.test.jsx'],
       only: ['**/native', '**/*.native.js'],
       presets: [
-        '@babel/preset-env',
+        ['@babel/preset-env', { loose: true }],
         '@babel/preset-react',
         'module:metro-react-native-babel-preset',
       ],
-      plugins: [
-        'import-glob',
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ],
+      plugins: ['import-glob'],
     },
     test: {
-      plugins: [
-        'import-glob',
-        '@babel/plugin-transform-flow-strip-types',
-        ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-        ['@babel/plugin-proposal-private-methods', { loose: true }],
-      ],
+      plugins: ['import-glob', '@babel/plugin-transform-flow-strip-types'],
       presets: [
-        '@babel/preset-env',
+        ['@babel/preset-env', { loose: true }],
         '@babel/preset-react',
         'module:metro-react-native-babel-preset',
       ],
