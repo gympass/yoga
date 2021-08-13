ccaa43fec366197312f831941f2b0b49f54b6120
Jonatas Falkaniere <jonatasfalkaniere@gmail.com>
2021-08-13 14:43:52 -0300

    feat(icon): add proptype for size on component input

--- packages/yoga/src/Icon/Icon.jsx
+++ packages/yoga/src/Icon/Icon.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { oneOf, elementType, string, instanceOf } from 'prop-types';
+import {
+  oneOf,
+  elementType,
+  string,
+  instanceOf,
+  checkPropTypes,
+  oneOfType,
+  number,
+} from 'prop-types';
 import { withTheme } from 'styled-components';
 
 const Icon = ({
@@ -52,7 +60,28 @@ Icon.propTypes = {
   height: oneOf([...commonSizes, instanceOf(Number)]),
   /** Size for vertical and horizontal of the SVG.
    * Use it as one of theme.spacing tokens (xxsmall, small, medium...) */
-  size: oneOf([...commonSizes, instanceOf(Number)]),
+  size: (props, propName, componentName) => {
+    const { size, width, height } = props;
+
+    if (size) {
+      checkPropTypes(
+        {
+          [propName]: oneOfType([oneOf(commonSizes), string, number]),
+        },
+        { size },
+        'prop',
+        componentName,
+      );
+
+      if (width || height) {
+        return new Error(
+          `you must use only ${propName}, alone, or width and/or height in ${componentName}`,
+        );
+      }
+    }
+
+    return null;
+  },
 };
 
 Icon.defaultProps = {
