e4b6400e08a61f9905b973f35ff2645b201b4601
ggdaltoso <ggdaltoso@gmail.com>
2021-08-19 13:31:44 -0300

    feat(system): create elevation for android in @gympass/yoga-system

---
+++ packages/system/src/elevation.android.js
@@ -0,0 +1,11 @@
+import { getElevation, generator } from './theme';
+
+const elevation = props =>
+  generator({
+    props,
+    prop: ['boxShadow', 'bs', 'elevation'],
+    cssProperty: 'elevation',
+    getter: getElevation,
+  });
+
+export { elevation };
