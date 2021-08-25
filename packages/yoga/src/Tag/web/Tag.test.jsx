b7ce49032cf2e51177f5c29d2701aa02d04908d0
Matheus Martis <mathews_2010@outlook.com>
2021-08-25 13:34:50 -0300

    test(tag): add tests with margin prop

--- packages/yoga/src/Tag/web/Tag.test.jsx
+++ packages/yoga/src/Tag/web/Tag.test.jsx
@@ -62,4 +62,32 @@ describe('<Tag />', () => {
 
     expect(container).toMatchSnapshot();
   });
+
+  it('should match snapshot with variant and margin prop ', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Tag variant="success">success</Tag>
+        <Tag mv="medium" variant="informative">
+          informative
+        </Tag>
+        <Tag variant="attention">attention</Tag>
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot with variant prop and informative type with margin prop', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Tag.Informative variant="success">success</Tag.Informative>
+        <Tag.Informative mv="medium" variant="informative">
+          informative
+        </Tag.Informative>
+        <Tag.Informative variant="attention">attention</Tag.Informative>
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
 });
