670414be51f453b035bbed16249f73accab6be5a
Nathalia Pacheco <nathalia.pacheco@gympass.com>
2021-08-27 16:12:05 -0300

    refactor(list-item): add small prop

--- packages/yoga/src/List/native/ListItem.jsx
+++ packages/yoga/src/List/native/ListItem.jsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 const StyledView = styled.View(
   ({
     divided,
+    small,
     theme: {
       yoga: {
         components: { list },
@@ -20,23 +21,37 @@ const StyledView = styled.View(
       : ''
   }
 
-    padding:
-      ${list.listItem.padding.top}px
-      ${list.listItem.padding.right}px
-      ${list.listItem.padding.bottom}px
-      ${list.listItem.padding.left}px;
+  ${
+    small
+      ? `
+          padding:
+            ${list.listItem.small.padding.top}px
+            ${list.listItem.small.padding.right}px
+            ${list.listItem.small.padding.bottom}px
+            ${list.listItem.small.padding.left}px;
+        `
+      : `
+          padding:
+            ${list.listItem.padding.top}px
+            ${list.listItem.padding.right}px
+            ${list.listItem.padding.bottom}px
+            ${list.listItem.padding.left}px;
+        `
+  }
   `,
 );
 
-const ListItem = ({ divided, ...rest }) => (
-  <StyledView divided={divided} {...rest} />
+const ListItem = ({ small, divided, ...rest }) => (
+  <StyledView small={small} divided={divided} {...rest} />
 );
 
 ListItem.propTypes = {
+  small: PropTypes.bool,
   divided: PropTypes.bool,
 };
 
 ListItem.defaultProps = {
+  small: false,
   divided: true,
 };
 
