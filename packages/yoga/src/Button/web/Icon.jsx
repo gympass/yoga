62e01da78da714780387b9bb9205db203d764983
Matheus Martins <mathews_2010@outlook.com>
2021-09-15 16:59:28 -0300

    feat(button): add forwardRef and large prop

--- packages/yoga/src/Button/web/Icon.jsx
+++ packages/yoga/src/Button/web/Icon.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import { bool } from 'prop-types';
+import React, { forwardRef } from 'react';
+import { oneOfType, func, node, bool } from 'prop-types';
 import styled from 'styled-components';
 import { hexToRgb } from '@gympass/yoga-common';
 
-import StyledButton from './StyledButton';
+import Button from './Button';
 
-const Icon = styled(StyledButton)`
+const IconStyled = styled(Button)`
   ${({
-    full,
+    large,
+    inverted,
     secondary,
     theme: {
       yoga: {
@@ -18,19 +19,29 @@ const Icon = styled(StyledButton)`
     const state = secondary ? 'secondary' : 'primary';
 
     return `
-      height: unset;
-      padding: 0;
-      background-color: unset;
-      border: none;
-      border-radius: 0;
-      color: ${button.types.icon.font[state].color};
+      width: ${
+        large ? button.types.icon.size.large : button.types.icon.size.default
+      }px;
+      height: ${
+        large ? button.types.icon.size.large : button.types.icon.size.default
+      }px;
+      padding: 4px;
+      border-radius: 50%;
+
+      svg {
+        margin: 0;
+        width: ${large ? '24' : '12'}px;
+        height: ${large ? '24' : '12'}px;
+        fill: ${button.types.contained.font.default.color};
+ 
+        transition: fill 0.2s;
+      }
 
       &:disabled,
       &:not([disabled]):hover,
       &:not([disabled]):focus,
       &:not([disabled]):active {
         box-shadow: unset;
-        background-color: unset;
       }
 
       &:not([disabled]):hover  {
@@ -45,21 +56,57 @@ const Icon = styled(StyledButton)`
         color: ${button.types.link.font.disabled.color};
       }
 
-      ${full ? 'width: 100%' : ''}
+      ${
+        inverted
+          ? `
+              background-color: ${button.types.contained.font.default.color};
+              color: ${button.types.contained.backgroundColor[state].default};
+      
+              svg {
+                fill: ${button.types.contained.backgroundColor[state].default};
+              }
+      
+              &:active {
+                background-color: ${hexToRgb(
+                  button.types.contained.font.default.color,
+                  0.75,
+                )};
+                color: ${button.types.contained.backgroundColor[state].pressed};
+      
+                svg {
+                  fill: ${
+                    button.types.contained.backgroundColor[state].pressed
+                  };
+                }
+              }
+      
+              &:not([disabled]):hover, &:not([disabled]):focus {
+                box-shadow: 0 4px 8px ${hexToRgb(
+                  button.types.contained.font.default.color,
+                  0.45,
+                )};
+              }
+            `
+          : ''
+      }
     `;
   }}
 `;
 
-const ButtonIcon = props => <Icon {...props} />;
+const ButtonIcon = forwardRef(({ icon: Icon, ...props }, ref) => (
+  <IconStyled ref={ref} {...props}>
+    {Icon && <Icon />}
+  </IconStyled>
+));
 
 ButtonIcon.propTypes = {
-  disabled: bool,
-  secondary: bool,
+  large: bool,
+  icon: oneOfType([node, func]),
 };
 
 ButtonIcon.defaultProps = {
-  disabled: false,
-  secondary: false,
+  large: undefined,
+  icon: undefined,
 };
 
 ButtonIcon.displayName = 'Button.Icon';
