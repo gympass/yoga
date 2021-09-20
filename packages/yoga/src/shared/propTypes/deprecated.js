f95253e1bed52d31ba0bfd9b509273882b32af88
Eric C. Leão <ericcleao@gmail.com>
2021-09-20 19:28:52 -0300

    refactor(prop-types): update deprecated validation to use console.warn instead of warning

--- packages/yoga/src/shared/propTypes/deprecated.js
+++ packages/yoga/src/shared/propTypes/deprecated.js
@@ -1,23 +1,14 @@
-import warning from 'warning';
+/* eslint-disable no-console */
 
-let warned = {};
-
-export default function deprecated(propType, explanation) {
+const deprecated = (propType, explanation) => {
   return function validate(props, propName, componentName, ...rest) {
     if (props[propName] != null) {
       const message = `"${propName}" property of "${componentName}" has been deprecated.\n${explanation}`;
-      if (!warned[message]) {
-        warning(false, message);
-        warned[message] = true;
-      }
+      console.warn(message);
     }
 
     return propType(props, propName, componentName, ...rest);
   };
-}
+};
 
-function resetWarned() {
-  warned = {};
-}
-
-deprecated.resetWarned = resetWarned;
+export default deprecated;
