78378774db5978b6595582653a559ad44c55116e
Lucas Coelho <lucascoelhodacosta@gmail.com>
2021-09-24 08:14:36 -0300

    test(theme-generator): add spec for invalid type

--- packages/yoga/src/Theme/helpers/themeGenerator/index.test.js
+++ packages/yoga/src/Theme/helpers/themeGenerator/index.test.js
@@ -1,16 +1,23 @@
 import * as tokens from '@gympass/yoga-tokens';
 import createTheme from './index';
 
-it('should generate a callable theme which overrides only received tokens', () => {
-  const generatedTheme = createTheme(yogaTokens => ({
-    colors: {
-      primary: yogaTokens.colors.stamina,
-    },
-  }));
-  expect(typeof generatedTheme === 'function');
-  expect(generatedTheme(tokens.default)).toEqual({
-    colors: {
-      primary: '#231B22',
-    },
+describe('given theme generator:createTheme()', () => {
+  it('should generate a callable theme which overrides only received tokens', () => {
+    const generatedTheme = createTheme(yogaTokens => ({
+      colors: {
+        primary: yogaTokens.colors.stamina,
+      },
+    }));
+
+    expect(typeof generatedTheme === 'function');
+    expect(generatedTheme(tokens.default)).toEqual({
+      colors: {
+        primary: '#231B22',
+      },
+    });
+  });
+
+  it('should not accept invalid parameters', () => {
+    expect(() => createTheme(true)).toThrow(TypeError);
   });
 });
