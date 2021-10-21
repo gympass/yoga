54dd9cd764ed8a736de2e328e9332c8d58d0c7a0
alycecristines <alyce.cristine.s@gmail.com>
2021-10-21 18:37:46 -0300

    feat(avatar): add children prop to avatar native
    in order to cover a new scenario for the app was added a new variation to avatar native where a children prop is accepted.

--- packages/yoga/src/Avatar/native/Avatar.jsx
+++ packages/yoga/src/Avatar/native/Avatar.jsx
@@ -10,26 +10,41 @@ import Icon from '../../Icon';
  * The Avatar component is used to display the image.
  * It has two shapes: default and circle
  */
-const Avatar = ({ src, icon = BuildingFilled, fill, stroke, ...props }) => (
-  <Box
-    bgColor="elements.selectionAndIcons"
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    width={48}
-    height={48}
-    borderRadius="small"
-    elevation="small"
-    overflow="hidden"
-    {...props}
-  >
-    {src ? (
-      <Box as={Image} width="100%" height="100%" source={src} />
-    ) : (
+const Avatar = ({
+  src,
+  children,
+  icon = BuildingFilled,
+  fill,
+  stroke,
+  ...props
+}) => {
+  function renderContent() {
+    if (src) return <Box as={Image} width="100%" height="100%" source={src} />;
+
+    if (children) return children;
+
+    return (
       <Icon as={icon} width="50%" height="50%" fill={fill} stroke={stroke} />
-    )}
-  </Box>
-);
+    );
+  }
+
+  return (
+    <Box
+      bgColor="elements.selectionAndIcons"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      width={48}
+      height={48}
+      borderRadius="small"
+      elevation="small"
+      overflow="hidden"
+      {...props}
+    >
+      {renderContent()}
+    </Box>
+  );
+};
 
 Avatar.propTypes = {
   src: Image.propTypes.source,
