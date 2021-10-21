6765e5b93a12d12c2a4c3a8c875fee93028e2121
Lucas Coelho <lucascoelhodacosta@gmail.com>
2021-10-21 08:07:01 -0300

    fix(navigation): typo inside getSorting's documentation

--- packages/doc/src/components/Navigation/Navigation.jsx
+++ packages/doc/src/components/Navigation/Navigation.jsx
@@ -158,9 +158,6 @@ const ListItem = ({
   const { pathname } = window.location;
   const linkPath = prefix ? `/yoga${filteredUrl}` : filteredUrl;
   const isActive = window && pathname.replace(/\/$/, '') === linkPath;
-  const innerSorting = sectionsSortedByName.includes(title.toLowerCase())
-    ? 'alphabetic'
-    : 'order';
 
   const onNavigate = () => {
     if (filteredUrl !== pathname) {
@@ -203,7 +200,6 @@ const ListItem = ({
             level={level + 1}
             toggleMenu={toggleMenu}
             prefix={prefix}
-            sorting={innerSorting}
           />
         </StyledList>
       )}
