e74d19766444d3766313cfddc8dfb62f964e6e6f
alycecristines <alyce.cristine.s@gmail.com>
2021-11-17 16:42:03 -0300

    fix(result): changing the limitLabel default props and remove the 0 from numberOfItemsLeft
    when using this component without passing anything in the limitLabel property the text undefined was
    showing and this change was made to fix this problem

--- packages/yoga/src/Result/native/Details.jsx
+++ packages/yoga/src/Result/native/Details.jsx
@@ -83,7 +83,7 @@ const ResultDetails = ({
                   <>
                     <Separator />
                     <StyledText variant={variant}>
-                      + {numberOfItemsLeft.toString().padStart(2, '0')}
+                      + {numberOfItemsLeft.toString()}
                       {limitLabel !== '' && ` ${limitLabel}`}
                     </StyledText>
                     <Separator />
@@ -117,7 +117,7 @@ ResultDetails.propTypes = {
 
 ResultDetails.defaultProps = {
   limit: undefined,
-  limitLabel: undefined,
+  limitLabel: '',
   dots: false,
   renderItem: Text.Tiny,
 };
