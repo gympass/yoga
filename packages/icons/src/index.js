589761254673d7b41b921348ac2d9c0ffe9f46e1
Matheus Martins <mathews_2010@outlook.com>
2022-05-04 07:49:37 -0300

    chore(icons): add flag countries

--- packages/icons/src/index.js
+++ packages/icons/src/index.js
@@ -136,6 +136,17 @@ import Visibility from './svg/visibility.svg';
 import Wallet from './svg/wallet.svg';
 import Whatsapp from './svg/whatsapp.svg';
 import Youtube from './svg/youtube.svg';
+import FlagArgentina from './svg/flag_argentina.svg';
+import FlagBrazil from './svg/flag_brazil.svg';
+import FlagChile from './svg/flag_chile.svg';
+import FlagGermany from './svg/flag_germany.svg';
+import FlagIreland from './svg/flag_ireland.svg';
+import FlagItaly from './svg/flag_italy.svg';
+import FlagMexico from './svg/flag_mexico.svg';
+import FlagPortugal from './svg/flag_portugal.svg';
+import FlagSpain from './svg/flag_spain.svg';
+import FlagUK from './svg/flag_uk.svg';
+import FlagUS from './svg/flag_us.svg';
 
 export {
   Accessibility,
@@ -276,4 +287,15 @@ export {
   Wallet,
   Whatsapp,
   Youtube,
+  FlagArgentina,
+  FlagBrazil,
+  FlagChile,
+  FlagGermany,
+  FlagIreland,
+  FlagItaly,
+  FlagMexico,
+  FlagPortugal,
+  FlagSpain,
+  FlagUK,
+  FlagUS,
 };
