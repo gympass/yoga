0722ec296f236cb6bd05624b8cb8d74bd876b2e1
Eric C. Leão <ericcleao@gmail.com>
2022-05-09 10:44:12 -0300

    test(snackbar): fix snapshot test

--- packages/yoga/src/Snackbar/native/Snackbar.test.jsx
+++ packages/yoga/src/Snackbar/native/Snackbar.test.jsx
@@ -4,10 +4,15 @@ import { render } from '@testing-library/react-native';
 import { ThemeProvider, Snackbar } from '../..';
 
 describe('<Snackbar />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.useFakeTimers();
+  });
+
   it('should match snapshot', () => {
     const { container } = render(
       <ThemeProvider>
-        <Snackbar />
+        <Snackbar message="Default snackbar" />
       </ThemeProvider>,
     );
 
