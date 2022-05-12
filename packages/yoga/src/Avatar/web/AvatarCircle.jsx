f0ba89b60c32d14af9dd3b9fba85ccc16e3c6cac
Matheus Martins <mathews_2010@outlook.com>
2022-05-12 10:40:01 -0300

    feat(avatar): add forwardRef to avatar component

--- packages/yoga/src/Avatar/web/AvatarCircle.jsx
+++ packages/yoga/src/Avatar/web/AvatarCircle.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { UserFilled } from '@gympass/yoga-icons';
 
 import Avatar from './Avatar';
 
-const AvatarCircle = ({ src, alt = '', ...props }) => (
+const AvatarCircle = forwardRef(({ src, alt = '', ...props }, ref) => (
   <Avatar
     {...props}
+    ref={ref}
     src={src}
     alt={alt}
     borderRadius="circle"
     icon={!src ? UserFilled : undefined}
   />
-);
+));
 
 AvatarCircle.propTypes = Avatar.propTypes;
 
