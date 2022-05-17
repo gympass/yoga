40aadaee4a16ef0414c7e4b01d8ed7f19200cf4f
Esthéfanie Vila Maior <contatoevilamaior@gmail.com>
2022-05-17 16:09:06 -0300

    docs(snackbar): add native documentation

--- packages/yoga/src/Snackbar/native/Snackbar.jsx
+++ packages/yoga/src/Snackbar/native/Snackbar.jsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react';
+import React, { createRef, forwardRef, useImperativeHandle } from 'react';
 import styled, { withTheme } from 'styled-components';
 import { string, oneOf, func, elementType, number } from 'prop-types';
 
@@ -22,7 +22,9 @@ const SnackbarContainer = styled.View`
           snackbar: {
             padding,
             margin,
-            variant: { [variant]: backgroundColor = feedback.success.light },
+            variant: {
+              color: { [variant]: backgroundColor = feedback.success.light },
+            },
             border,
           },
         },
@@ -39,96 +41,130 @@ const SnackbarContainer = styled.View`
   `}
 `;
 
-const Snackbar = ({
-  icon,
-  message,
-  actionLabel,
-  onAction,
-  variant,
-  onSnackbarClose,
-  duration,
-  bottomOffset,
-  ...props
-}) => {
-  const wrapperRef = createRef();
+/**
+ * Gympass `<Snackbar />` is the proper component to show alert messages.
+ *
+ * For web components, the `Snackbar` may have an icon, a custom action and a close button.
+ *
+ * For native, the `Snackbar` may have an icon and a custom action.
+ */
+const Snackbar = forwardRef(
+  (
+    {
+      icon,
+      message,
+      actionLabel,
+      onAction,
+      variant,
+      onSnackbarClose,
+      duration,
+      bottomOffset,
+      ...props
+    },
+    ref,
+  ) => {
+    const wrapperRef = createRef();
 
-  const handlePanResponderRelease = (_evt, gestureState) => {
-    if (gestureState.dy > SWIPE_THRESHOLD) {
-      wrapperRef.current.close();
-    } else {
-      wrapperRef.current.open();
-    }
-  };
+    useImperativeHandle(ref, () => ({
+      open: () => {
+        wrapperRef.current.open();
+      },
+      close: () => {
+        wrapperRef.current.close();
+      },
+    }));
 
-  const panResponder = PanResponder.create({
-    onPanResponderMove: (_, gestureState) => {
-      if (gestureState.dy > 0) {
-        wrapperRef.current.translateY(gestureState.dy);
+    const handlePanResponderRelease = (_evt, gestureState) => {
+      if (gestureState.dy > SWIPE_THRESHOLD) {
+        wrapperRef.current.close();
+      } else {
+        wrapperRef.current.open();
       }
-    },
-    onPanResponderRelease: handlePanResponderRelease,
-    onPanResponderTerminate: handlePanResponderRelease,
-    onStartShouldSetPanResponder: () => true,
-    onMoveShouldSetPanResponder: (_, gestureState) => {
-      return !(gestureState.dy <= 2 && gestureState.dy >= -2);
-    },
-    onMoveShouldSetPanResponderCapture: () => true,
-    onPanResponderTerminationRequest: () => true,
-    onShouldBlockNativeResponder: () => true,
-  });
+    };
 
-  const handleOnAction = () => {
-    wrapperRef.current.close();
-    onAction();
-  };
+    const panResponder = PanResponder.create({
+      onPanResponderMove: (_, gestureState) => {
+        if (gestureState.dy > 0) {
+          wrapperRef.current.translateY(gestureState.dy);
+        }
+      },
+      onPanResponderRelease: handlePanResponderRelease,
+      onPanResponderTerminate: handlePanResponderRelease,
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: (_, gestureState) => {
+        return !(gestureState.dy <= 2 && gestureState.dy >= -2);
+      },
+      onMoveShouldSetPanResponderCapture: () => true,
+      onPanResponderTerminationRequest: () => true,
+      onShouldBlockNativeResponder: () => true,
+    });
 
-  return (
-    <SnackbarAnimationWrapper
-      onSnackbarClose={onSnackbarClose}
-      duration={duration}
-      ref={wrapperRef}
-    >
-      <SnackbarContainer
-        variant={variant}
-        bottomOffset={bottomOffset}
-        {...props}
-        {...panResponder.panHandlers}
+    const handleOnAction = () => {
+      wrapperRef.current.close();
+      onAction();
+    };
+
+    return (
+      <SnackbarAnimationWrapper
+        onSnackbarClose={onSnackbarClose}
+        duration={duration}
+        ref={wrapperRef}
       >
-        {icon && (
-          <Icon as={icon} fill="secondary" size="large" marginRight="xxsmall" />
-        )}
-        <Text
-          flex={1}
-          fontSize="small"
-          marginVertical="xxxsmall"
-          numberOfLines={2}
+        <SnackbarContainer
+          variant={variant}
+          bottomOffset={bottomOffset}
+          {...props}
+          {...panResponder.panHandlers}
         >
-          {message}
-        </Text>
-        {actionLabel && onAction && (
-          <Box
-            as={Button.Text}
-            small
-            secondary
-            marginLeft="xxsmall"
-            onPress={handleOnAction}
+          {icon && (
+            <Icon
+              as={icon}
+              fill="secondary"
+              size="large"
+              marginRight="xxsmall"
+            />
+          )}
+          <Text
+            flex={1}
+            fontSize="small"
+            marginVertical="xxxsmall"
+            numberOfLines={2}
           >
-            {actionLabel}
-          </Box>
-        )}
-      </SnackbarContainer>
-    </SnackbarAnimationWrapper>
-  );
-};
+            {message}
+          </Text>
+          {actionLabel && onAction && (
+            <Box
+              as={Button.Text}
+              small
+              secondary
+              marginLeft="xxsmall"
+              onPress={handleOnAction}
+            >
+              {actionLabel}
+            </Box>
+          )}
+        </SnackbarContainer>
+      </SnackbarAnimationWrapper>
+    );
+  },
+);
 
 Snackbar.propTypes = {
-  variant: oneOf(['success', 'informative', 'attention', 'failure', 'info']),
+  /** The style variant change the color of the component, it may be "success", "informative" or "attention". */
+  variant: oneOf(['success', 'informative', 'attention']),
+  /** Can be any icon of yoga-icons. */
   icon: elementType,
+  /** The message shown when snackbar is opened. The maximum number of lines is two. */
   message: string.isRequired,
+  /** Label for a custom action. */
   actionLabel: string,
+  /** Function for the custom action. The `actionLabel` becomes required when passing this function. */
   onAction: func,
+  /** Callback function triggered by the Snackbar close action. Can be used for events, for example. */
   onSnackbarClose: func,
+  /** The duration sets how long it will take to close snackbar automatically, it may be "fast" (4 seconds), "default" (8 seconds), "slow" (10 seconds) or "indefinite" (it doesn't close automatically). */
   duration: oneOf(['fast', 'default', 'slow', 'indefinite']),
+  /** Add extra margin to Snackbar. Can be useful for SafeAreaView or button cases. */
   bottomOffset: number,
 };
 
