86e0b496c5d620056203d0c35eec47fa2ad15e78
Esthéfanie Vila Maior <contatoevilamaior@gmail.com>
2022-05-18 13:41:45 -0300

    test(snackbar): add icon, action, long text and open function scenarios

--- packages/yoga/src/Snackbar/native/Snackbar.test.jsx
+++ packages/yoga/src/Snackbar/native/Snackbar.test.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
+import React, { useRef } from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
 
+import { CheckedFull } from '@gympass/yoga-icons';
 import { ThemeProvider, Snackbar } from '../..';
+import { Button } from '../../Card/web/PlanCard/Actions';
 
 describe('<Snackbar />', () => {
   beforeEach(() => {
@@ -9,12 +11,90 @@ describe('<Snackbar />', () => {
     jest.useFakeTimers();
   });
 
-  it('should match snapshot', () => {
+  it('should match snapshot when snackbar is default', () => {
     const { container } = render(
       <ThemeProvider>
-        <Snackbar message="Default snackbar" />
+        <Snackbar message="Default snackbar." />
       </ThemeProvider>,
     );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot when have an icon and action', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Snackbar
+          message="Snackbar with an icon."
+          icon={CheckedFull}
+          actionLabel="Action"
+          onAction={() => {}}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot when have a long text', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Snackbar
+          message="Lorem Ipsum is simply dummy text of the printing and types."
+          icon={CheckedFull}
+          actionLabel="Action"
+          onAction={() => {}}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot when have a variant informative or attention prop', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Snackbar
+          message="Feedback Message"
+          icon={CheckedFull}
+          actionLabel="Action"
+          onAction={() => {}}
+          variant="informative"
+        />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should show snackbar when the open function is called', () => {
+    const Component = () => {
+      const snackbarRef = useRef();
+
+      return (
+        <ThemeProvider>
+          <Button
+            onPress={() => {
+              snackbarRef.current.open();
+            }}
+          >
+            Tap to open snackbar
+          </Button>
+          <Snackbar
+            ref={snackbarRef}
+            message="Feedback Message"
+            icon={CheckedFull}
+            actionLabel="Action"
+            onAction={() => {}}
+            variant="informative"
+          />
+        </ThemeProvider>
+      );
+    };
+
+    const { container, getByText } = render(<Component />);
+
+    fireEvent.press(getByText('Tap to open snackbar'));
 
     expect(container).toMatchSnapshot();
   });
