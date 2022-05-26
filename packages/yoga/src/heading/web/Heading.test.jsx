e7d03fda0cfb3799f8f359b9327304812d3ef047
Thiago Amâncio <invillia.thiago.amancio@ext.gympass.com>
2022-05-26 09:57:05 -0300

    feat(heading): create tests

--- packages/yoga/src/heading/web/Heading.test.jsx
+++ packages/yoga/src/heading/web/Heading.test.jsx
@@ -1,13 +1,58 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { Upload2 } from '@gympass/yoga-icons/src';
 import { ThemeProvider, Heading } from '../..';
+import Title from './Title';
+import BackButton from './BackButton';
+import RightButton from './RightButton';
 
 describe('<Heading />', () => {
-  it('should match snapshot', () => {
+  it('should match snapshot with title', () => {
     const { container } = render(
       <ThemeProvider>
-        <Heading />
+        <Heading>
+          <Title>Gympass</Title>
+        </Heading>
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot with title and back button', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Heading>
+          <Title>Gympass</Title>
+          <BackButton />
+        </Heading>
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot with all components', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Heading>
+          <Title>Gympass</Title>
+          <BackButton />
+          <RightButton icon={Upload2} />
+        </Heading>
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot with back button', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <Heading>
+          <BackButton />
+        </Heading>
       </ThemeProvider>,
     );
 
