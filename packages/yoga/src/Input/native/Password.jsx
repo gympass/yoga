0b729e4a35f3424355ddbdffd274199fe649cdcc
Guilherme Cardoso <guilherme.cardoso@gympass.com>
2022-06-03 14:21:41 -0300

    fix: add forwardRef on all components Input

--- packages/yoga/src/Input/native/Password.jsx
+++ packages/yoga/src/Input/native/Password.jsx
@@ -36,79 +36,85 @@ const IconWrapper = styled.View(
   `,
 );
 
-const Password = ({
-  disabled,
-  style,
-  full,
-  onBlur,
-  onFocus,
-  theme: {
-    yoga: {
-      colors,
-      components: { input },
+const Password = React.forwardRef(
+  (
+    {
+      disabled,
+      style,
+      full,
+      onBlur,
+      onFocus,
+      theme: {
+        yoga: {
+          colors,
+          components: { input },
+        },
+      },
+      ...props
     },
+    ref,
+  ) => {
+    const [showPassword, toggleShowPassword] = useState(false);
+    const [focused, setFocused] = useState(false);
+
+    const iconColor = () => {
+      if (disabled) {
+        return colors.elements.backgroundAndDisabled;
+      }
+
+      if (focused) {
+        return input.font.color.focus;
+      }
+
+      return input.font.color.default;
+    };
+
+    return (
+      <Wrapper style={style} full={full}>
+        <Input
+          ref={ref}
+          {...props}
+          autoCompleteType="password"
+          blurOnSubmit={false}
+          cleanable={false}
+          clearTextOnFocus={false}
+          disabled={disabled}
+          full={full}
+          secureTextEntry={!showPassword}
+          textContentType="password"
+          onSubmitEditing={() => Keyboard.dismiss()}
+          onBlur={e => {
+            setFocused(false);
+            onBlur(e);
+          }}
+          onFocus={e => {
+            setFocused(true);
+            onFocus(e);
+          }}
+        />
+        <TouchableWithoutFeedback
+          accessibilityRole="button"
+          onPress={() => {
+            if (disabled) return;
+            toggleShowPassword(!showPassword);
+          }}
+        >
+          <IconWrapper disabled={disabled}>
+            {showPassword ? (
+              <Visibility height={input.height} width={20} fill={iconColor()} />
+            ) : (
+              <VisibilityOff
+                height={input.height}
+                width={20}
+                fill={iconColor()}
+              />
+            )}
+          </IconWrapper>
+        </TouchableWithoutFeedback>
+      </Wrapper>
+    );
   },
-  ...props
-}) => {
-  const [showPassword, toggleShowPassword] = useState(false);
-  const [focused, setFocused] = useState(false);
-
-  const iconColor = () => {
-    if (disabled) {
-      return colors.elements.backgroundAndDisabled;
-    }
-
-    if (focused) {
-      return input.font.color.focus;
-    }
-
-    return input.font.color.default;
-  };
-
-  return (
-    <Wrapper style={style} full={full}>
-      <Input
-        {...props}
-        autoCompleteType="password"
-        blurOnSubmit={false}
-        cleanable={false}
-        clearTextOnFocus={false}
-        disabled={disabled}
-        full={full}
-        secureTextEntry={!showPassword}
-        textContentType="password"
-        onSubmitEditing={() => Keyboard.dismiss()}
-        onBlur={e => {
-          setFocused(false);
-          onBlur(e);
-        }}
-        onFocus={e => {
-          setFocused(true);
-          onFocus(e);
-        }}
-      />
-      <TouchableWithoutFeedback
-        accessibilityRole="button"
-        onPress={() => {
-          if (disabled) return;
-          toggleShowPassword(!showPassword);
-        }}
-      >
-        <IconWrapper disabled={disabled}>
-          {showPassword ? (
-            <Visibility height={input.height} width={20} fill={iconColor()} />
-          ) : (
-            <VisibilityOff
-              height={input.height}
-              width={20}
-              fill={iconColor()}
-            />
-          )}
-        </IconWrapper>
-      </TouchableWithoutFeedback>
-    </Wrapper>
-  );
-};
+);
 
 Password.propTypes = {
   disabled: bool,
