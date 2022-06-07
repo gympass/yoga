bd999483a7f99edf801025e88f89a0d5c2bdaeb6
Matheus Martins <mathews_2010@outlook.com>
2022-06-07 09:47:29 -0300

    chore: removing ignore on babel

--- packages/helpers/babel.config.js
+++ packages/helpers/babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  env: {
-    esm: {
-      presets: [['@babel/preset-env', { modules: false }]],
-    },
-    cjs: {
-      presets: ['@babel/preset-env'],
-    },
-  },
+  extends: '../../babel.config.js',
 };
--- packages/system/babel.config.js
+++ packages/system/babel.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  ignore: ['**/*.test.js'],
-  env: {
-    esm: {
-      presets: [['@babel/preset-env', { modules: false }]],
-    },
-    cjs: {
-      presets: ['@babel/preset-env'],
-    },
-  },
+  extends: '../../babel.config.js',
 };
