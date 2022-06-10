20872f4e6569202bcfbb66511454952f38f0adb6
Natalia Braz <naabraz@gmail.com>
2022-06-10 10:51:24 -0300

    fix(labnative): packages adjustment

--- packages/labnative/babel.config.js
+++ packages/labnative/babel.config.js
@@ -1,4 +1,39 @@
+const path = require('path');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['import-glob', 'react-native-reanimated/plugin'],
+  plugins: [
+    'import-glob',
+    [
+      'module-resolver',
+      {
+        alias: {
+          react: require.resolve('react', {
+            paths: [path.join(__dirname, './')],
+          }),
+          '^react-native$': require.resolve('react-native', {
+            paths: [path.join(__dirname, './')],
+          }),
+          '^react-native/(.+)': ([, name]) =>
+            require.resolve(`react-native/${name}`, {
+              paths: [path.join(__dirname, './')],
+            }),
+        },
+        extensions: [
+          '.ios.js',
+          '.ios.ts',
+          '.ios.tsx',
+          '.android.js',
+          '.android.ts',
+          '.android.tsx',
+          '.native.js',
+          '.native.ts',
+          '.native.tsx',
+          '.js',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    ],
+  ],
 };
