c0f06f666e928fca86419860ef5b008f48a3c4a5
Caio Tracera <ctrcra@gmail.com>
2022-07-13 16:20:15 -0300

    fix(yoga-illustrations): fix missing readme and broken components

---
+++ packages/illustrations/readme.md
@@ -0,0 +1,25 @@
+# Yoga Illustrations
+
+## Instalation
+
+```
+yarn add @gympass/yoga-illustrations
+```
+
+`@gympass/yoga-illustrations` is a package containing all illustrations used in `@gympass/yoga` package.
+
+### React native
+
+To use in `react-native` make sure you have installed [react-native-svg](https://github.com/react-native-community/react-native-svg).
+
+## Usage
+
+Icon works out of the box for both platforms.
+
+```
+import { Img2d01 } from '@gympass/yoga-illustrations';
+
+export default () => <img2d01  />;
+```
+
+Here you can check an example in [web](https://gympass.github.io/yoga/components/illustrations#web) and [native](https://gympass.github.io/yoga/components/illustrations#native) platforms.
--- packages/illustrations/src/index.js
+++ packages/illustrations/src/index.js
@@ -1,27 +1,27 @@
-import img2d01 from './svg/img2d01.svg';
-import img2d02 from './svg/img2d02.svg';
-import img2d03 from './svg/img2d03.svg';
-import img2d04 from './svg/img2d04.svg';
-import img2d05 from './svg/img2d05.svg';
-import img2d06 from './svg/img2d06.svg';
-import img2d07 from './svg/img2d07.svg';
-import img2d08 from './svg/img2d08.svg';
-import img2d09 from './svg/img2d09.svg';
-import img2d10 from './svg/img2d10.svg';
-import img2d11 from './svg/img2d11.svg';
-import img2d12 from './svg/img2d12.svg';
+import Img2d01 from './svg/img2d01.svg';
+import Img2d02 from './svg/img2d02.svg';
+import Img2d03 from './svg/img2d03.svg';
+import Img2d04 from './svg/img2d04.svg';
+import Img2d05 from './svg/img2d05.svg';
+import Img2d06 from './svg/img2d06.svg';
+import Img2d07 from './svg/img2d07.svg';
+import Img2d08 from './svg/img2d08.svg';
+import Img2d09 from './svg/img2d09.svg';
+import Img2d10 from './svg/img2d10.svg';
+import Img2d11 from './svg/img2d11.svg';
+import Img2d12 from './svg/img2d12.svg';
 
 export {
-  img2d01,
-  img2d02,
-  img2d03,
-  img2d04,
-  img2d05,
-  img2d06,
-  img2d07,
-  img2d08,
-  img2d09,
-  img2d10,
-  img2d11,
-  img2d12,
+  Img2d01,
+  Img2d02,
+  Img2d03,
+  Img2d04,
+  Img2d05,
+  Img2d06,
+  Img2d07,
+  Img2d08,
+  Img2d09,
+  Img2d10,
+  Img2d11,
+  Img2d12,
 };
