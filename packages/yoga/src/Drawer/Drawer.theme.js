7fe54a02b7fcdc3586443d0368f63d774e542978
Guilherme Cardoso <guilherme.cardoso@gympass.com>
2022-08-02 16:23:46 -0300

    feat(drawer): create a intial develop of drawer

---
+++ packages/yoga/src/Drawer/Drawer.theme.js
@@ -0,0 +1,19 @@
+const Drawer = ({ spacing }) => ({
+  padding: {
+    top: spacing.xxxlarge,
+    right: spacing.xxxlarge,
+    bottom: spacing.xhuge,
+    left: spacing.xxxlarge,
+  },
+  width: {
+    default: 600,
+  },
+  // height: {
+  //   min: 160,
+  // },
+  // position: {
+  //   default: spacing.zero,
+  // },
+});
+
+export default Drawer;
---
+++ packages/yoga/src/Drawer/web/Drawer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import { node } from 'prop-types';
+import Dialog from '../../Dialog';
+
+const StyledDrawer = styled(Dialog)`
+  ${({
+    theme: {
+      yoga: {
+        components: { drawer },
+      },
+    },
+  }) => `
+  padding: ${drawer.padding.top}px ${drawer.padding.right}px ${drawer.padding.bottom}px ${drawer.padding.left}px;
+  width: ${drawer.width.default}px;
+  `}
+  border-radius: 0;
+  height: 100%;
+  align-self: flex-end;
+  position: absolute;
+  right: 0;
+  animation: content;
+  animation-duration: 400ms;
+  animation-fill-mode: forwards;
+  transition: 0.25s ease-in-out;
+  @keyframes content {
+    0% {
+      transform: translate3d(100%, 0, 0);
+    }
+  }
+`;
+
+const Drawer = props => <StyledDrawer {...props} />;
+
+Drawer.propTypes = {
+  children: node.isRequired,
+};
+
+export default Drawer;
