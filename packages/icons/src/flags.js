331d46ae0b6536c61dd407bd3238dbc20d3c3dcb
Matheus Henrique <matheushds.br@gmail.com>
2022-09-01 14:15:37 -0300

    docs(icons): modularize flag icons and ui icons

---
+++ packages/icons/src/flags.js
@@ -0,0 +1,25 @@
+import FlagArgentina from './svg/flag_argentina.svg';
+import FlagBrazil from './svg/flag_brazil.svg';
+import FlagChile from './svg/flag_chile.svg';
+import FlagGermany from './svg/flag_germany.svg';
+import FlagIreland from './svg/flag_ireland.svg';
+import FlagItaly from './svg/flag_italy.svg';
+import FlagMexico from './svg/flag_mexico.svg';
+import FlagPortugal from './svg/flag_portugal.svg';
+import FlagSpain from './svg/flag_spain.svg';
+import FlagUK from './svg/flag_uk.svg';
+import FlagUS from './svg/flag_us.svg';
+
+export {
+  FlagArgentina,
+  FlagBrazil,
+  FlagChile,
+  FlagGermany,
+  FlagIreland,
+  FlagItaly,
+  FlagMexico,
+  FlagPortugal,
+  FlagSpain,
+  FlagUK,
+  FlagUS,
+};
