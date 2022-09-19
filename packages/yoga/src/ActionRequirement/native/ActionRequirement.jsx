4e75b0554dd45050843994cd7c26ad93db2dc62e
Guilherme Cardoso <guilherme.cardoso@gympass.com>
2022-09-19 17:46:15 -0300

    feat(actionrequirement): add action requirement mobile

---
+++ packages/yoga/src/ActionRequirement/native/ActionRequirement.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { arrayOf, node, oneOfType, string } from 'prop-types';
+import styled from 'styled-components/native';
+import { View } from 'react-native';
+import Box from '../../Box';
+import { Title } from './ActionRequirementStyles';
+import Text from '../../Text';
+
+const StyledActionRequirement = styled.View``;
+
+function ActionRequirement(props) {
+  const { title, description, children, checkable, illustration, list } = props;
+
+  return (
+    <StyledActionRequirement {...props}>
+      {illustration && <Box>{illustration}</Box>}
+      <Title>{title}</Title>
+      <Text mt="small" color="deep">
+        {description}
+      </Text>
+      <Text mt="small" color="deep">
+        {list && <Box mt="xxxlarge">{list}</Box>}
+      </Text>
+      {checkable && <Box mt="xxxlarge">{checkable}</Box>}
+      <View>{children}</View>
+    </StyledActionRequirement>
+  );
+}
+
+ActionRequirement.propTypes = {
+  title: string.isRequired,
+  children: oneOfType([arrayOf(node), node]),
+  description: string.isRequired,
+  checkable: oneOfType([arrayOf(node), node]),
+  illustration: oneOfType([arrayOf(node), node]),
+  list: oneOfType([arrayOf(node), node]),
+};
+
+ActionRequirement.defaultProps = {
+  children: undefined,
+  checkable: undefined,
+  illustration: undefined,
+  list: undefined,
+};
+
+export default ActionRequirement;
