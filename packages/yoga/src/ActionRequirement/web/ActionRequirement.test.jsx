a16448f825d40380a197764af63c0e71a8e10c5c
Dani Poquet <daniel.poquet@gympass.com>
2022-10-07 15:22:10 +0200

    fix: remove empty paragraph if is not needed

--- packages/yoga/src/ActionRequirement/web/ActionRequirement.test.jsx
+++ packages/yoga/src/ActionRequirement/web/ActionRequirement.test.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
-import { ThemeProvider } from '../..';
+import { Text } from '@gympass/yoga';
+import { Dumbbell, User, Star } from '@gympass/yoga-icons';
 import ActionRequirement from './ActionRequirement';
+import { ThemeProvider } from '../..';
+
+function handleGetList() {
+  return (
+    <>
+      <Text>
+        <Dumbbell /> 1x/day standard access
+      </Text>
+      <Text mt="small">
+        <User /> 4x/month 1-on-1 sessions
+      </Text>
+      <Text mt="small">
+        <Star /> 2x/month premium classes
+      </Text>
+    </>
+  );
+}
 
 describe('<ActionRequirement />', () => {
-  it('should match snapshot', () => {
+  it('should default match snapshot', () => {
     const { container } = render(
       <ThemeProvider>
         <ActionRequirement />
+      </ThemeProvider>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot with list', () => {
+    const { container } = render(
+      <ThemeProvider>
+        <ActionRequirement list={handleGetList()} />
       </ThemeProvider>,
     );
 
