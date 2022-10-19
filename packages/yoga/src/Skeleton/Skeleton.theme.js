fe4216e305ece1abd1ed5d7636c7d0da2375f9f2
Eric C Leão <ericcleao@gmail.com>
2022-10-19 12:40:04 +0100

    feat(skeleton): update component theme file adding border and height theme values

--- packages/yoga/src/Skeleton/Skeleton.theme.js
+++ packages/yoga/src/Skeleton/Skeleton.theme.js
@@ -1,3 +1,20 @@
-const Skeleton = () => ({});
+const Skeleton = ({ lineHeights, radii }) => ({
+  border: {
+    circular: radii.circle,
+  },
+  height: {
+    text: {
+      h1: lineHeights.huge,
+      h2: lineHeights.xxxlarge,
+      h3: lineHeights.xxlarge,
+      h4: lineHeights.xlarge,
+      h5: lineHeights.large,
+      body1: lineHeights.medium,
+      body2: lineHeights.small,
+      overline: lineHeights.xsmall,
+      exception: lineHeights.xxsmall,
+    },
+  },
+});
 
 export default Skeleton;
