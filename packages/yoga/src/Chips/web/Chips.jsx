3bd2f6f3d35307808c7d5778315fca4759f52b33
Caio Augusto <caio.augusto@gympass.com>
2023-01-09 16:47:18 -0300

    feat(chips): add testId

--- packages/yoga/src/Chips/web/Chips.jsx
+++ packages/yoga/src/Chips/web/Chips.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styled, { withTheme } from 'styled-components';
-import { node, number, arrayOf, bool, func, oneOfType } from 'prop-types';
+import {
+  node,
+  number,
+  arrayOf,
+  bool,
+  func,
+  oneOfType,
+  string,
+} from 'prop-types';
 import { hexToRgb } from '@gympass/yoga-common';
 
 import { theme } from '../../Theme';
@@ -119,6 +127,7 @@ const Chips = React.forwardRef(
       disabled,
       onToggle,
       onClick = onToggle,
+      testId,
       theme: {
         yoga: { spacing },
       },
@@ -134,6 +143,7 @@ const Chips = React.forwardRef(
         disabled={disabled}
         onClick={onClick}
         ref={ref}
+        data-testid={testId}
         {...props}
       >
         {SecondIcon && (
@@ -180,6 +190,7 @@ Chips.propTypes = {
   /** click event */
   onToggle: func,
   onClick: func,
+  testId: string,
 };
 
 Chips.defaultProps = {
@@ -190,6 +201,7 @@ Chips.defaultProps = {
   icons: [],
   onToggle: undefined,
   onClick: undefined,
+  testId: undefined,
 };
 
 export default withTheme(Chips);
