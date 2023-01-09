f4bb620d03442f55bcfea0a60a4b77d9635b7e81
Caio Augusto <caio.augusto@gympass.com>
2023-01-09 16:37:28 -0300

    feat(box): add testId

--- packages/yoga/src/Box/web/Box.jsx
+++ packages/yoga/src/Box/web/Box.jsx
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 import { system } from '@gympass/yoga-system';
+import { string } from 'prop-types';
 
-const Box = styled.div(system);
+const Box = styled.div.attrs(({ testId }) => ({
+  'data-testid': testId,
+}))(system);
+
+Box.propTypes = {
+  testId: string,
+};
+
+Box.defaultProps = {
+  testId: undefined,
+};
 
 export default Box;
