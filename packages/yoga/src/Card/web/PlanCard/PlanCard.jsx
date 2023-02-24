a58e1c0d5aefe07d18d9f80184c3f22b63a39e7c
Leonardo Luiz <leonardokl@hotmail.com>
2023-02-24 15:02:52 -0300

    feat(PlanCard): add discount

--- packages/yoga/src/Card/web/PlanCard/PlanCard.jsx
+++ packages/yoga/src/Card/web/PlanCard/PlanCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { node, oneOf } from 'prop-types';
+import { node, oneOf, string } from 'prop-types';
+import Box from '../../../Box';
+import Text from '../../../Text';
 
 export const PLAN_LINE_HEIGHT = 4;
 
@@ -47,15 +49,64 @@ const Border = styled.span`
   }) => color};
 `;
 
-const PlanCard = ({ children, variant, ...rest }) => (
-  <Plan {...rest}>
-    <Border variant={variant} />
-    {children}
-  </Plan>
-);
+const DiscountWrapper = styled(Box).attrs({
+  ph: 'xsmall',
+  pv: 'xxxsmall',
+})`
+  ${({ bg, theme }) => {
+    const MIN_WRAPPER_HEIGHT = 28;
+    const baseCurveStyle = css`
+      content: '';
+      position: absolute;
+      left: -${MIN_WRAPPER_HEIGHT}px;
+      height: ${MIN_WRAPPER_HEIGHT}px;
+      width: ${MIN_WRAPPER_HEIGHT}px;
+    `;
+
+    return css`
+      position: absolute;
+      top: 0;
+      right: 0;
+      display: flex;
+      align-items: center;
+      min-height: ${MIN_WRAPPER_HEIGHT}px;
+      border-bottom-left-radius: 16px;
+
+      ::before {
+        ${baseCurveStyle}
+        top: 0;
+        background: ${theme.yoga.colors[bg]};
+      }
+
+      ::after {
+        ${baseCurveStyle}
+        top: ${PLAN_LINE_HEIGHT}px;
+        background: ${theme.yoga.colors.white};
+        border-top-right-radius: ${MIN_WRAPPER_HEIGHT / 2 - PLAN_LINE_HEIGHT}px;
+      }
+    `;
+  }}
+`;
+
+function PlanCard({ children, discount, variant, ...rest }) {
+  return (
+    <Plan {...rest}>
+      {discount && (
+        <DiscountWrapper bg={variant}>
+          <Text.Tiny color="white" fontWeight="medium">
+            {discount}
+          </Text.Tiny>
+        </DiscountWrapper>
+      )}
+      <Border variant={variant} />
+      {children}
+    </Plan>
+  );
+}
 
 PlanCard.propTypes = {
   children: node,
+  discount: string,
   /** style the card border top color following the theme (primary, secondary,
    * vibin, hope, energy, relax, peace, verve, uplift, deepPurple, deep,
    * stamina, dark, medium, light, clear, white) */
@@ -82,6 +133,7 @@ PlanCard.propTypes = {
 
 PlanCard.defaultProps = {
   children: undefined,
+  discount: undefined,
   variant: 'deepPurple',
 };
 
