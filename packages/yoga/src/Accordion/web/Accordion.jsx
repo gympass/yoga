58c7f244fb8cf17f6fed976aaf4b4b0cb03f615b
Tomás Keong <tomas.keong@gympass.com>
2023-04-03 08:00:56 +0100

    fix(Accordion): move styled components outside the component render

--- packages/yoga/src/Accordion/web/Accordion.jsx
+++ packages/yoga/src/Accordion/web/Accordion.jsx
@@ -5,6 +5,198 @@ import { ChevronDown } from '@gympass/yoga-icons';
 import { Text, Divider } from '@gympass/yoga';
 import Content from './Content';
 
+const AccordionWrapper = styled.div`
+  border: none;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  z-index: 1;
+
+  ${({
+    theme: {
+      yoga: {
+        colors: { white, elements },
+      },
+    },
+    disabled,
+  }) => {
+    return `
+  background: ${disabled ? elements.backgroundAndDisabled : white}
+`;
+  }}
+
+  hr {
+    bottom: 0;
+    left: 0;
+    margin: 0;
+    position: absolute;
+    z-index: -1;
+  }
+`;
+
+const getVerticalPadding = (accordion, small) => {
+  if (small) return accordion.padding.small;
+  return accordion.padding.large;
+};
+
+const getHorizontalPadding = (accordion, hasHorizontalPadding) => {
+  if (hasHorizontalPadding) return accordion.padding.standard;
+  return accordion.padding.zero;
+};
+
+const Header = styled.button`
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  ${({
+    theme: {
+      yoga: {
+        components: { accordion },
+      },
+    },
+    small,
+    hasHorizontalPadding,
+  }) => {
+    return `
+  padding:${getVerticalPadding(accordion, small)}px ${getHorizontalPadding(
+      accordion,
+      hasHorizontalPadding,
+    )}px;
+`;
+  }}
+
+  &:focus-visible {
+    ${({
+      theme: {
+        yoga: {
+          colors: { elements },
+        },
+      },
+    }) => {
+      return `
+    background: ${elements.backgroundAndDisabled};
+
+  `;
+    }}
+  }
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      cursor: not-allowed;
+    `}
+`;
+
+const Title = styled.div`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+
+  ${({
+    theme: {
+      yoga: {
+        components: { accordion },
+      },
+    },
+    subtitle,
+  }) => {
+    return `
+  gap: ${subtitle ? accordion.gap.header : 0}px;
+  margin: ${subtitle ? 0 : `${accordion.paddingArrow.total}px 0`};
+`;
+  }}
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      ${({
+        theme: {
+          yoga: {
+            colors: { text },
+          },
+        },
+      }) => {
+        return `
+      color: ${text.secundary};
+    `;
+      }}
+    `}
+`;
+
+const AccordionContent = styled.div`
+  height: auto;
+  max-height: ${({ isOpen }) => (isOpen ? '9999px' : '0')};
+  overflow: hidden;
+  transition: max-height 200ms ease-in-out;
+
+  ${({
+    theme: {
+      yoga: {
+        components: { accordion },
+      },
+    },
+    small,
+  }) =>
+    small &&
+    css`
+      ${Content} {
+        padding-bottom: ${accordion.padding.small}px;
+      }
+    `}
+
+  ${({ hasHorizontalPadding }) =>
+    !hasHorizontalPadding &&
+    css`
+      ${Content} {
+        padding-left: 0;
+        padding-right: 0;
+      }
+    `}
+`;
+
+const ArrowWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  svg {
+    transform: ${({ isOpen }) => (isOpen ? 'rotate(180deg)' : 'rotate(0deg)')};
+    transition: all 200ms ease-out 0s;
+
+    ${({
+      theme: {
+        yoga: {
+          colors: { primary, text },
+        },
+      },
+      disabled,
+    }) => {
+      return `
+      fill: ${disabled ? text.secondary : primary};
+    `;
+    }}
+  }
+
+  ${({
+    theme: {
+      yoga: {
+        components: { accordion },
+      },
+    },
+  }) => {
+    return `
+  padding: ${accordion.paddingArrow.total}px;
+`;
+  }}
+`;
+
 const Accordion = ({
   title,
   subtitle,
@@ -17,198 +209,17 @@ const Accordion = ({
 }) => {
   const [open, setOpen] = useState(expanded);
 
-  const getVerticalPadding = accordion => {
-    if (small) return accordion.padding.small;
-    return accordion.padding.large;
-  };
-
-  const getHorizontalPadding = accordion => {
-    if (hasHorizontalPadding) return accordion.padding.standard;
-    return accordion.padding.zero;
-  };
-
-  const AccordionWrapper = styled.div`
-    border: none;
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    width: 100%;
-    z-index: 1;
-
-    ${({
-      theme: {
-        yoga: {
-          colors: { white, elements },
-        },
-      },
-    }) => {
-      return `
-      background: ${disabled ? elements.backgroundAndDisabled : white}
-    `;
-    }}
-
-    hr {
-      bottom: 0;
-      left: 0;
-      margin: 0;
-      position: absolute;
-      z-index: -1;
-    }
-  `;
-
-  const Header = styled.button`
-    align-items: center;
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-
-    ${({
-      theme: {
-        yoga: {
-          components: { accordion },
-        },
-      },
-    }) => {
-      return `
-      padding:${getVerticalPadding(accordion)}px ${getHorizontalPadding(
-        accordion,
-      )}px;
-    `;
-    }}
-
-    &:focus-visible {
-      ${({
-        theme: {
-          yoga: {
-            colors: { elements },
-          },
-        },
-      }) => {
-        return `
-        background: ${elements.backgroundAndDisabled};
-
-      `;
-      }}
-    }
-
-    ${disabled &&
-    css`
-      cursor: not-allowed;
-    `}
-  `;
-
-  const Title = styled.div`
-    align-items: flex-start;
-    display: flex;
-    flex-direction: column;
-    text-align: start;
-
-    ${({
-      theme: {
-        yoga: {
-          components: { accordion },
-        },
-      },
-    }) => {
-      return `
-      gap: ${subtitle ? accordion.gap.header : 0}px;
-      margin: ${subtitle ? 0 : `${accordion.paddingArrow.total}px 0`};
-    `;
-    }}
-
-    ${disabled &&
-    css`
-      ${({
-        theme: {
-          yoga: {
-            colors: { text },
-          },
-        },
-      }) => {
-        return `
-          color: ${text.secundary};
-        `;
-      }}
-    `}
-  `;
-
-  const AccordionContent = styled.div`
-    height: auto;
-    max-height: ${({ isOpen }) => (isOpen ? '9999px' : '0')};
-    overflow: hidden;
-    transition: max-height 200ms ease-in-out;
-
-    ${({
-      theme: {
-        yoga: {
-          components: { accordion },
-        },
-      },
-    }) =>
-      small &&
-      css`
-        ${Content} {
-          padding-bottom: ${accordion.padding.small}px;
-        }
-      `}
-
-    ${!hasHorizontalPadding &&
-    css`
-      ${Content} {
-        padding-left: 0;
-        padding-right: 0;
-      }
-    `}
-  `;
-
-  const ArrowWrapper = styled.div`
-    align-items: center;
-    display: flex;
-    justify-content: center;
-
-    svg {
-      transform: ${({ isOpen }) =>
-        isOpen ? 'rotate(180deg)' : 'rotate(0deg)'};
-      transition: all 200ms ease-out 0s;
-
-      ${({
-        theme: {
-          yoga: {
-            colors: { primary, text },
-          },
-        },
-      }) => {
-        return `
-          fill: ${disabled ? text.secondary : primary};
-        `;
-      }}
-    }
-
-    ${({
-      theme: {
-        yoga: {
-          components: { accordion },
-        },
-      },
-    }) => {
-      return `
-      padding: ${accordion.paddingArrow.total}px;
-    `;
-    }}
-  `;
-
   return (
-    <AccordionWrapper {...props}>
+    <AccordionWrapper disabled={disabled} {...props}>
       <Header
         disabled={disabled}
         onClick={() => {
           setOpen(!open);
         }}
+        small={small}
+        hasHorizontalPadding={hasHorizontalPadding}
       >
-        <Title>
+        <Title subtitle={subtitle}>
           {small ? (
             <Text.Small color={disabled ? 'deep' : undefined}>
               {title}
@@ -225,12 +236,18 @@ const Accordion = ({
           <Text.Small color="deep">{subtitle}</Text.Small>
         </Title>
 
-        <ArrowWrapper isOpen={open}>
+        <ArrowWrapper isOpen={open} disabled={disabled}>
           <ChevronDown width={24} height={24} />
         </ArrowWrapper>
       </Header>
 
-      <AccordionContent isOpen={open}>{children}</AccordionContent>
+      <AccordionContent
+        isOpen={open}
+        small={small}
+        hasHorizontalPadding={hasHorizontalPadding}
+      >
+        {children}
+      </AccordionContent>
       <Divider />
     </AccordionWrapper>
   );
