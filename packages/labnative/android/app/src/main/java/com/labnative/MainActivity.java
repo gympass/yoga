b5e345f8633b42f982063071980588070ff33977
Rafael Rollo <rafaelrollo92@gmail.com>
2023-06-06 17:50:16 +0100

    chore(labnative): upgrades to react native v0.69.10

--- packages/labnative/android/app/src/main/java/com/labnative/MainActivity.java
+++ packages/labnative/android/app/src/main/java/com/labnative/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends ReactActivity {
 
     /**
      * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-     * you can specify the rendered you wish to use (Fabric or the older renderer).
+     * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
+     * (Paper).
      */
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
@@ -26,12 +27,20 @@ public class MainActivity extends ReactActivity {
         public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
             super(activity, mainComponentName);
         }
+
         @Override
         protected ReactRootView createRootView() {
             ReactRootView reactRootView = new ReactRootView(getContext());
             // If you opted-in for the New Architecture, we enable the Fabric Renderer.
             reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
             return reactRootView;
+        }
+
+        @Override
+        protected boolean isConcurrentRootEnabled() {
+          // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
+          // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
         }
     }
 }
--- packages/labnative/android/app/src/main/java/com/labnative/newarchitecture/MainApplicationReactNativeHost.java
+++ packages/labnative/android/app/src/main/java/com/labnative/newarchitecture/MainApplicationReactNativeHost.java
@@ -16,8 +16,8 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.UIManager;
 import com.facebook.react.fabric.ComponentFactory;
 import com.facebook.react.fabric.CoreComponentsRegistry;
-import com.facebook.react.fabric.EmptyReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
+import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
 import com.labnative.BuildConfig;
 import com.labnative.newarchitecture.components.MainComponentsRegistry;
@@ -94,7 +94,7 @@ public class MainApplicationReactNativeHost extends ReactNativeHost {
                                 return new FabricJSIModuleProvider(
                                         reactApplicationContext,
                                         componentFactory,
-                                        new EmptyReactNativeConfig(),
+                                        ReactNativeConfig.DEFAULT_CONFIG,
                                         viewManagerRegistry);
                             }
                         });
