ca00ae877a4a264a727da6fb1ae989f0d01a5697
João Victor <joaovicdsantos@gmail.com>
2023-07-31 17:16:43 -0300

    test lint

--- packages/yoga/src/Stepper/native/Dots.jsx
+++ packages/yoga/src/Stepper/native/Dots.jsx
@@ -42,25 +42,25 @@ const Dot = styled.View(
 const Label = styled(Text.Bold)(
   ({
     active,
-  secondary,
+    secondary,
     theme: {
       yoga: {
         components: { stepper },
       },
     },
-}) => {
-  const state = secondary ? 'secondary' : 'active';
+  }) => {
+    const state = secondary ? 'secondary' : 'active';
 
-  return css`
-    width: 95px;
-    margin-top: 10px;
-    margin-left: -40px;
-    color: ${active
-      ? stepper.label.color[state]
-      : stepper.label.color.inactive};
-    font-size: ${stepper.label.font.size}px;
-    text-align: center;
-  `;
+    return css`
+      width: 95px;
+      margin-top: 10px;
+      margin-left: -40px;
+      color: ${active
+        ? stepper.label.color[state]
+        : stepper.label.color.inactive};
+      font-size: ${stepper.label.font.size}px;
+      text-align: center;
+    `;
   },
 );
 
