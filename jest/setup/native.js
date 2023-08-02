2dcf21cfbdbf6e69d131e06550745de6703ac290
Rafael Rollo <rafaelrollo92@gmail.com>
2023-08-02 17:32:18 +0100

    build(native): fixes test issues on ci

--- jest/setup/native.js
+++ jest/setup/native.js
@@ -1,5 +1,17 @@
 import '@testing-library/jest-native/extend-expect';
 
+const frameTime = 10;
+
+/**
+ * This fix came to help with the RN's jest setup requestAnimationFrame issue that
+ * was affecting native's components tests that relate with animations.
+ * At the link below we can find more detailed description.
+ *  * https://stackoverflow.com/questions/42268673/jest-test-animated-view-for-react-native-app/51067606#51067606
+ */
+global.requestAnimationFrame = cb => {
+  setTimeout(cb, frameTime);
+};
+
 /**
  * This fix was originally proposed by the package react-16-node-hanging-test-fix
  * (https://npmjs.com/package/react-16-node-hanging-test-fix). Actually, the problem
