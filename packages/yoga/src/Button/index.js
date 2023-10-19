52d65561f8e4297432036d4b745c154525eb3af3
Matheus Henrique <matheushds.br@gmail.com>
2023-10-19 15:12:17 -0300

    refactor(button-loading): remove button loading

--- packages/yoga/src/Button/index.js
+++ packages/yoga/src/Button/index.js
@@ -3,12 +3,10 @@ import ButtonOutline from './web/Outline';
 import ButtonText from './web/Text';
 import ButtonLink from './web/Link';
 import ButtonIcon from './web/Icon';
-import ButtonLoading from './web/Loading';
 
 Button.Outline = ButtonOutline;
 Button.Text = ButtonText;
 Button.Link = ButtonLink;
 Button.Icon = ButtonIcon;
-Button.Loading = ButtonLoading;
 
 export default Button;
