00ff94be8949f56f2a6afcee867e35fb13ac1872
Kaic Bastidas <kaic.bastidas@gympass.com>
2024-01-16 16:36:33 +0000

    refactor: convert themeGenerator

--- packages/yoga/src/Theme/helpers/themeGenerator/themeGenerator.ts
+++ packages/yoga/src/Theme/helpers/themeGenerator/themeGenerator.ts
@@ -1,14 +1,20 @@
+import YogaTokens from '../../../../../tokens';
+
+type ThemeVars = {
+  (tokens: typeof YogaTokens): {};
+  isFromThemeBuilder?: boolean;
+};
+
 /**
  * Gets a function that one could sent to \<ThemeProvider> in order to inject a custom thme (or sets of tokens)
- * @param {themeVars} themeVars - should return an object with a similar scheme of our tokens that will be replaced, can receive a tokens parameter
- * @returns {themeVars} themeVars - will return the same function which is injected to \<ThemeProvider>
  */
-function createTheme(themeVars) {
+function createTheme(themeVars: ThemeVars) {
   if (typeof themeVars !== 'function')
     throw new TypeError(
       `Invalid param 'themeVars', expected 'function', received '${typeof themeVars}'.`,
     );
 
+  console.log('themeVars', themeVars);
   const themeBuilder = themeVars.bind({});
 
   themeBuilder.isFromThemeBuilder = true;
@@ -18,8 +24,5 @@ function createTheme(themeVars) {
 
 /**
  * This callback is displayed as part of the Requester class.
- * @callback themeVars
- * @param {Object.<string, string | number>} tokens - object containing Yoga tokens
  */
-
 export default createTheme;
