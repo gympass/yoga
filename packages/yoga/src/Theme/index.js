ba50375ae8308844d5d58c319dee1ef89ae5e37b
Kaic Bastidas <kaic.bastidas@gympass.com>
2024-01-19 15:26:17 +0000

    style: codacy

--- packages/yoga/src/Theme/index.js
+++ packages/yoga/src/Theme/index.js
@@ -3,11 +3,6 @@ import yogaTheme, { v3theme } from './theme';
 import theme from './helpers/themeReader';
 import createTheme from './helpers/themeGenerator';
 
-export {
-  ThemeProvider as default,
-  FontLoader,
-  yogaTheme,
-  theme,
-  createTheme,
-  v3theme,
-};
+export { ThemeProvider, FontLoader, yogaTheme, theme, createTheme, v3theme };
+
+export default ThemeProvider;
