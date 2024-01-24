a5355bcf43da9bb7cf30f9332575d3b62cadeb07
Kaic Bastidas <kaic.bastidas@gympass.com>
2024-01-24 20:25:50 +0000

    refactor: use `ts-deepmerge`

--- packages/common/src/merge.ts
+++ packages/common/src/merge.ts
@@ -1,17 +1,3 @@
-// import mergeLib from 'ts-deepmerge';
-
-const merge = (target, source) => {
-  const newTarget = { ...target };
-
-  Object.keys(source).forEach(key => {
-    newTarget[key] = source[key];
-
-    if (source[key] !== null && typeof source[key] === 'object') {
-      newTarget[key] = merge(target[key] || {}, newTarget[key]);
-    }
-  });
-
-  return newTarget;
-};
+import merge from 'ts-deepmerge';
 
 export default merge;
