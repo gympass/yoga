a930ded2d76c6634d67faa982c26c00904009640
Diogo Verde <diogo.verde05@gmail.com>
2024-02-23 14:23:23 +0000

    feat: update flag icons and add new ones

--- packages/icons/src/flags.js
+++ packages/icons/src/flags.js
@@ -1,28 +1,98 @@
 import FlagArgentina from './svg/flag_argentina.svg';
+import FlagAustralia from './svg/flag_australia.svg';
+import FlagAustria from './svg/flag_austria.svg';
+import FlagBelgium from './svg/flag_belgium.svg';
 import FlagBrazil from './svg/flag_brazil.svg';
+import FlagBulgary from './svg/flag_bulgary.svg';
+import FlagCanada from './svg/flag_canada.svg';
 import FlagChile from './svg/flag_chile.svg';
+import FlagChina from './svg/flag_china.svg';
+import FlagColombia from './svg/flag_colombia.svg';
+import FlagCostaRica from './svg/flag_costarica.svg';
+import FlagCroatia from './svg/flag_croatia.svg';
+import FlagCzechRepublic from './svg/flag_czechrepublic.svg';
+import FlagDenmark from './svg/flag_denmark.svg';
+import FlagDominicanRepublic from './svg/flag_dominicanrepublic.svg';
+import FlagEcuador from './svg/flag_ecuador.svg';
+import FlagElSalvador from './svg/flag_elsalvador.svg';
+import FlagFinland from './svg/flag_finland.svg';
+import FlagFrance from './svg/flag_france.svg';
 import FlagGermany from './svg/flag_germany.svg';
+import FlagGuatemala from './svg/flag_guatemala.svg';
+import FlagHonduras from './svg/flag_honduras.svg';
+import FlagHungary from './svg/flag_hungary.svg';
+import FlagIndia from './svg/flag_india.svg';
+import FlagIndonesia from './svg/flag_indonesia.svg';
 import FlagIreland from './svg/flag_ireland.svg';
+import FlagIsrael from './svg/flag_israel.svg';
 import FlagItaly from './svg/flag_italy.svg';
+import FlagJapan from './svg/flag_japan.svg';
+import FlagLuxembourg from './svg/flag_luxembourg.svg';
 import FlagMexico from './svg/flag_mexico.svg';
 import FlagNetherlands from './svg/flag_netherlands.svg';
+import FlagPanama from './svg/flag_panama.svg';
+import FlagParaguay from './svg/flag_paraguay.svg';
+import FlagPeru from './svg/flag_peru.svg';
+import FlagPoland from './svg/flag_poland.svg';
 import FlagPortugal from './svg/flag_portugal.svg';
+import FlagPuertoRico from './svg/flag_puertorico.svg';
+import FlagSingapore from './svg/flag_singapore.svg';
+import FlagSouthAfrica from './svg/flag_southafrica.svg';
+import FlagSouthKorea from './svg/flag_southkorea.svg';
 import FlagSpain from './svg/flag_spain.svg';
+import FlagSweden from './svg/flag_sweden.svg';
+import FlagSwitzerland from './svg/flag_switzerland.svg';
+import FlagTurkey from './svg/flag_turkey.svg';
 import FlagUK from './svg/flag_uk.svg';
 import FlagUruguay from './svg/flag_uruguay.svg';
 import FlagUS from './svg/flag_us.svg';
 
 export {
   FlagArgentina,
+  FlagAustralia,
+  FlagAustria,
+  FlagBelgium,
   FlagBrazil,
+  FlagBulgary,
+  FlagCanada,
   FlagChile,
+  FlagChina,
+  FlagColombia,
+  FlagCostaRica,
+  FlagCroatia,
+  FlagCzechRepublic,
+  FlagDenmark,
+  FlagDominicanRepublic,
+  FlagEcuador,
+  FlagElSalvador,
+  FlagFinland,
+  FlagFrance,
   FlagGermany,
+  FlagGuatemala,
+  FlagHonduras,
+  FlagHungary,
+  FlagIndia,
+  FlagIndonesia,
   FlagIreland,
+  FlagIsrael,
   FlagItaly,
+  FlagJapan,
+  FlagLuxembourg,
   FlagMexico,
   FlagNetherlands,
+  FlagPanama,
+  FlagParaguay,
+  FlagPeru,
+  FlagPoland,
   FlagPortugal,
+  FlagPuertoRico,
+  FlagSingapore,
+  FlagSouthAfrica,
+  FlagSouthKorea,
   FlagSpain,
+  FlagSweden,
+  FlagSwitzerland,
+  FlagTurkey,
   FlagUK,
   FlagUruguay,
   FlagUS,
