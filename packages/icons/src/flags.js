f3f0eaa800ff79d698f446b78a48af47e0d7fa46
Dario-Felipe <dario.felipe@gympass.com>
2024-03-14 12:24:54 -0300

    feat(icons): add third wave of new countries flag, adjust imports and order by name

--- packages/icons/src/flags.js
+++ packages/icons/src/flags.js
@@ -1,8 +1,15 @@
+import FlagAlgeria from './svg/flag_algeria.svg';
 import FlagArgentina from './svg/flag_argentina.svg';
+import FlagArmenia from './svg/flag_armenia.svg';
 import FlagAustralia from './svg/flag_australia.svg';
 import FlagAustria from './svg/flag_austria.svg';
+import FlagBarbados from './svg/flag_barbados.svg';
+import FlagBelarus from './svg/flag_belarus.svg';
 import FlagBelgium from './svg/flag_belgium.svg';
+import FlagBoliva from './svg/flag_boliva.svg';
+import FlagBosnia from './svg/flag_bosnia.svg';
 import FlagBrazil from './svg/flag_brazil.svg';
+import FlagBritishVirginIslands from './svg/flag_britishvirginislands.svg';
 import FlagBulgary from './svg/flag_bulgary.svg';
 import FlagCanada from './svg/flag_canada.svg';
 import FlagChile from './svg/flag_chile.svg';
@@ -10,89 +17,94 @@ import FlagChina from './svg/flag_china.svg';
 import FlagColombia from './svg/flag_colombia.svg';
 import FlagCostaRica from './svg/flag_costarica.svg';
 import FlagCroatia from './svg/flag_croatia.svg';
+import FlagCyprus from './svg/flag_cyprus.svg';
 import FlagCzechRepublic from './svg/flag_czechrepublic.svg';
 import FlagDenmark from './svg/flag_denmark.svg';
 import FlagDominicanRepublic from './svg/flag_dominicanrepublic.svg';
 import FlagEcuador from './svg/flag_ecuador.svg';
+import FlagEgypt from './svg/flag_egypt.svg';
 import FlagElSalvador from './svg/flag_elsalvador.svg';
+import FlagEstonia from './svg/flag_estonia.svg';
 import FlagFinland from './svg/flag_finland.svg';
 import FlagFrance from './svg/flag_france.svg';
+import FlagGeorgia from './svg/flag_georgia.svg';
 import FlagGermany from './svg/flag_germany.svg';
+import FlagGibraltar from './svg/flag_gibraltar.svg';
+import FlagGreece from './svg/flag_greece.svg';
+import FlagGrenada from './svg/flag_grenada.svg';
 import FlagGuatemala from './svg/flag_guatemala.svg';
 import FlagHonduras from './svg/flag_honduras.svg';
+import FlagHongKong from './svg/flag_hongkong.svg';
 import FlagHungary from './svg/flag_hungary.svg';
 import FlagIndia from './svg/flag_india.svg';
 import FlagIndonesia from './svg/flag_indonesia.svg';
+import FlagIran from './svg/flag_iran.svg';
 import FlagIreland from './svg/flag_ireland.svg';
 import FlagIsrael from './svg/flag_israel.svg';
 import FlagItaly from './svg/flag_italy.svg';
 import FlagJapan from './svg/flag_japan.svg';
+import FlagJordan from './svg/flag_jordan.svg';
+import FlagLatvia from './svg/flag_latvia.svg';
 import FlagLuxembourg from './svg/flag_luxembourg.svg';
+import FlagMalasya from './svg/flag_malasya.svg';
 import FlagMexico from './svg/flag_mexico.svg';
+import FlagMoldova from './svg/flag_moldova.svg';
+import FlagMorocco from './svg/flag_morocco.svg';
 import FlagNetherlands from './svg/flag_netherlands.svg';
+import FlagNewZealeand from './svg/flag_newzealand.svg';
+import FlagNigeria from './svg/flag_nigeria.svg';
+import FlagNorthKorea from './svg/flag_northkorea.svg';
+import FlagNorthMacedonia from './svg/flag_northmacedonia.svg';
+import FlagNorway from './svg/flag_norway.svg';
+import FlagPakistan from './svg/flag_pakistan.svg';
 import FlagPanama from './svg/flag_panama.svg';
 import FlagParaguay from './svg/flag_paraguay.svg';
 import FlagPeru from './svg/flag_peru.svg';
+import FlagPhilippines from './svg/flag_philippines.svg';
 import FlagPoland from './svg/flag_poland.svg';
 import FlagPortugal from './svg/flag_portugal.svg';
 import FlagPuertoRico from './svg/flag_puertorico.svg';
+import FlagRomania from './svg/flag_romania.svg';
+import FlagRussia from './svg/flag_russia.svg';
+import FlagSaudiArabia from './svg/flag_saudiarabia.svg';
+import FlagSerbia from './svg/flag_serbia.svg';
 import FlagSingapore from './svg/flag_singapore.svg';
+import FlagSlovakia from './svg/flag_slovakia.svg';
+import FlagSlovenia from './svg/flag_slovenia.svg';
 import FlagSouthAfrica from './svg/flag_southafrica.svg';
 import FlagSouthKorea from './svg/flag_southkorea.svg';
 import FlagSpain from './svg/flag_spain.svg';
 import FlagSweden from './svg/flag_sweden.svg';
 import FlagSwitzerland from './svg/flag_switzerland.svg';
+import FlagTaiwan from './svg/flag_taiwan.svg';
+import FlagThailand from './svg/flag_thailand.svg';
+import FlagTunisia from './svg/flag_tunisia.svg';
 import FlagTurkey from './svg/flag_turkey.svg';
 import FlagUK from './svg/flag_uk.svg';
-import FlagUruguay from './svg/flag_uruguay.svg';
 import FlagUS from './svg/flag_us.svg';
-import FlagBoliva from './svg/flag_boliva.svg';
-import FlagGreece from './svg/flag_greece.svg';
-import FlagHongKong from './svg/flag_hongkong.svg';
-import FlagNewZealeand from './svg/flag_newzealand.svg';
-import FlagNorway from './svg/flag_norway.svg';
-import FlagRomania from './svg/flag_romania.svg';
-import FlagThailand from './svg/flag_thailand.svg';
-import FlagTaiwan from './svg/flag_taiwan.svg';
-import FlagTunisia from './svg/flag_tunisia.svg';
-import FlagMalasya from './svg/flag_malasya.svg';
-import flagAlgeria from './svg/flag_algeria.svg';
-import flagBarbados from './svg/flag_barbados.svg';
-import flagBelarus from './svg/flag_belarus.svg';
-import flagBosnia from './svg/flag_bosnia.svg';
-import flagBritishVirginIslands from './svg/flag_britishvirginislands.svg';
-import flagEgypt from './svg/flag_egypt.svg';
-import flagEstonia from './svg/flag_estonia.svg';
-import flagGeorgia from './svg/flag_georgia.svg';
-import flagGibraltar from './svg/flag_gibraltar.svg';
-import flagGrenada from './svg/flag_grenada.svg';
-import flagJordan from './svg/flag_jordan.svg';
-import flagLatvia from './svg/flag_latvia.svg';
-import flagNorthMacedonia from './svg/flag_northmacedonia.svg';
-import flagMoldova from './svg/flag_moldova.svg';
-import flagMorocco from './svg/flag_morocco.svg';
-import flagNigeria from './svg/flag_nigeria.svg';
-import flagPakistan from './svg/flag_pakistan.svg';
-import flagPhilippines from './svg/flag_philippines.svg';
-import flagSerbia from './svg/flag_serbia.svg';
-import flagRussia from './svg/flag_russia.svg';
-import flagSaudiArabia from './svg/flag_saudiarabia.svg';
-import flagSlovakia from './svg/flag_slovakia.svg';
-import flagSlovenia from './svg/flag_slovenia.svg';
-import flagUkraine from './svg/flag_ukraine.svg';
-import flagVenezuela from './svg/flag_venezuela.svg';
-import flagVietnam from './svg/flag_vietnam.svg';
+import FlagUkraine from './svg/flag_ukraine.svg';
+import FlagUruguay from './svg/flag_uruguay.svg';
+import FlagVenezuela from './svg/flag_venezuela.svg';
+import FlagVietnam from './svg/flag_vietnam.svg';
 
 export {
+  FlagAlgeria,
   FlagArgentina,
+  FlagArmenia,
   FlagAustralia,
   FlagAustria,
+  FlagBarbados,
+  FlagBelarus,
   FlagBelgium,
+  FlagBoliva,
+  FlagBosnia,
   FlagBrazil,
+  FlagBritishVirginIslands,
   FlagBulgary,
   FlagCanada,
   FlagChile,
   FlagChina,
+  FlagCyprus,
   FlagColombia,
   FlagCostaRica,
   FlagCroatia,
@@ -100,11 +112,18 @@ export {
   FlagDenmark,
   FlagDominicanRepublic,
   FlagEcuador,
+  FlagEgypt,
   FlagElSalvador,
+  FlagEstonia,
   FlagFinland,
   FlagFrance,
+  FlagGeorgia,
   FlagGermany,
+  FlagGibraltar,
+  FlagGreece,
+  FlagGrenada,
   FlagGuatemala,
+  FlagHongKong,
   FlagHonduras,
   FlagHungary,
   FlagIndia,
@@ -112,60 +131,49 @@ export {
   FlagIreland,
   FlagIsrael,
   FlagItaly,
+  FlagIran,
   FlagJapan,
+  FlagJordan,
+  FlagLatvia,
   FlagLuxembourg,
+  FlagMalasya,
   FlagMexico,
+  FlagMoldova,
+  FlagMorocco,
   FlagNetherlands,
+  FlagNewZealeand,
+  FlagNigeria,
+  FlagNorway,
   FlagPanama,
   FlagParaguay,
   FlagPeru,
+  FlagPhilippines,
   FlagPoland,
   FlagPortugal,
   FlagPuertoRico,
+  FlagPakistan,
+  FlagNorthKorea,
+  FlagNorthMacedonia,
+  FlagRomania,
+  FlagRussia,
+  FlagSaudiArabia,
   FlagSingapore,
+  FlagSerbia,
+  FlagSlovakia,
+  FlagSlovenia,
   FlagSouthAfrica,
   FlagSouthKorea,
   FlagSpain,
   FlagSweden,
   FlagSwitzerland,
+  FlagTaiwan,
+  FlagThailand,
+  FlagTunisia,
   FlagTurkey,
   FlagUK,
+  FlagUkraine,
   FlagUruguay,
   FlagUS,
-  FlagBoliva,
-  FlagGreece,
-  FlagHongKong,
-  FlagNewZealeand,
-  FlagNorway,
-  FlagRomania,
-  FlagThailand,
-  FlagTaiwan,
-  FlagTunisia,
-  FlagMalasya,
-  flagAlgeria,
-  flagBarbados,
-  flagBelarus,
-  flagBosnia,
-  flagBritishVirginIslands,
-  flagEgypt,
-  flagEstonia,
-  flagGeorgia,
-  flagGibraltar,
-  flagGrenada,
-  flagJordan,
-  flagLatvia,
-  flagNorthMacedonia,
-  flagMoldova,
-  flagMorocco,
-  flagNigeria,
-  flagPakistan,
-  flagPhilippines,
-  flagSerbia,
-  flagRussia,
-  flagSaudiArabia,
-  flagSlovakia,
-  flagSlovenia,
-  flagUkraine,
-  flagVenezuela,
-  flagVietnam,
+  FlagVenezuela,
+  FlagVietnam,
 };
