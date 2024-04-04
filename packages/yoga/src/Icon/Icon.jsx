8e58c2dbcacdfd7925622152ed65bbb059d12303
Flavia Fernanda Rocha Moraes <ext.flavia.moraes@gympass.com>
2024-04-04 11:01:31 -0300

    style: create fixed color condition for new icons

--- packages/yoga/src/Icon/Icon.jsx
+++ packages/yoga/src/Icon/Icon.jsx
@@ -25,6 +25,8 @@ const Icon = ({
   ariaLabel,
   ...props
 }) => {
+  const fixedColorIcons = ['Attention', 'Delayed', 'Information', 'Success'];
+
   const componentWithTitle = propsTitle => {
     const titleId = `${ariaLabel}-titleId`;
     let ariaDescribedBy = titleId;
@@ -65,13 +67,16 @@ const Icon = ({
     return newSvg;
   };
 
+  const isColorable = !fixedColorIcons.includes(Component.name);
+
   return (
     <Box
       as={title && ariaLabel ? componentWithTitle : Component}
       width={get(theme.yoga.spacing, width, width)}
       height={get(theme.yoga.spacing, height, height)}
-      {...(fill && { fill: get(theme.yoga.colors, fill, fill) })}
-      {...(stroke && { stroke: get(theme.yoga.colors, stroke, stroke) })}
+      {...(fill && isColorable && { fill: get(theme.yoga.colors, fill, fill) })}
+      {...(stroke &&
+        isColorable && { stroke: get(theme.yoga.colors, stroke, stroke) })}
       {...props}
       aria-hidden={title ? undefined : true}
     />
