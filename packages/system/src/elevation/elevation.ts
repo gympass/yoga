9aa77a5a4633df102a8614db75735c0a56e7cdd8
Esthefanie Vila Maior <esthefanie.maior@gympass.com>
2024-05-16 17:39:17 +0100

    feat: add elevation types

--- packages/system/src/elevation/elevation.ts
+++ packages/system/src/elevation/elevation.ts
@@ -1,11 +1,11 @@
-import { getElevation, generator } from './theme';
+import { getElevation, generator } from '../theme';
 
 const elevation = props =>
   generator({
     props,
     prop: ['boxShadow', 'bs', 'elevation'],
     cssProperty: 'boxShadow',
-    getter: getElevation,
+    getter: getElevation
   });
 
 export { elevation };
