4ff2dff29c78b679fd4fde81d43f7fdc988ecfcc
Matheus Henrique <matheushds.br@gmail.com>
2024-05-21 14:47:34 -0300

    refactor(system): adjust files structure

--- packages/system/src/layout/layout.test.js
+++ packages/system/src/layout/layout.test.js
@@ -8,7 +8,7 @@ import {
   bottom,
   left,
   zIndex,
-} from '.';
+} from './layout';
 
 const spacings = [0, 4, 8, 12];
 
---
+++ packages/system/src/layout/layout.ts
@@ -0,0 +1,63 @@
+import { toPx } from '../unit';
+import { compose, generator, getSpacing } from '../theme';
+
+const display = props =>
+  generator({
+    props,
+    prop: ['display', 'd'],
+    cssProperty: 'display',
+  });
+
+const position = props =>
+  generator({
+    props,
+    prop: ['position'],
+    cssProperty: 'position',
+  });
+
+const top = props =>
+  generator({
+    props,
+    prop: ['top'],
+    cssProperty: 'top',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const right = props =>
+  generator({
+    props,
+    prop: ['right'],
+    cssProperty: 'right',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const bottom = props =>
+  generator({
+    props,
+    prop: ['bottom'],
+    cssProperty: 'bottom',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const left = props =>
+  generator({
+    props,
+    prop: ['left'],
+    cssProperty: 'left',
+    getter: getSpacing,
+    transform: toPx,
+  });
+
+const zIndex = props =>
+  generator({
+    props,
+    prop: ['zIndex'],
+    cssProperty: 'zIndex',
+  });
+
+const positions = compose(position, top, right, bottom, left, zIndex);
+
+export { display, positions, position, top, right, bottom, left, zIndex };
