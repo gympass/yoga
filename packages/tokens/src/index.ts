8e8b4a9837774bbf8b76f4ddabdde49071aea622
Matheus Henrique <matheushds.br@gmail.com>
2024-05-27 14:51:24 -0300

    refactor(tokens): remove breakpoins types exports

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,6 +1,5 @@
 import tokens from './global';
 import znCn from './locale/zn-cn';
-import { BreakpointsKey, BreakpointsType } from './global/breakpoints';
 
-export { tokens as default, znCn, BreakpointsKey, BreakpointsType };
+export { tokens as default, znCn };
 export * from './global';
