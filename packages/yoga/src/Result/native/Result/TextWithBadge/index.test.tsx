69c00a7b5349a36b2572c51103f06d57763957cf
Felipe Rocha <felipe.rocha@gympass.com>
2024-06-11 18:25:41 -0300

    chore: add textwithbadge tests

---
+++ packages/yoga/src/Result/native/Result/TextWithBadge/index.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { ThemeProvider } from '../../../../index';
+import TextWithBadge from '.';
+
+import { WellhubIcon } from '../../../../../../icons/src/svg/icon_wellhub.svg';
+
+describe('TextWithBadge', () => {
+  it('should match snapshot', () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <TextWithBadge
+          avatarWidth={50}
+          badgeIcon={WellhubIcon}
+          title="Text with badge"
+        />
+      </ThemeProvider>,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should match snapshot with long title', () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <TextWithBadge
+          avatarWidth={50}
+          badgeIcon={WellhubIcon}
+          title="This is an example of a very long title that should be truncated"
+        />
+      </ThemeProvider>,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should match snapshot without badgeIcon', () => {
+    const { toJSON } = render(
+      <ThemeProvider>
+        <TextWithBadge
+          avatarWidth={50}
+          badgeIcon={null}
+          title="Title without Badge"
+        />
+      </ThemeProvider>,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
