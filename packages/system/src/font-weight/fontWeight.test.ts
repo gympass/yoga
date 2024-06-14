a8ee1ed1a4ce742ba1a44a82648a650d43fe6cf4
flavia-moraes <ext.flavia.moraes@gympass.com>
2024-06-14 11:52:00 -0300

    fix: linter fixes

--- packages/system/src/font-weight/fontWeight.test.ts
+++ packages/system/src/font-weight/fontWeight.test.ts
@@ -5,13 +5,13 @@ describe('Android', () => {
   const fontWeights = {
     regular: 400,
     semiBold: 600,
-    black: 900
+    black: 900,
   };
 
   const baseFont = {
     family: 'sans-serif',
-  }
-  
+  };
+
   const theme = {
     yoga: {
       baseFont,
@@ -19,28 +19,28 @@ describe('Android', () => {
   };
 
   describe('fontWeight', () => {
-      it('Should return values for fontWeight prop', () => {
-        const expectedRegularFontWeight = css({ fontFamily: `${baseFont.family}` });
-        const expectedSemiBoldFontWeight = css({ fontFamily: `${baseFont.family}-${fontWeights.semiBold}` });
-        const expectedBlackFontWeight = css({ fontFamily: `${baseFont.family}-${fontWeights.black}` });
-  
-        const regular = androidFontWeight({ theme, fontWeight: 400 });
-        const regular1 = androidFontWeight({ theme, fw: 400 });
-        const semiBold = androidFontWeight({ theme, fontWeight: 600 });
-        const black = androidFontWeight({ theme, fw: 900 });
+    it('Should return values for fontWeight prop', () => {
+      const expectedRegularFontWeight = css({ fontFamily: `${baseFont.family}` });
+      const expectedSemiBoldFontWeight = css({ fontFamily: `${baseFont.family}-${fontWeights.semiBold}` });
+      const expectedBlackFontWeight = css({ fontFamily: `${baseFont.family}-${fontWeights.black}` });
+
+      const regular = androidFontWeight({ theme, fontWeight: 400 });
+      const regular1 = androidFontWeight({ theme, fw: 400 });
+      const semiBold = androidFontWeight({ theme, fontWeight: 600 });
+      const black = androidFontWeight({ theme, fw: 900 });
             
-        expect(regular).toStrictEqual(regular1);
-        expect(regular).toStrictEqual(expectedRegularFontWeight);
-        expect(semiBold).toStrictEqual(expectedSemiBoldFontWeight);
-        expect(black).toStrictEqual(expectedBlackFontWeight);      
-      });
+      expect(regular).toStrictEqual(regular1);
+      expect(regular).toStrictEqual(expectedRegularFontWeight);
+      expect(semiBold).toStrictEqual(expectedSemiBoldFontWeight);
+      expect(black).toStrictEqual(expectedBlackFontWeight);      
+    });
     
-      it('Should return the value if there is no theme match', () => {
-        const expectedNoTheme = css({ fontFamily: `${baseFont.family}-800` });
-     
-        const fw = androidFontWeight({ theme, fontWeight: 800 });
+    it('Should return the value if there is no theme match', () => {
+      const expectedNoTheme = css({ fontFamily: `${baseFont.family}-800` });
+
+      const fw = androidFontWeight({ theme, fontWeight: 800 });
     
-        expect(fw).toStrictEqual(expectedNoTheme);
-      });
-    })
+      expect(fw).toStrictEqual(expectedNoTheme);
+    });
+  })
 });
