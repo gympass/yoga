8dc1ca83b1fc6d9374f256781872fd8dc10a9994
thiamancio <thiago.amancio@gympass.com>
2024-06-17 07:52:02 -0300

    feat: format

--- packages/system/src/newSystem/translate.android.ts
+++ packages/system/src/newSystem/translate.android.ts
@@ -1,9 +1,6 @@
-import defaultProps from './defaultProps'
+import defaultProps from './defaultProps';
 
 export default function translate(prop: string): string {
-  const props = { ...defaultProps, elevation: 'elevation' }
+  const props = { ...defaultProps, elevation: 'elevation' };
   return props[prop] || prop;
 }
-
-
-
