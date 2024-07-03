f876c95fc9b1e7f6a781c86532860d5ef9b0afe3
Matheus Henrique <matheushds.br@gmail.com>
2024-07-03 10:59:20 -0300

    refactor(system): add types

--- packages/system/src/font-weight/fontWeight.android.ts
+++ packages/system/src/font-weight/fontWeight.android.ts
@@ -1,13 +1,14 @@
 import { getFontWeight, generator } from '../theme';
+import { SystemValues } from '../types';
 
-export const fontWeight = props =>
+export const fontWeight = (props: SystemValues) =>
   generator({
     props,
     prop: ['fontWeight', 'fw'],
     cssProperty: 'font-family',
     getter: getFontWeight,
     transform: value => {
-      if (!value) {
+      if (!value || !props.theme) {
         return undefined;
       }
 
