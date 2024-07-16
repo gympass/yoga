0671419275623795a19b0fdd6f2fcc2ed24f5b6d
Matheus Henrique <matheushds.br@gmail.com>
2024-07-16 11:55:03 -0300

    refactor(yoga): more descriptive types names

--- packages/common/src/merge.ts
+++ packages/common/src/merge.ts
@@ -1,6 +1,6 @@
 type UnknownObject = Record<string, unknown>;
 
-const merge = <T extends UnknownObject, U extends UnknownObject>(target: T, source: U): T & U => {
+const merge = <TargetType extends UnknownObject, SourceType extends UnknownObject>(target: TargetType, source: SourceType): TargetType & SourceType => {
   const newTarget: UnknownObject = { ...target };
 
   Object.keys(source).forEach(key => {
@@ -15,7 +15,7 @@ const merge = <T extends UnknownObject, U extends UnknownObject>(target: T, sour
     }
   });
 
-  return newTarget as T & U;
+  return newTarget as TargetType & SourceType;
 };
 
 export default merge;
