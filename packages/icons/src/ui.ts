e7ebb8099e1c76f26153863492ddd2e693d1a8b8
Luciano Lima <luciano.lima@gympass.com>
2025-06-22 13:57:40 -0300

    feat: [PAC-1166] Add new icons for firt check-in
    Also, updated icons for Racquet and YogaMat.

--- packages/icons/src/ui.ts
+++ packages/icons/src/ui.ts
@@ -3,8 +3,8 @@ import Accessible from './svg/accessible.svg';
 import AccessibleParking from './svg/accessible_parking_space.svg';
 import AccessibleRestroom from './svg/accessible_restroom.svg';
 import AccessibleShower from './svg/accessible_shower.svg';
-import AddBooking from './svg/add_booking.svg';
 import Add from './svg/add.svg';
+import AddBooking from './svg/add_booking.svg';
 import AirConditioning from './svg/air-conditioning.svg';
 import AlertCircle from './svg/alert_circle.svg';
 import AlertTriangle from './svg/alert_triangle.svg';
@@ -17,6 +17,7 @@ import ArrowUp from './svg/arrow_up.svg';
 import Attention from './svg/attention.svg';
 import AutismFriendly from './svg/autism_friendly.svg';
 import AutomaticInvite from './svg/automatic_invite.svg';
+import Backspace from './svg/backspace.svg';
 import Bank from './svg/bank.svg';
 import BankStatement from './svg/bank_statement.svg';
 import BarChart from './svg/bar_chart.svg';
@@ -26,33 +27,33 @@ import BeautySalon from './svg/beauty-salon.svg';
 import Bike from './svg/bike.svg';
 import BioimpedanceScale from './svg/bioimpedance-scale.svg';
 import BodyMassage from './svg/body-massage.svg';
-import BookingFilled from './svg/booking_filled.svg';
 import Booking from './svg/booking.svg';
+import BookingFilled from './svg/booking_filled.svg';
 import Boxing from './svg/boxing.svg';
 import BrailleSigns from './svg/braille_signs.svg';
-import BuildingFilled from './svg/building_filled.svg';
 import Building from './svg/building.svg';
+import BuildingFilled from './svg/building_filled.svg';
 import BulletList from './svg/bullet_list.svg';
 import Camera from './svg/camera.svg';
 import Cart from './svg/cart.svg';
 import Chat from './svg/chat.svg';
-import CheckFilled from './svg/check_filled.svg';
 import Check from './svg/check.svg';
+import CheckFilled from './svg/check_filled.svg';
 import CheckedFull from './svg/checked_full.svg';
 import CheckedHalf from './svg/checked_half.svg';
-import CheckinFilled from './svg/checkin_filled.svg';
 import Checkin from './svg/checkin.svg';
+import CheckinFilled from './svg/checkin_filled.svg';
 import ChevronDown from './svg/chevron_down.svg';
 import ChevronLeft from './svg/chevron_left.svg';
 import ChevronRight from './svg/chevron_right.svg';
 import ChevronUp from './svg/chevron_up.svg';
 import ChildrenArea from './svg/children-area.svg';
-import CloseFilled from './svg/close_filled.svg';
 import Close from './svg/close.svg';
+import CloseFilled from './svg/close_filled.svg';
 import Code from './svg/code.svg';
 import CoffeeShop from './svg/coffee-shop.svg';
-import CompassFilled from './svg/compass_filled.svg';
 import Compass from './svg/compass.svg';
+import CompassFilled from './svg/compass_filled.svg';
 import Copy from './svg/copy.svg';
 import CornerUpLeft from './svg/corner_up_left.svg';
 import CornerUpRight from './svg/corner_up_right.svg';
@@ -65,8 +66,8 @@ import Dance from './svg/dance.svg';
 import Delayed from './svg/delayed.svg';
 import Diagram from './svg/diagram.svg';
 import Disconnect from './svg/disconnect.svg';
-import DislikeFilled from './svg/dislike_filled.svg';
 import Dislike from './svg/dislike.svg';
+import DislikeFilled from './svg/dislike_filled.svg';
 import Doc from './svg/doc.svg';
 import Download from './svg/download.svg';
 import Draggable from './svg/draggable.svg';
@@ -80,9 +81,10 @@ import ExternalLink from './svg/external_link.svg';
 import Facebook from './svg/facebook.svg';
 import FamilyMember from './svg/family_member.svg';
 import FamilyMemberFilled from './svg/family_member_filled.svg';
-import FavoriteFilled from './svg/favorite_filled.svg';
 import Favorite from './svg/favorite.svg';
+import FavoriteFilled from './svg/favorite_filled.svg';
 import Filter from './svg/filter.svg';
+import FootwearSneakers from './svg/footwear_sneakers.svg';
 import FreeWeight from './svg/free-weight.svg';
 import FunctionalTraining from './svg/functional-training.svg';
 import FunnelFilter from './svg/funnel_filter.svg';
@@ -93,26 +95,27 @@ import Gympass from './svg/gympass.svg';
 import Headphone from './svg/headphone.svg';
 import Heartbeat from './svg/heartbeat.svg';
 import Heater from './svg/heater.svg';
-import HelpCircle from './svg/help_circle.svg';
 import Help from './svg/help.svg';
+import HelpCircle from './svg/help_circle.svg';
 import History from './svg/history.svg';
 import Hit from './svg/hit.svg';
-import HomeFilled from './svg/home_filled.svg';
 import Home from './svg/home.svg';
+import HomeFilled from './svg/home_filled.svg';
 import HotTub from './svg/hot-tub.svg';
+import WellhubIcon from './svg/icon_wellhub.svg';
 import Image from './svg/image.svg';
 import Info from './svg/info.svg';
 import Information from './svg/information.svg';
 import Instagram from './svg/instagram.svg';
 import JumpRope from './svg/jump_rope.svg';
-import LikeFilled from './svg/like_filled.svg';
 import Like from './svg/like.svg';
+import LikeFilled from './svg/like_filled.svg';
 import Link from './svg/link.svg';
 import Linkedin from './svg/linkedin.svg';
 import Location from './svg/location.svg';
+import Lock from './svg/lock.svg';
 import LockOff from './svg/lock_off.svg';
 import LockOnFilled from './svg/lock_on_filled.svg';
-import Lock from './svg/lock.svg';
 import LockerRoom from './svg/locker_room.svg';
 import Lockers from './svg/lockers.svg';
 import LogOut from './svg/log_out.svg';
@@ -120,8 +123,8 @@ import Loop from './svg/loop.svg';
 import MailRead from './svg/mail_read.svg';
 import MailUnread from './svg/mail_unread.svg';
 import ManualInvite from './svg/manual_invite.svg';
-import MapPin from './svg/map_pin.svg';
 import Map from './svg/map.svg';
+import MapPin from './svg/map_pin.svg';
 import MartialArts from './svg/martial-arts.svg';
 import MassageChair from './svg/massage-chair.svg';
 import MenuList from './svg/menu_list.svg';
@@ -134,32 +137,35 @@ import NetworkOn from './svg/network_on.svg';
 import NotificationFilled from './svg/notification_filled.svg';
 import NotificationOff from './svg/notification_off.svg';
 import Nutritionist from './svg/nutritionist.svg';
+import PaddleBoard from './svg/paddle_board.svg';
 import PaperPin from './svg/paper_pin.svg';
 import Paperclip from './svg/paperclip.svg';
+import ParkingLot from './svg/parking-lot.svg';
 import Password from './svg/password.svg';
 import Pause from './svg/pause.svg';
 import PauseFilled from './svg/pause_filled.svg';
-import PhoneFilled from './svg/phone_filled.svg';
 import Phone from './svg/phone.svg';
+import PhoneFilled from './svg/phone_filled.svg';
+import Physiotherapy from './svg/physiotherapy.svg';
 import PieChart from './svg/pie_chart.svg';
 import Pilates from './svg/pilates.svg';
 import PinFilled from './svg/pin_filled.svg';
-import Physiotherapy from './svg/physiotherapy.svg';
 import Play from './svg/play.svg';
 import PlayArrow from './svg/play_arrow.svg';
 import PlayFilled from './svg/play_filled.svg';
 import PlusCircle from './svg/plus_circle.svg';
 import Pool from './svg/pool.svg';
+import PriceTag from './svg/price_tag.svg';
 import Printer from './svg/printer.svg';
-import QRCodeReader from './svg/qr_code_reader.svg';
 import QRCode from './svg/qr_code.svg';
+import QRCodeReader from './svg/qr_code_reader.svg';
 import Racquet from './svg/racquet.svg';
 import Rectangle from './svg/rectangle.svg';
 import Refresh from './svg/refresh.svg';
 import Restaurant from './svg/restaurant.svg';
 import Sauna from './svg/sauna.svg';
-import SearchFilled from './svg/search_filled.svg';
 import Search from './svg/search.svg';
+import SearchFilled from './svg/search_filled.svg';
 import ServiceDogs from './svg/service_dogs.svg';
 import Settings from './svg/settings.svg';
 import ShareAndroid from './svg/share_android.svg';
@@ -167,31 +173,31 @@ import ShareIos from './svg/share_ios.svg';
 import ShieldCheck from './svg/shield_check.svg';
 import ShieldSecure from './svg/shield_secure.svg';
 import ShineFilled from './svg/shine_filled.svg';
-import Shower from './svg/shower.svg';
 import ShowerTowels from './svg/shower-towels.svg';
+import Shower from './svg/shower.svg';
 import SignLanguage from './svg/sign_language.svg';
 import Signal from './svg/signal.svg';
-import SmartphoneFilled from './svg/smartphone_filled.svg';
+import SibngleWomanFlag from './svg/single_woman_flag.svg';
 import Smartphone from './svg/smartphone.svg';
+import SmartphoneFilled from './svg/smartphone_filled.svg';
 import SnackBar from './svg/snack-bar.svg';
 import SortAscending from './svg/sort_ascending.svg';
 import SortDescending from './svg/sort_descending.svg';
 import Spa from './svg/spa.svg';
 import SportsEquipment from './svg/sports-equipment-store.svg';
 import StairFreeAccess from './svg/stair_free_access.svg';
-import StarFilled from './svg/star_filled.svg';
 import Star from './svg/star.svg';
+import StarFilled from './svg/star_filled.svg';
 import Stretching from './svg/stretching.svg';
 import Success from './svg/success.svg';
 import SupportTrainer from './svg/support-trainer.svg';
 import SystemStatus from './svg/system_status.svg';
 import TactilePaving from './svg/tactile_paving.svg';
-import ParkingLot from './svg/parking-lot.svg';
-import PriceTag from './svg/price_tag.svg';
+import Tennis from './svg/tennis.svg';
 import Time from './svg/time.svg';
 import TimeFilled from './svg/time_filled.svg';
-import Token from './svg/token.svg';
 import Toiletries from './svg/toiletries.svg';
+import Token from './svg/token.svg';
 import TrainingTowels from './svg/training-towels.svg';
 import Trash from './svg/trash.svg';
 import TrendingNoChange from './svg/trending_no_change.svg';
@@ -200,25 +206,23 @@ import TriangleAlert from './svg/triangle_alert.svg';
 import Twitter from './svg/twitter.svg';
 import Upload from './svg/upload.svg';
 import Upload2 from './svg/upload2.svg';
+import User from './svg/user.svg';
 import UserAdd from './svg/user_add.svg';
-import Tennis from './svg/tennis.svg';
 import UserFilled from './svg/user_filled.svg';
 import UserFilledStar from './svg/user_filled_star.svg';
-import User from './svg/user.svg';
 import Video from './svg/video.svg';
-import VisibilityOff from './svg/visibility_off.svg';
 import Visibility from './svg/visibility.svg';
+import VisibilityOff from './svg/visibility_off.svg';
 import Wallet from './svg/wallet.svg';
+import WaterBottle from './svg/water_bottle.svg';
 import Waze from './svg/waze.svg';
 import WeightMachine from './svg/weight-machine.svg';
 import Whatsapp from './svg/whatsapp.svg';
 import WheelchairFriendlyDoors from './svg/wheelchair_friendly_doors.svg';
-import WellhubIcon from './svg/icon_wellhub.svg';
 import WiFi from './svg/wi-fi.svg';
 import YogaMat from './svg/yoga-mat.svg';
 import Youtube from './svg/youtube.svg';
 import Zen from './svg/zen.svg';
-import Backspace from './svg/backspace.svg';
 
 export {
   Accessibility,
@@ -226,8 +230,8 @@ export {
   AccessibleParking,
   AccessibleRestroom,
   AccessibleShower,
-  AddBooking,
   Add,
+  AddBooking,
   AirConditioning,
   AlertCircle,
   AlertTriangle,
@@ -240,6 +244,7 @@ export {
   Attention,
   AutismFriendly,
   AutomaticInvite,
+  Backspace,
   Bank,
   BankStatement,
   BarChart,
@@ -249,33 +254,33 @@ export {
   Bike,
   BioimpedanceScale,
   BodyMassage,
-  BookingFilled,
   Booking,
+  BookingFilled,
   Boxing,
   BrailleSigns,
-  BuildingFilled,
   Building,
+  BuildingFilled,
   BulletList,
   Camera,
   Cart,
   Chat,
-  CheckFilled,
   Check,
   CheckedFull,
   CheckedHalf,
-  CheckinFilled,
+  CheckFilled,
   Checkin,
+  CheckinFilled,
   ChevronDown,
   ChevronLeft,
   ChevronRight,
   ChevronUp,
   ChildrenArea,
-  CloseFilled,
   Close,
+  CloseFilled,
   Code,
   CoffeeShop,
-  CompassFilled,
   Compass,
+  CompassFilled,
   Copy,
   CornerUpLeft,
   CornerUpRight,
@@ -288,8 +293,8 @@ export {
   Delayed,
   Diagram,
   Disconnect,
-  DislikeFilled,
   Dislike,
+  DislikeFilled,
   Doc,
   Download,
   Draggable,
@@ -303,9 +308,10 @@ export {
   Facebook,
   FamilyMember,
   FamilyMemberFilled,
-  FavoriteFilled,
   Favorite,
+  FavoriteFilled,
   Filter,
+  FootwearSneakers,
   FreeWeight,
   FunctionalTraining,
   FunnelFilter,
@@ -316,35 +322,35 @@ export {
   Headphone,
   Heartbeat,
   Heater,
-  HelpCircle,
   Help,
+  HelpCircle,
   History,
   Hit,
-  HomeFilled,
   Home,
+  HomeFilled,
   HotTub,
   Image,
   Info,
   Information,
   Instagram,
   JumpRope,
-  LikeFilled,
   Like,
+  LikeFilled,
   Link,
   Linkedin,
   Location,
-  LockOff,
-  LockOnFilled,
   Lock,
   LockerRoom,
   Lockers,
+  LockOff,
+  LockOnFilled,
   LogOut,
   Loop,
   MailRead,
   MailUnread,
   ManualInvite,
-  MapPin,
   Map,
+  MapPin,
   MartialArts,
   MassageChair,
   MenuList,
@@ -357,31 +363,35 @@ export {
   NotificationFilled,
   NotificationOff,
   Nutritionist,
-  PaperPin,
+  PaddleBoard,
   Paperclip,
+  PaperPin,
+  ParkingLot,
   Password,
   Pause,
   PauseFilled,
-  PhoneFilled,
   Phone,
+  PhoneFilled,
+  Physiotherapy,
   PieChart,
   Pilates,
   PinFilled,
-  Physiotherapy,
   Play,
   PlayArrow,
   PlayFilled,
   PlusCircle,
   Pool,
+  PriceTag,
   Printer,
-  QRCodeReader,
   QRCode,
+  QRCodeReader,
   Racquet,
   Rectangle,
   Refresh,
   Restaurant,
-  SearchFilled,
+  Sauna,
   Search,
+  SearchFilled,
   ServiceDogs,
   Settings,
   ShareAndroid,
@@ -391,31 +401,29 @@ export {
   ShineFilled,
   Shower,
   ShowerTowels,
-  SignLanguage,
+  SibngleWomanFlag,
   Signal,
-  SmartphoneFilled,
+  SignLanguage,
   Smartphone,
+  SmartphoneFilled,
   SnackBar,
   SortAscending,
   SortDescending,
   Spa,
   SportsEquipment,
   StairFreeAccess,
-  StarFilled,
   Star,
+  StarFilled,
   Stretching,
   Success,
   SupportTrainer,
   SystemStatus,
   TactilePaving,
-  ParkingLot,
-  PriceTag,
-  Sauna,
   Tennis,
   Time,
   TimeFilled,
-  Token,
   Toiletries,
+  Token,
   TrainingTowels,
   Trash,
   TrendingNoChange,
@@ -424,22 +432,23 @@ export {
   Twitter,
   Upload,
   Upload2,
+  User,
   UserAdd,
   UserFilled,
   UserFilledStar,
-  User,
   Video,
-  VisibilityOff,
   Visibility,
+  VisibilityOff,
   Wallet,
+  WaterBottle,
   Waze,
   WeightMachine,
+  WellhubIcon,
   Whatsapp,
   WheelchairFriendlyDoors,
-  WellhubIcon,
   WiFi,
   YogaMat,
   Youtube,
-  Zen,
-  Backspace,
+  Zen
 };
+
